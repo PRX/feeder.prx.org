460171421ed964a910d8cbc712d4e549a83e2b40
Eve Asher <eve.adele.asher@gmail.com>
2015-01-09 13:50:51 -0500

    add pub_date to episode factory

--- test/factories/episode_factory.rb
+++ test/factories/episode_factory.rb
@@ -3,6 +3,12 @@ FactoryGirl.define do
     podcast
     association :image, factory: :image
     prx_id 87683
-    sequence(:overrides) {|n| "{\"title\":\"Episode #{n}\"}" }
+
+    sequence(:overrides) do |n|
+      {
+        title: "Episode #{n}",
+        pub_date: "Fri, 09 Jan 2015 12:49:44 EST"
+      }.to_json
+    end
   end
 end
