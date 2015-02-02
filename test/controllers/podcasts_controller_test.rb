9439b11f74dff8bfdd62cd1104ff3f3168faca47
Eve Asher <eve.adele.asher@gmail.com>
2015-02-02 14:52:29 -0500

    test podcasts controller to make sure conditional get is implemented correctly

---
+++ test/controllers/podcasts_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+describe PodcastsController do
+  before do
+    stub_requests_to_prx_cms
+    @podcast = create(:podcast, :with_images, last_build_date: Time.now)
+  end
+
+  it 'returns a fresh version of the podcast when it has been updated' do
+    @request.headers["HTTP_IF_MODIFIED_SINCE"] = 1.day.ago.strftime('%a, %d %b %Y %H:%M:%S %Z')
+    get :show, id: @podcast.id, format: 'rss'
+
+    response.status.to_i.must_equal 200
+  end
+
+  it 'returns 304 if the resource has not been updated' do
+    @request.headers["HTTP_IF_MODIFIED_SINCE"] = Time.now.strftime('%a, %d %b %Y %H:%M:%S %Z')
+    get :show, id: @podcast.id, format: 'rss'
+
+    response.status.to_i.must_equal 304
+  end
+end
