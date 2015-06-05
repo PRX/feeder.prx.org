4b4885adf3ed6d1c68c8a4c8a6147ebcf00eb91e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-05 18:26:31 -0400

    have the controller also publish the feed

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,31 +1,38 @@
 class EpisodesController < ApplicationController
   def create
-    podcast = Podcast.find_by(podcast_params)
-    episode = Episode.with_deleted.find_or_initialize_by(prx_id: episode_params[:prx_id],
-                                                         podcast_id: podcast.id)
+    @podcast = Podcast.find_by(podcast_params)
+    @episode = Episode.with_deleted.find_or_initialize_by(prx_id: episode_params[:prx_id],
+                                                          podcast_id: @podcast.id)
 
-    episode.overrides = episode_params[:overrides].to_json
-    episode.deleted_at = nil
+    @episode.overrides = episode_params[:overrides].to_json
+    @episode.deleted_at = nil
 
-    if episode.save
-      DateUpdater.both_dates(podcast)
-      render json: episode
+    if @episode.save
+      publish_feed
+      render json: @episode
+    else
+      render json: @episode.errors
+    end
+  end
+
+  def update
+    @episode = Episode.find(params[:id])
+    @podcast = @episode.podcast
+
+    @episode.overrides = episode_params[:overrides].to_json
+
+    if @episode.save
+      publish_feed
+      render nothing: true
     else
       render json: episode.errors
     end
   end
 
-  def update
-    episode = Episode.find(params[:id])
-
-    episode.overrides = episode_params[:overrides].to_json
-
-    if episode.save
-      DateUpdater.both_dates(episode.podcast)
-      render nothing: true
-    else
-      render json: episode.errors
-    end
+  def publish_feed
+    return unless @podcast
+    DateUpdater.both_dates(@podcast)
+    PublishFeedJob.perform_later(@podcast)
   end
 
   private
