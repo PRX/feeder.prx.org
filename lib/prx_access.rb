88756706759e1bad4bf0d534d89a15c1daff48fe
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-07-07 18:26:47 -0400

    add token requests and api w optional account

--- lib/prx_access.rb
+++ lib/prx_access.rb
@@ -1,6 +1,28 @@
 module PrxAccess
-  def api
-    HyperResource.new(root: cms_root)
+  def api(account = nil)
+    options = { root: cms_root }
+    if account
+      token = get_account_token(account)
+      options[:headers] = { 'Authorization' =>  "Bearer #{token}" }
+    end
+
+    HyperResource.new(options)
+  end
+
+  def get_account_token(account)
+    id = ENV['PRX_CLIENT_ID']
+    se = ENV['PRX_SECRET']
+    oauth_options = { site: id_root, token_url: '/token' }
+    client = OAuth2::Client.new(id, se, oauth_options) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  :excon
+    end
+    client.client_credentials.get_token(account: account).token
+  end
+
+  def id_root
+    ENV['ID_ROOT'] || 'https://id.prx.org/'
   end
 
   def cms_root
