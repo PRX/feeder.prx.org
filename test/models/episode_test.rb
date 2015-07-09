3fa61220958dcbe6ecd865c2d259c0619960ecd9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-07-09 12:07:30 -0400

    add some tests using story

--- test/models/episode_test.rb
+++ test/models/episode_test.rb
@@ -3,6 +3,57 @@ require 'test_helper'
 describe Episode do
   let(:episode) { create(:episode) }
 
+  let(:msg) do
+    %{
+      {
+        "_links": {
+          "curies": [{
+            "name": "prx",
+            "href": "http://meta.prx.org/relation/{rel}",
+            "templated": true
+          }],
+          "self": {
+            "href": "/api/v1/stories/80548",
+            "profile": "http://meta.prx.org/model/story"
+          },
+          "prx:account": {
+            "href": "/api/v1/accounts/125347",
+            "title": "American Routes",
+            "profile": "http://meta.prx.org/model/account/group"
+          },
+          "prx:series": {
+            "href": "/api/v1/series/32166",
+            "title": "American Routes"
+          },
+          "prx:audio": {
+            "href": "/api/v1/stories/80548/audio_files",
+            "count": 2
+          }
+        }
+      }
+    }
+  end
+
+  let(:story) do
+    body = JSON.parse(msg)
+    href = body['_links']['self']['href']
+    resource = HyperResource.new(root: 'https://cms.prx.org/api/vi/')
+    link = HyperResource::Link.new(resource, href: href)
+    HyperResource.new_from(body: body, resource: resource, link: link)
+  end
+
+  it 'can be created from a story' do
+    podcast = create(:podcast, prx_uri: '/api/v1/series/32166')
+    episode = Episode.create_from_story!(story)
+    episode.wont_be_nil
+  end
+
+  it 'can be found by story' do
+    create(:episode, prx_uri: '/api/v1/stories/80548')
+    episode = Episode.by_prx_story(story)
+    episode.wont_be_nil
+  end
+
   it 'must belong to a podcast' do
     episode = build_stubbed(:episode)
     episode.must_be(:valid?)
