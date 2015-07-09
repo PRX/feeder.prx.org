68f0765bfe5d541a4b9cde9d0b8ab2b5d6d6bef8
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-07-09 10:47:17 -0400

    use full audio info, indifferent access, fix prefix

--- test/models/episode_builder_test.rb
+++ test/models/episode_builder_test.rb
@@ -3,41 +3,44 @@ require 'test_helper'
 describe EpisodeBuilder do
   before do
     stub_requests_to_prx_cms
-    @record = build_stubbed(:episode, prx_uri: "/api/v1/stories/87683", overrides: nil)
-    @record.created_at, @record.updated_at = [Time.now, Time.now + 1.day]
-
-    @ep = EpisodeBuilder.from_prx_story(@record)
   end
+
+  let(:episode) do
+    create(:episode, prx_uri: "/api/v1/stories/87683", overrides: nil).tap do |e|
+      e.created_at, e.updated_at = [Time.now, Time.now + 1.day]
+     end
+   end
+
+  let(:eb) { EpisodeBuilder.from_prx_story(episode) }
 
   describe 'without overrides' do
     it 'gets the right story from the prx api' do
-      @ep[:title].must_equal "Virginity, Fidelity, and Fertility"
+      eb[:title].must_equal "Virginity, Fidelity, and Fertility"
     end
 
     it 'gets audio file type' do
-      @ep[:audio_file_type].must_equal "audio/mpeg"
+      eb[:audio][:type].must_equal 'audio/mpeg'
     end
 
     it 'appends podtrac redirect to audio file link' do
-      link = "/pub/472875466d225aca0480000fea4b5fc2/0/web/audio_file/451642/broadcast/Moth1301GarrisonFinal.mp3"
-      prefix = EpisodeBuilder.new(@record).prefix + "mp3"
+      link = '/podcast/episode/filename.mp3'
+      prefix = EpisodeBuilder.new(episode).prefix + 'mp3'
 
-      @ep[:audio_file].must_equal prefix + link
+      eb[:audio][:url].must_equal prefix + '/test-f.prxu.org' + link
     end
 
     it 'gets author info' do
-      @ep[:author_name].must_equal "The Moth"
+      eb[:author_name].must_equal 'The Moth'
     end
   end
 
   describe 'with overrides' do
     it 'includes overrides' do
-      @record.overrides = { title: 'Virginity & Fidelity' }
+      episode.overrides = { title: 'Virginity & Fidelity' }.with_indifferent_access
+      eb = EpisodeBuilder.from_prx_story(episode)
 
-      @ep = EpisodeBuilder.from_prx_story(@record)
-
-      @ep[:title].must_equal "Virginity & Fidelity"
-      @ep[:author_name].must_equal "The Moth"
+      eb[:title].must_equal "Virginity & Fidelity"
+      eb[:author_name].must_equal "The Moth"
     end
   end
 end
