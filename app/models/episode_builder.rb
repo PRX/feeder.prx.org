264f4d2cf7b1cfd7f828fa3b229a9241e0b6f169
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-10-07 11:09:19 -0400

    update how urls are audio are determined

--- app/models/episode_builder.rb
+++ app/models/episode_builder.rb
@@ -1,3 +1,5 @@
+require 'addressable/uri'
+require 'addressable/template'
 require 'prx_access'
 
 class EpisodeBuilder
@@ -14,49 +16,57 @@ class EpisodeBuilder
   end
 
   def from_prx_story
-    @story = get_story
-    sa = @story.attributes
-    HashWithIndifferentAccess.new(
-      guid:  "prx:#{@ep.story_id}:#{@ep.guid}",
-      link: link(@story),
-      title: sa[:title],
-      subtitle: Sanitize.fragment(sa[:shortDescription] || '').strip,
-      description: Sanitize.fragment(sa[:description] || '').strip,
-      summary: sa[:description],
-      content: sa[:description],
+    info = HashWithIndifferentAccess.new(
+      guid:  "prx:#{@ep.prx_uri || @ep.podcast_id }:#{@ep.guid}",
+      audio: audio_file,
       image_url: nil,
-      audio: audio_file(@ep),
-      explicit: sa[:contentAdvisory] ? 'yes' : 'no',
-      keywords: sa[:tags].join(', '),
-      categories: sa[:tags].join(', '),
-      created: @ep.created_at,
-      modified: @ep.updated_at
-    ).merge(@overrides)
-  end
+      created: @ep.created_at.utc.rfc2822,
+      modified: @ep.updated_at.utc.rfc2822
+    )
 
-  def audio_file(episode)
-    info = episode.enclosure_info
-    info[:url] = prefix_audio_url(info[:url])
+    if @story = get_story
+      sa = @story.attributes
+      story_info = HashWithIndifferentAccess.new(
+        link: link(@story),
+        title: sa[:title],
+        subtitle: Sanitize.fragment(sa[:shortDescription] || '').strip,
+        description: Sanitize.fragment(sa[:description] || '').strip,
+        summary: sa[:description],
+        content: sa[:description],
+        explicit: sa[:contentAdvisory] ? 'yes' : 'no',
+        keywords: sa[:tags],
+        categories: sa[:tags]
+      )
+      info.merge!(story_info)
+    end
+
+    info.merge!(@overrides) if @overrides
+
     info
   end
 
-  def prefix_audio_url(url)
-    return url if prefix.blank?
+  def audio_file(episode = @ep)
+    episode.enclosure_info.tap do |info|
+      info[:url] = rewrite_audio_url(info[:url])
+    end
+  end
 
-    link = url.sub(/^http(s*):\/\//, '/')
-    extension = URI.parse(url).path.split('.').pop
-    prefix + extension + link
+  def rewrite_audio_url(url)
+    et = @ep.enclosure_template
+    return url if et.blank?
+
+    url_info = Addressable::URI.parse(url).to_hash
+    url_info[:extension] = url_info[:path].split('.').pop
+    template = Addressable::Template.new(et)
+    template.expand(url_info).to_str
   end
 
   def get_story(account = nil)
+    return nil unless @prx_uri
     api(account: account).tap { |a| a.href = @prx_uri }.get
   end
 
   def link(story = @story)
-    "#{prx_root}#{story.id}"
-  end
-
-  def prefix
-    ENV['AUDIO_FILE_PREFIX'] || ''
+    Addressable::URI.join(prx_root, story.id.to_s) if story
   end
 end
