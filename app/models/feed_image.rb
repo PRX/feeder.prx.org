8971c2ad2a9a69f70132a3ca96b862a3f1a1c103
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-03 09:45:01 -0500

    cleanup unused validations

--- app/models/feed_image.rb
+++ app/models/feed_image.rb
@@ -2,9 +2,4 @@ class FeedImage < ActiveRecord::Base
   include ImageFile
 
   belongs_to :podcast
-
-  # validates :link, :title, presence: true
-
-  # validates :width, numericality: { less_than_or_equal_to: 144 }
-  # validates :height, numericality: { less_than_or_equal_to: 400 }
 end
