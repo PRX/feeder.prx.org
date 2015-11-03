b044f2f6a9c057b86e40e21f561675c3e69ea171
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-03 09:20:43 -0500

    update shoryuken, use configure_client

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,28 +1,24 @@
-require 'logger'
 require 'shoryuken'
 require 'shoryuken/extensions/active_job_adapter'
 
-Shoryuken.configure_server do |config|
-  # Replace Rails logger so messages are logged wherever Shoryuken is logging
-  # Note: this entire block is only run by the processor, so we don't overwrite
-  #       the logger when the app is running as usual.
-  Rails.logger = Shoryuken::Logging.logger
-  ActiveJob::Base.logger = Shoryuken::Logging.logger
-  ActiveRecord::Base.logger = Shoryuken::Logging.logger
-
-  # config.server_middleware do |chain|
-  #   chain.add Shoryuken::MyMiddleware
-  # end
-end
-
 Shoryuken.default_worker_options =  {
-  'queue'                   => (ENV['RAILS_ENV'] || 'development') + '_feeder_default',
+  'queue'                   => "#{Rails.env}_feeder_default",
   'auto_delete'             => true,
   'auto_visibility_timeout' => true,
   'batch'                   => false,
   'body_parser'             => :json
 }
 
-unless Rails.env.test?
-  Shoryuken::EnvironmentLoader.load(config_file: (Rails.root + 'config' + 'shoryuken.yml'))
+Shoryuken.configure_server do |config|
+  Rails.logger = Shoryuken::Logging.logger
+  ActiveJob::Base.logger = Shoryuken::Logging.logger
+  ActiveRecord::Base.logger = Shoryuken::Logging.logger
+end
+
+Shoryuken.configure_client do |config|
+  unless Rails.env.test?
+    config_file = File.join(Rails.root, 'config', 'shoryuken.yml')
+    Shoryuken::EnvironmentLoader.load(config_file: config_file)
+    Shoryuken::Client.account_id = Shoryuken.options[:aws][:account_id] || ENV['AWS_ACCOUNT_ID']
+  end
 end
