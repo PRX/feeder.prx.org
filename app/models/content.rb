9e5af5b18e00e446b1d2c4a49b935dc3392678d9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-10 14:05:02 -0500

    Save fixer result audio info

--- app/models/content.rb
+++ app/models/content.rb
@@ -11,15 +11,16 @@ class Content < MediaResource
   end
 
   def update_attributes_with_content(content)
-    %w( url medium expression bitrate framerate samplingrate
-      channels duration height width lang
-    ).each do |at|
+    %w( medium expression channels duration height width lang).each do |at|
       self.try("#{at}=", content[at])
     end
 
     self.original_url = content['url']
     self.mime_type    = content['type']
     self.file_size    = content['file_size'].to_i
+    self.bit_rate     = content['bitrate'].to_i
+    self.frame_rate   = content['framerate'].to_i
+    self.sample_rate  = content['samplingrate'].to_f * 1000
     self.is_default   = content['is_default'] && content['is_default'].downcase == 'true'
     self
   end
