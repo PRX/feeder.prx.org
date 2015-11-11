be6ffccec54416321882fa061347c6a600c17f8e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-11 13:43:23 -0500

    Fill out the rest of the audio file info

--- test/factories/media_resource_factory.rb
+++ test/factories/media_resource_factory.rb
@@ -3,6 +3,14 @@ FactoryGirl.define do
     association :episode, factory: :episode, prx_uri: '/api/v1/stories/80548'
     sequence(:guid) { |n| "ca047dce-9df5-4132-a04b-31d24c7c55a#{n}" }
 
+    mime_type 'audio/mpeg'
+    file_size 774059
+    medium 'audio'
+    sample_rate 44100
+    channels 2
+    duration 48
+    bit_rate 64
+
     after(:create) do |media_resource, evaluator|
       create_list(:copy_audio_task, 1, owner: media_resource)
     end
