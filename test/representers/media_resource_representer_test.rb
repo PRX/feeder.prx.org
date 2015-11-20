d489870ef41221c60580bb58fec305d677c12acd
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-20 09:42:40 -0500

    Add media resource representer

---
+++ test/representers/media_resource_representer_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+describe MediaResourceRepresenter do
+  let(:media_resource) { MediaResource.new }
+  let(:representer) { MediaResourceRepresenter.new(media_resource) }
+
+  it 'includes the href' do
+    media_resource.stub(:url, 'url') do
+      representer.as_json['href'].must_equal 'url'
+    end
+  end
+
+  it 'includes the type' do
+    media_resource.stub(:mime_type, 'audio/taco') do
+      representer.as_json['type'].must_equal 'audio/taco'
+    end
+  end
+
+  it 'includes the size' do
+    media_resource.stub(:file_size, 123456) do
+      representer.as_json['size'].must_equal 123456
+    end
+  end
+
+  it 'includes the duration' do
+    media_resource.stub(:duration, 1234) do
+      representer.as_json['duration'].must_equal 1234
+    end
+  end
+end
