5e45d1f6d82b5eb629872ec37d5fedd6bc419648
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-10-13 14:28:39 -0400

    Get rid of schema, fix entries, add default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
-    scope ':api_version', api_version: 'v1' do
-
-      root to: 'base#entrypoint'
-      match '*any', via: [:options], to: 'base#options'
-
+    scope ':api_version', api_version: 'v1', defaults: { format: 'hal' } do
       resources :podcasts, only: [:show, :index] do
         resources :episodes, only: [:show, :index]
       end
-      resources :entries, only: [:show, :index]
+      resources :episodes, only: [:show, :index]
+
+      root to: 'base#entrypoint'
+      match '*any', via: [:options], to: 'base#options'
     end
   end
 
@@ -17,5 +16,4 @@ Rails.application.routes.draw do
 
   resources :podcasts, only: [:show], defaults: { format: 'rss' }
   resources :episodes, only: [:show]
-  get "/schema", to: "schema#show", defaults: { format: 'json' }
 end
