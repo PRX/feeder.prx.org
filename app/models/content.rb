9eae0c13cad3327fab6ad4c2878dae38e76d68ed
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-10-18 16:19:21 -0400

    copy the content position

--- app/models/content.rb
+++ app/models/content.rb
@@ -14,17 +14,17 @@ class Content < MediaResource
   end
 
   def update_attributes_with_content(content)
-    %w( medium expression channels duration height width lang).each do |at|
+    %w(position medium expression channels duration height width lang).each do |at|
       self.try("#{at}=", content[at])
     end
 
     self.original_url = content['url']
-    self.mime_type    = content['type']
-    self.file_size    = content['file_size'].to_i
-    self.bit_rate     = content['bitrate'].to_i
-    self.frame_rate   = content['framerate'].to_i
-    self.sample_rate  = content['samplingrate'].to_f * 1000
-    self.is_default   = content['is_default'] && content['is_default'].downcase == 'true'
+    self.mime_type = content['type']
+    self.file_size = content['file_size'].to_i
+    self.bit_rate = content['bitrate'].to_i
+    self.frame_rate = content['framerate'].to_i
+    self.sample_rate = content['samplingrate'].to_f * 1000
+    self.is_default = content['is_default'] && content['is_default'].downcase == 'true'
     self
   end
 
