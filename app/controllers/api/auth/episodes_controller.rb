15992fedee36e6478d401a2178b7c295b780a81b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-11-22 11:50:46 -0500

    auth endpoints

--- app/controllers/api/auth/episodes_controller.rb
+++ app/controllers/api/auth/episodes_controller.rb
@@ -3,6 +3,8 @@
 class Api::Auth::EpisodesController < Api::EpisodesController
   include ApiAuthenticated
   api_versions :v1
+  represent_with Api::EpisodeRepresenter
+  find_method :find_by_guid
 
   def scoped(relation)
     relation
