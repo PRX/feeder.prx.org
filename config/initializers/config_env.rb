dc61f3cf359234eb6f90046e678af460e72f76c6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-13 12:21:20 -0500

    Update env vars

--- config/initializers/config_env.rb
+++ config/initializers/config_env.rb
@@ -1,14 +1,15 @@
 # default env vars that may not be set
-ENV['ID_ROOT'] ||= 'https://id.prx.org/'
-ENV['CMS_ROOT'] ||= 'https://cms.prx.org/api/v1/'
-ENV['PRX_ROOT'] ||= 'https://beta.prx.org/stories/'
-ENV['CRIER_ROOT'] ||= 'https://crier.prx.org/api/v1'
+ENV['CMS_HOST']    ||= 'cms.prx.org'
+ENV['CRIER_HOST']  ||= 'crier.prx.org'
+ENV['FEEDER_HOST'] ||= 'feeder.prx.org'
+ENV['ID_HOST']     ||= 'id.prx.org'
+ENV['META_HOST']   ||= 'meta.prx.org'
+ENV['PRX_HOST']    ||= 'www.prx.org'
+
+# default env vars that may not be set
 ENV['FEEDER_WEB_MASTER'] ||= 'prxhelp@prx.org (PRX)'
 ENV['FEEDER_GENERATOR'] ||= "PRX Feeder v#{Feeder::VERSION}"
 
 env_prefix = Rails.env.production? ? '' : (Rails.env + '-')
 ENV['FEEDER_CDN_HOST'] ||= "#{env_prefix}f.prxu.org"
 ENV['FEEDER_STORAGE_BUCKET'] ||= "#{env_prefix}prx-feed"
-
-env_suffix = Rails.env.development? ? 'dev' : 'org'
-ENV['FEEDER_APP_HOST'] ||= "feeder.prx.#{env_suffix}"
