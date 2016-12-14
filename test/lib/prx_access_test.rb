ee3fcce3ed5889bff993059a655539477b02eeac
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-14 16:13:02 -0500

    update prx access

--- test/lib/prx_access_test.rb
+++ test/lib/prx_access_test.rb
@@ -7,6 +7,7 @@ end
 describe PRXAccess do
   let(:prx_access) { PRXAccessTest.new }
   let(:crier_entry) { JSON.parse(json_file(:crier_entry)) }
+  let(:resource) { PRXAccess::PRXHyperResource.new }
 
   it 'returns an api' do
     prx_access.api.wont_be_nil
@@ -23,5 +24,15 @@ describe PRXAccess do
     res = prx_access.api_resource(crier_entry, prx_access.crier_root)
     res.wont_be_nil
     res.attributes['url'] = 'http://traffic.libsyn.com/test/test.mp3'
+  end
+
+  it 'underscores incoming hash keys' do
+    input = { 'camelCase' => 1 }
+    resource.incoming_body_filter(input)['camel_case'].must_equal 1
+  end
+
+  it 'underscores outgoing hash keys' do
+    input = { 'under_score' => 1 }
+    resource.outgoing_body_filter(input)['underScore'].must_equal 1
   end
 end
