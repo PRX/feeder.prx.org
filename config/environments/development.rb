af5e381c3fb12bac0a67f999eef923b43a5336e9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-15 11:19:07 -0500

    fix stuff I checked in by accident

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
