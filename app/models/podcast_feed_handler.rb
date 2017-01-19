37ef0607f465e44d5be4d4eebc002b4cdf26045f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-01-19 15:05:29 -0500

    separate code to have handlers for crier crier.feeds and cms.series

---
+++ app/models/podcast_feed_handler.rb
@@ -0,0 +1,99 @@
+require 'podcast'
+
+class PodcastFeedHandler
+  FEED_ATTRS = %w( complete copyright description explicit keywords language
+    subtitle summary title update_frequency update_period
+    author managing_editor new_feed_url owners ).freeze
+
+  attr_accessor :podcast, :feed
+
+  def initialize(podcast)
+    self.podcast = podcast
+  end
+
+  def self.create_from_feed!(feed)
+    update_from_feed!(Podcast.new, feed)
+  end
+
+  def self.update_from_feed!(podcast, feed = nil)
+    new(podcast).update_from_feed!(feed)
+  end
+
+  def update_from_feed!(feed)
+    Podcast.transaction do
+      podcast.lock!
+      update_from_feed(feed)
+      podcast.save!
+    end
+    podcast
+  end
+
+  def update_from_feed(feed)
+    self.feed = feed
+    update_attributes
+    update_images
+    update_categories
+  end
+
+  def update_attributes
+    fa = feed.attributes.with_indifferent_access
+    self.podcast.attributes = fa.slice(*FEED_ATTRS).with_indifferent_access
+
+    {feed_url: :source_url, url: :link}.each do |k,v|
+      self.podcast.send("#{v}=", fa[k.to_s])
+    end
+
+    self.podcast.path ||= fa['feedburner_name']
+  end
+
+  def update_images
+    { feed: :thumb_url, itunes: :image_url }.each do |type, url|
+      if feed.attributes[url]
+        save_image(podcast, type, feed.attributes[url])
+      elsif i = podcast.send("#{type}_image")
+        i.destroy
+      end
+    end
+  end
+
+  def save_image(podcast, type, url)
+    if i = podcast.send("#{type}_image")
+      i.update_attributes!(url: url)
+    else
+      podcast.send("build_#{type}_image", url: url)
+    end
+  end
+
+  def update_categories
+    itunes_cats = {}
+    cats = []
+    feed.categories.each do |cat|
+      if ITunesCategoryValidator.is_category?(cat)
+        itunes_cats[cat] ||= []
+      elsif parent_cat = ITunesCategoryValidator.is_subcategory?(cat)
+        itunes_cats[parent_cat] ||= []
+        itunes_cats[parent_cat] << cat
+      else
+        cats << cat
+      end
+    end
+
+    # delete and update existing itunes_categories
+    self.podcast.itunes_categories.each do |icat|
+      if itunes_cats.key?(icat.name)
+        subs = itunes_cats.delete(icat.name).sort.uniq
+        icat.update_attributes!(subcategories: subs)
+      else
+        icat.destroy!
+      end
+    end
+
+    # create missing itunes_categories
+    itunes_cats.keys.each do |cat|
+      subs = itunes_cats[cat].sort.uniq
+      self.podcast.itunes_categories.build(name: cat, subcategories: subs)
+    end
+
+    self.podcast.categories = cats
+  end
+end
---
+++ app/models/podcast_series_handler.rb
@@ -0,0 +1,89 @@
+require 'podcast'
+
+class PodcastSeriesHandler
+  include PRXAccess
+
+  attr_accessor :podcast, :series
+
+  def initialize(podcast)
+    self.podcast = podcast
+  end
+
+  def self.create_from_series!(series)
+    podcast = Podcast.new
+    update_from_series!(podcast, series)
+  end
+
+  def self.update_from_series!(podcast, series = nil)
+    series ||= get_series
+    new(podcast).update_from_series!(series)
+  end
+
+  def update_from_series!(series)
+    Podcast.transaction do
+      podcast.lock!
+      update_from_series(series)
+      podcast.save!
+    end
+    podcast
+  end
+
+  def update_from_series(series)
+    self.series = series
+    self.podcast.prx_uri = series.links['self'].href
+    self.podcast.prx_account_uri = series.links['account'].href
+
+    update_attributes
+    update_images
+  end
+
+  def update_attributes
+    sa = series.attributes
+    self.podcast.title = sa[:title]
+    self.podcast.subtitle = sa[:short_description]
+    self.podcast.description = sa[:description]
+    self.podcast.summary = sa[:description]
+    self.podcast.updated_at = Time.parse(sa[:updated_at]) if sa[:updated_at]
+    self.podcast.published_at ||= Time.now # always set this for series (for now)
+  end
+
+  def update_images
+    images = series.objects['prx:images'].objects['prx:items'] rescue []
+    { feed: 'thumbnail', itunes: 'profile' }.each do |type, purpose|
+      if image = images.detect { |i| i.attributes['purpose'] == purpose }
+        save_image(podcast, type, cms_url(image.links['original'].href))
+      elsif i = send("#{type}_image")
+        i.destroy
+      end
+    end
+  end
+
+  def save_image(podcast, type, url)
+    if i = podcast.send("#{type}_image")
+      i.update_attributes!(url: url)
+    else
+      podcast.send("build_#{type}_image", url: url)
+    end
+  end
+
+  def get_series(account = nil)
+    return nil unless podcast.prx_uri
+    api(account: account).tap { |a| a.href = podcast.prx_uri }.get
+  end
+
+  def cms_url(url)
+    if url =~ /^http/
+      url
+    else
+      path_to_url(ENV['CMS_HOST'], url)
+    end
+  end
+
+  def path_to_url(host, path)
+    if host =~ /\.org/ # TODO: should .tech's be here too?
+      URI::HTTPS.build(host: host, path: path).to_s
+    else
+      URI::HTTP.build(host: host, path: path).to_s
+    end
+  end
+end
