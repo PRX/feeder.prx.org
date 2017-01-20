241ab2de2857713aa74cf2ab521582b3e942fdc5
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-01-20 17:12:05 -0500

    test more attributes and images being set

--- test/models/podcast_series_handler_test.rb
+++ test/models/podcast_series_handler_test.rb
@@ -6,6 +6,8 @@ describe PodcastSeriesHandler do
 
   let(:podcast) { create(:podcast) }
 
+  let(:profile) { 'https://cms-staging.prx.tech/pub/d754c711890d7b7a57a43a432dd79137/0/web/series_image/15407/original/mothradiohr-whitelogo.jpg' }
+
   let(:series) do
     msg = json_file(:prx_series)
     body = JSON.parse(msg)
@@ -15,10 +17,20 @@ describe PodcastSeriesHandler do
     PRXAccess::PRXHyperResource.new_from(body: body, resource: resource, link: link)
   end
 
+  before {
+    stub_request(:get, profile).
+      to_return(status: 200, body: test_file('/fixtures/transistor1400.jpg'), headers: {})
+  }
+
   it 'can be created from a series' do
     podcast = PodcastSeriesHandler.create_from_series!(series)
     podcast.wont_be_nil
-    podcast.published_at.wont_be_nil
+    podcast.title.must_equal 'The Moth Radio Hour'
     podcast.description.must_match /^Brought to you by PRX/
+    podcast.summary.must_match /^Brought to you by PRX/
+    podcast.subtitle.must_match /^The Moth Radio Hour is a weekly series/
+
+    podcast.itunes_image.url.must_equal profile
+    podcast.feed_image.must_be_nil
   end
 end
