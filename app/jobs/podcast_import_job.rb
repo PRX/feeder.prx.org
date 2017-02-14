ff742c25d8bcb3720bac8dadeb8649a641771c01
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-02-14 15:22:04 -0500

    job test pass

--- app/jobs/podcast_import_job.rb
+++ app/jobs/podcast_import_job.rb
@@ -6,13 +6,7 @@ class PodcastImportJob < ActiveJob::Base
 
   def perform(account_uri, podcast_uri)
     ActiveRecord::Base.connection_pool.with_connection do
-      begin
-        import_podcast(account_uri, podcast_uri)
-      ensure
-        if reschedule
-          ReleaseEpisodesJob.set(wait: release_check_delay).perform_later(true)
-        end
-      end
+      import_podcast(account_uri, podcast_uri)
     end
   end
 
--- test/jobs/podcast_import_job_test.rb
+++ test/jobs/podcast_import_job_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 describe PodcastImportJob do
 
+  let(:account_path) { '/api/v1/accounts/8' }
+  let(:podcast_url) { 'http://feeds.prx.org/transistor_stem' }
   let(:job) { PodcastImportJob.new }
 
   it 'import that podcast' do
-    job.perform('', '')
+    job.stub(:import_podcast, true) do
+      job.perform(account_path, podcast_url).must_equal true
+    end
   end
 end
