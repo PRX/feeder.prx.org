be547ab1e018229ac35091d18d90de5a46c27e2f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-03-02 14:35:24 -0500

    hound line length

--- test/models/podcast_import_test.rb
+++ test/models/podcast_import_test.rb
@@ -45,9 +45,10 @@ describe PodcastImport do
     importer.series.account_id.wont_be_nil
     importer.series.title.must_equal 'Transistor'
     importer.series.short_description.must_equal 'A podcast of scientific questions and ' +
-      'stories featuring guest hosts and reporters.'
+                                                 'stories featuring guest hosts and reporters.'
     importer.series.description.must_equal 'A podcast of scientific questions and stories, ' +
-      'with many episodes hosted by key scientists at the forefront of discovery.'
+                                           'with many episodes hosted by key scientists at the ' +
+                                           'forefront of discovery.'
     importer.series.images.count.must_equal 2
     importer.series.audio_version_templates.count.must_equal 1
     template = importer.series.audio_version_templates.first
