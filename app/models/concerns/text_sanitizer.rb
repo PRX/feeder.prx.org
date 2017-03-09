d42a9d51d1f04ac456a6820455455844a1c3aef7
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-03-09 13:08:04 -0500

    add a sanitizer helper

---
+++ app/models/concerns/text_sanitizer.rb
@@ -0,0 +1,26 @@
+require 'active_support/concern'
+require 'loofah'
+
+module TextSanitizer
+  extend ActiveSupport::Concern
+
+  def sanitize_white_list(text)
+    return nil if text.blank?
+    sanitizer = Rails::Html::WhiteListSanitizer.new
+    sanitizer.sanitize(Loofah.fragment(text).scrub!(:prune).to_s)
+  end
+
+  def sanitize_links_only(text)
+    return nil if text.blank?
+    scrubber = Rails::Html::PermitScrubber.new
+    scrubber.tags = %w(a)
+    scrubber.attributes = %w(href target nofollow)
+    Loofah.fragment(text).scrub!(:prune).scrub!(scrubber).to_s
+  end
+
+  def sanitize_text_only(text)
+    return nil if text.blank?
+    sanitizer = Rails::Html::FullSanitizer.new
+    sanitizer.sanitize(text)
+  end
+end
