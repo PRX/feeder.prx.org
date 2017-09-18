498be5f1b82da04372b059db1495f3636ae5d8cb
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-09-18 13:17:00 -0400

    test for importing with different number of segments

--- test/models/podcast_import_test.rb
+++ test/models/podcast_import_test.rb
@@ -15,10 +15,8 @@ describe PodcastImport do
 
   let(:feed) { Feedjira::Feed.parse(test_file('/fixtures/transistor_two.xml')) }
   let(:series) { create(:series) }
-  let(:template) { create(:audio_version_template, series: series) }
   let(:distribution) do
-    build(:podcast_distribution,
-          audio_version_template: template,
+    create(:podcast_distribution,
           url: 'https://feeder.prx.org/api/v1/podcasts/51')
   end
 
@@ -37,7 +35,7 @@ describe PodcastImport do
     importer.set_config_url('http://test.prx.org/transistor_import_config.json')
     importer.config[:segments].must_equal 1
     importer.config[:program].must_equal 'transistor_stem'
-    importer.config[:audio]['https://transistor.prx.org/?p=1286'].count.must_equal 1
+    importer.config[:audio]['https://transistor.prx.org/?p=1286'].count.must_equal 2
   end
 
   it 'fails when feed is invalid' do
@@ -59,18 +57,13 @@ describe PodcastImport do
                                            'by radio and podcast powerhouse PRX, with support ' +
                                            'from the Sloan Foundation.'
     importer.series.images.count.must_equal 2
-    importer.series.audio_version_templates.count.must_equal 1
-    template = importer.series.audio_version_templates.first
-    template.audio_file_templates.count.must_equal 1
     importer.distribution.wont_be_nil
     importer.distribution.distributable.must_equal importer.series
-    importer.distribution.audio_version_template.must_equal template
   end
 
   it 'creates a podcast' do
     importer.feed = feed
     importer.series = series
-    importer.template = template
     importer.distribution = distribution.tap { |d| d.url = nil }
     importer.create_podcast
     importer.podcast.wont_be_nil
@@ -81,7 +74,7 @@ describe PodcastImport do
     importer.set_config_url('http://test.prx.org/transistor_import_config.json')
     importer.feed = feed
     importer.series = series
-    importer.template = template
+    series.audio_version_templates.clear
     importer.distribution = distribution
     importer.podcast = podcast
     stories = importer.create_stories
@@ -104,6 +97,10 @@ describe PodcastImport do
     version.explicit.must_be_nil
     l = stories.last
     l.images.count.must_equal 0
+    importer.series.audio_version_templates.count.must_equal 2
+    importer.distribution.audio_version_templates.count.must_equal 2
+    importer.templates[1].audio_file_templates.count.must_equal 1
+    importer.templates[2].audio_file_templates.count.must_equal 2
   end
 
   it 'imports a feed' do
