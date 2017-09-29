abd2fdee81388b4aa7ed89cd4d1b2af6e0c64e0e
gcampo88 <gigi.campo@gmail.com>
2017-09-29 11:15:30 -0400

    dont infer ep type

--- db/migrate/20170926210603_add_episode_tags.rb
+++ db/migrate/20170926210603_add_episode_tags.rb
@@ -4,8 +4,6 @@ class AddEpisodeTags < ActiveRecord::Migration
     add_column :episodes, :episode_number, :integer
     add_column :episodes, :itunes_type, :string, default: 'full'
     execute "UPDATE episodes SET itunes_type = 'full'"
-    execute "UPDATE episodes SET itunes_type = 'bonus' WHERE title LIKE '%bonus%'"
-    execute "UPDATE episodes SET itunes_type = 'trailer' WHERE title LIKE '%trailer%'"
   end
 
   def down
