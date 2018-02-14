0deb0a07ce88779608174b2b1f53bb5c3c416c4f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2018-02-14 09:58:13 -0500

    set the default for existing records

--- db/migrate/20180214144148_add_frozen_to_podcasts.rb
+++ db/migrate/20180214144148_add_frozen_to_podcasts.rb
@@ -1,5 +1,10 @@
 class AddFrozenToPodcasts < ActiveRecord::Migration
-  def change
+  def up
     add_column :podcasts, :frozen, :boolean, default: false
+    execute "UPDATE podcasts SET frozen = false"
+  end
+
+  def down
+    remove_column :podcasts, :frozen
   end
 end
