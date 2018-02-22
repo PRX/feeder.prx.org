2d8ac12ca99e3da5425c039b6fa5cfcb2cc6fff7
Andrew Kuklewicz <andrew@beginsinwonder.com>
2018-02-22 15:02:46 -0500

    refactor job error handling, include NR notice

--- app/jobs/episode_import_job.rb
+++ app/jobs/episode_import_job.rb
@@ -4,8 +4,6 @@ class EpisodeImportJob < ApplicationJob
   queue_as :cms_default
 
   def perform(episode_import)
-    ActiveRecord::Base.connection_pool.with_connection do
-      episode_import.import
-    end
+    episode_import.import
   end
 end
--- app/jobs/podcast_import_job.rb
+++ app/jobs/podcast_import_job.rb
@@ -1,12 +1,9 @@
 # encoding: utf-8
 
 class PodcastImportJob < ApplicationJob
-
   queue_as :cms_default
 
   def perform(podcast_import)
-    ActiveRecord::Base.connection_pool.with_connection do
-      podcast_import.import
-    end
+    podcast_import.import
   end
 end
