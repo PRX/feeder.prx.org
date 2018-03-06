4668bcc7711f30db2ee9aa685af61b02f283a082
gcampo88 <gigi.campo@gmail.com>
2018-03-06 18:22:40 -0500

    block syntax for around perform

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,15 +1,14 @@
 require 'newrelic_rpm'
 
 class ApplicationJob < ActiveJob::Base
-  around_perform :use_db_pool
 
   rescue_from(StandardError) do |e|
     NewRelic::Agent.notice_error(e)
   end
 
-  def use_db_pool(job)
+  around_perform do |job, block|
     ActiveRecord::Base.connection_pool.with_connection do
-      yield
+      block.call
     end
   end
 end
