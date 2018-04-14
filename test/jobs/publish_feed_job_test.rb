dabc74e170429da1533199440fca63cd777c5a54
Andrew Kuklewicz <andrew@beginsinwonder.com>
2018-04-14 09:58:05 -0400

    mark test tbd

--- test/jobs/publish_feed_job_test.rb
+++ test/jobs/publish_feed_job_test.rb
@@ -42,36 +42,9 @@ describe PublishFeedJob do
     rss[0, 38].must_equal '<?xml version="1.0" encoding="UTF-8"?>'
   end
 
-  # describe 'test writing file' do
-  #
-  #   let(:local_connection) do
-  #     opts = {
-  #       provider: 'Local',
-  #       local_root: File.join(Rails.root, 'tmp')
-  #     }
-  #     Fog::Storage.new(opts)
-  #   end
-  #
-  #   let(:local_file) { File.join(Rails.root, 'tmp', 'test-prx-feed', 'jjgo/feed-rss.xml') }
-  #
-  #   before {
-  #     FileUtils.rm_f(local_file)
-  #   }
-  #
-  #   it 'can save a podcast file' do
-  #     rss = '<?xml version="1.0" encoding="UTF-8"?>'
-  #     job.podcast = podcast
-  #     job.stub(:connection, local_connection) do
-  #       job.save_podcast_file(rss)
-  #     end
-  #     File.must_be :exists?, local_file
-  #   end
-  #
-  #   it 'can publish a podcast to storage' do
-  #     job.stub(:connection, local_connection) do
-  #       job.perform(podcast)
-  #     end
-  #     File.must_be :exists?, local_file
-  #   end
-  # end
+  describe 'test writing file' do
+    it 'can save a podcast file' do
+      raise "finish this"
+    end
+  end
 end
