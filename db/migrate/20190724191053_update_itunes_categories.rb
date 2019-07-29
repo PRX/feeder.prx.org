fda14ee5196eba6edf3517f8d454c4b92c672f54
Sandi Barr <sandi.barr@prx.org>
2019-07-29 11:12:38 -0500

    fix includes

--- db/migrate/20190724191053_update_itunes_categories.rb
+++ db/migrate/20190724191053_update_itunes_categories.rb
@@ -2,63 +2,63 @@ class UpdateITunesCategories < ActiveRecord::Migration
   def change
     ITunesCategories.find_each do |category|
       # 'Arts' sub 'Literature' -> 'Books'
-      if category.name == 'Arts' && category.subcategories.includes? = 'Literature'
+      if category.name == 'Arts' && category.subcategories.include? = 'Literature'
         category.subcategories[category.subcategories.index('Literature')] = 'Books'
         category.save!
       end
 
       # 'Business' sub 'Business News' -> 'News' sub 'Business News'
-      if category.name == 'Business' && category.subcategories.includes? = 'Business News'
+      if category.name == 'Business' && category.subcategories.include? = 'Business News'
         category.name = 'News'
         category.subcategories = ['Business News']
         category.save!
       end
 
       # 'Business' sub 'Management & Marketing' -> 'Management' and 'Marketing'
-      if category.name == 'Business' && category.subcategories.includes? = 'Management & Marketing'
+      if category.name == 'Business' && category.subcategories.include? = 'Management & Marketing'
         category.subcategories[category.subcategories.index('Management & Marketing')] = 'Management'
         category.subcategories.push('Marketing')
         category.save!
       end
 
       # 'Business' sub 'Shopping' discontinued
-      if category.name == 'Business' && category.subcategories.includes? = 'Shopping'
+      if category.name == 'Business' && category.subcategories.include? = 'Shopping'
         category.subcategories.delete_at(category.subcategories.index('Shopping'))
         category.save!
       end
 
       # 'Education' sub 'Educational Technology' discontinued
-      if category.name == 'Education' && category.subcategories.includes? = 'Educational Technology'
+      if category.name == 'Education' && category.subcategories.include? = 'Educational Technology'
         category.subcategories.delete_at(category.subcategories.index('Educational Technology'))
         category.save!
       end
 
       # 'Education' sub 'Higher Education' discontinued
-      if category.name == 'Education' && category.subcategories.includes? = 'Higher Education'
+      if category.name == 'Education' && category.subcategories.include? = 'Higher Education'
         category.subcategories.delete_at(category.subcategories.index('Higher Education'))
         category.save!
       end
 
       # 'Education' sub 'K-12' discontinued
-      if category.name == 'Education' && category.subcategories.includes? = 'K-12'
+      if category.name == 'Education' && category.subcategories.include? = 'K-12'
         category.subcategories.delete_at(category.subcategories.index('K-12'))
         category.save!
       end
 
       # 'Education' sub 'Language Courses' discontinued
-      if category.name == 'Education' && category.subcategories.includes? = 'Language Courses'
+      if category.name == 'Education' && category.subcategories.include? = 'Language Courses'
         category.subcategories.delete_at(category.subcategories.index('Language Courses'))
         category.save!
       end
 
       # 'Education' sub 'Training' discontinued
-      if category.name == 'Education' && category.subcategories.includes? = 'Training'
+      if category.name == 'Education' && category.subcategories.include? = 'Training'
         category.subcategories.delete_at(category.subcategories.index('Training'))
         category.save!
       end
 
       # 'Games & Hobbies' sub 'Other Games' discontinued
-      if category.name == 'Games & Hobbies' && category.subcategories.includes? = 'Other Games'
+      if category.name == 'Games & Hobbies' && category.subcategories.include? = 'Other Games'
         category.subcategories.delete_at(category.subcategories.index('Other Games'))
         category.save!
       end
@@ -70,26 +70,26 @@ class UpdateITunesCategories < ActiveRecord::Migration
       end
 
       # 'Government & Organizations' sub 'Local' discontinued
-      if category.name == 'Government & Organizations' && category.subcategories.includes? = 'Local'
+      if category.name == 'Government & Organizations' && category.subcategories.include? = 'Local'
         category.subcategories.delete_at(category.subcategories.index('Local'))
         category.save!
       end
 
       # 'Government & Organizations' sub 'National' discontinued
-      if category.name == 'Government & Organizations' && category.subcategories.includes? = 'National'
+      if category.name == 'Government & Organizations' && category.subcategories.include? = 'National'
         category.subcategories.delete_at(category.subcategories.index('National'))
         category.save!
       end
 
       # 'Government & Organizations' sub 'Non-Profit' -> 'Business' sub 'Non-Profit'
-      if category.name == 'Government & Organizations' && category.subcategories.includes? = 'Non-Profit'
+      if category.name == 'Government & Organizations' && category.subcategories.include? = 'Non-Profit'
         category.name = 'Business'
         category.subcategories = ['Non-Profit']
         category.save!
       end
 
       # 'Government & Organizations' sub 'Regional' discontinued
-      if category.name == 'Government & Organizations' && category.subcategories.includes? = 'Regional'
+      if category.name == 'Government & Organizations' && category.subcategories.include? = 'Regional'
         category.subcategories.delete_at(category.subcategories.index('Regional'))
         category.save!
       end
@@ -101,13 +101,13 @@ class UpdateITunesCategories < ActiveRecord::Migration
       end
 
       # 'Health' sub 'Self-Help' discontinued
-      if category.name == 'Health' && category.subcategories.includes? = 'Self-Help'
+      if category.name == 'Health' && category.subcategories.include? = 'Self-Help'
         category.subcategories.delete_at(category.subcategories.index('Self-Help'))
         category.save!
       end
       
       # 'Health' sub 'Fitness & Nutrition' -> 'Fitness' and 'Nutrition'
-      if category.name == 'Health' && category.subcategories.includes? = 'Fitness & Nutrition'
+      if category.name == 'Health' && category.subcategories.include? = 'Fitness & Nutrition'
         category.subcategories[category.subcategories.index('Fitness & Nutrition')] = 'Fitness'
         category.subcategories.push('Nutrition')
         category.save!
@@ -126,13 +126,13 @@ class UpdateITunesCategories < ActiveRecord::Migration
       end
 
       # 'Religion & Spirituality' sub 'Other' -> 'Religion'
-      if category.name == 'Religion & Spirituality' && category.subcategories.includes? = 'Other'
+      if category.name == 'Religion & Spirituality' && category.subcategories.include? = 'Other'
         category.subcategories[category.subcategories.index('Other')] = 'Religion'
         category.save!
       end
 
       # 'Science & Medicine' sub 'Medicine' -> 'Health & Fitness' sub 'Medicine'
-      if category.name == 'Science & Medicine' && category.subcategories.includes? = 'Medicine'
+      if category.name == 'Science & Medicine' && category.subcategories.include? = 'Medicine'
         category.name = 'Health & Fitness'
         category.subcategories = ['Medicine']
         category.save!
@@ -146,32 +146,32 @@ class UpdateITunesCategories < ActiveRecord::Migration
       end
 
       # 'Society & Culture' sub 'History' top-level 'History' no subs
-      if category.name == 'Society & Culture' && category.subcategories.includes? = 'History'
+      if category.name == 'Society & Culture' && category.subcategories.include? = 'History'
         category.name = 'History'
         category.subcategories = []
         category.save!
       end
 
       # 'Sports & Recreation' sub 'Amateur' discontinued
-      if category.name == 'Sports & Recreation' && category.subcategories.includes? = 'Amateur'
+      if category.name == 'Sports & Recreation' && category.subcategories.include? = 'Amateur'
         category.subcategories.delete_at(category.subcategories.index('Amateur'))
         category.save!
       end
 
       # 'Sports & Recreation' sub 'College & High School' discontinued
-      if category.name == 'Sports & Recreation' && category.subcategories.includes? = 'College & High School'
+      if category.name == 'Sports & Recreation' && category.subcategories.include? = 'College & High School'
         category.subcategories.delete_at(category.subcategories.index('College & High School'))
         category.save!
       end
 
       # 'Sports & Recreation' sub 'Outdoor' discontinued
-      if category.name == 'Sports & Recreation' && category.subcategories.includes? = 'Outdoor'
+      if category.name == 'Sports & Recreation' && category.subcategories.include? = 'Outdoor'
         category.subcategories.delete_at(category.subcategories.index('Outdoor'))
         category.save!
       end
 
       # 'Sports & Recreation' sub 'Professional' discontinued
-      if category.name == 'Sports & Recreation' && category.subcategories.includes? = 'Professional'
+      if category.name == 'Sports & Recreation' && category.subcategories.include? = 'Professional'
         category.subcategories.delete_at(category.subcategories.index('Professional'))
         category.save!
       end
@@ -183,26 +183,26 @@ class UpdateITunesCategories < ActiveRecord::Migration
       end
 
       # 'Technology' sub 'Gadgets' discontinued
-      if category.name == 'Technology' && category.subcategories.includes? = 'Gadgets'
+      if category.name == 'Technology' && category.subcategories.include? = 'Gadgets'
         category.subcategories.delete_at(category.subcategories.index('Gadgets'))
         category.save!
       end
 
       # 'Technology' sub 'Tech News' -> 'News' sub 'Tech News'
-      if category.name == 'Technology' && category.subcategories.includes? = 'Tech News'
+      if category.name == 'Technology' && category.subcategories.include? = 'Tech News'
         category.name = 'News'
         category.subcategories = ['Tech News']
         category.save!
       end
 
       # 'Technology' sub 'Podcasting' discontinued
-      if category.name == 'Technology' && category.subcategories.includes? = 'Podcasting'
+      if category.name == 'Technology' && category.subcategories.include? = 'Podcasting'
         category.subcategories.delete_at(category.subcategories.index('Podcasting'))
         category.save!
       end
 
       # 'Technology' sub 'Software How-To' discontinued
-      if category.name == 'Technology' && category.subcategories.includes? = 'Software How-To'
+      if category.name == 'Technology' && category.subcategories.include? = 'Software How-To'
         category.subcategories.delete_at(category.subcategories.index('Software How-To'))
         category.save!
       end
