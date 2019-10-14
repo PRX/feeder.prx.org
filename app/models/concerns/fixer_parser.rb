2a4a6abce4f9b472bed8d1e68b1b1f8ade69734b
cavis <ryancavis@gmail.com>
2019-10-14 11:00:51 -0600

    Test fixer parser

--- app/models/concerns/fixer_parser.rb
+++ app/models/concerns/fixer_parser.rb
@@ -5,46 +5,55 @@ module FixerParser
 
   class_methods do
     def fixer_callback_job_id(msg)
-      msg.try(:[], 'task').try(:[], 'job').try(:[], 'id')
+      fixer_task(msg).try(:[], :job).try(:[], :id)
     end
 
     def fixer_callback_status(msg)
-      fixer_result(msg).try(:[], 'status')
+      fixer_result(msg).try(:[], :status)
     end
 
     def fixer_callback_time(msg)
-      logged_at = fixer_result(msg).try(:[], 'logged_at')
+      logged_at = fixer_result(msg).try(:[], :logged_at)
       Time.parse(logged_at) if logged_at
     end
 
     def fixer_callback_info(msg)
-      fixer_result(msg).try(:[], 'info')
+      fixer_result(msg).try(:[], :info)
+    end
+
+    protected
+
+    def fixer_task(msg)
+      msg.try(:with_indifferent_access).try(:[], :task)
     end
 
     def fixer_result(msg)
-      msg.try(:[], 'task').try(:[], 'result_details')
+      fixer_task(msg).try(:[], :result_details)
     end
   end
 
   def fixer_callback_audio_meta
     info = self.class.fixer_callback_info(result)
-    mime = fixer_callback_mime(info)
-    {
-      mime_type: mime,
-      medium: mime.split('/').first,
-      file_size: info['size'].to_i,
-      sample_rate: info['sample_rate'].to_i,
-      channels: info['channels'].to_i,
-      duration: info['length'].to_f,
-      bit_rate: info['bit_rate'].to_i,
-    }
+    if info
+      mime = fixer_callback_mime(info)
+      {
+        mime_type: mime,
+        medium: mime.split('/').first,
+        file_size: info[:size].to_i,
+        sample_rate: info[:sample_rate].to_i,
+        channels: info[:channels].to_i,
+        duration: info[:length].to_f,
+        bit_rate: info[:bit_rate].to_i,
+      }
+    end
   end
 
   def fixer_callback_mime(info)
-    if info['content_type'].nil? || info['content_type'] == 'application/octect-stream'
+    content_type = info.with_indifferent_access[:content_type]
+    if content_type.blank? || content_type == 'application/octet-stream'
       'audio/mpeg'
     else
-      info['content_type']
+      content_type
     end
   end
 end
