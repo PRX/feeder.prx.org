22ac424caa694d778f62e36156f7cff4ea2611be
Chris Quamme Rhoden <carhoden@gmail.com>
2020-08-07 14:46:09 -0400

    Implement scope-based episode policy for write methods

--- test/policies/episode_policy_test.rb
+++ test/policies/episode_policy_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 describe EpisodePolicy do
   let(:account_id) { 123 }
-  let(:non_member_token) { StubToken.new(account_id + 1, ['no']) }
-  let(:member_token) { StubToken.new(account_id, ['member']) }
   let(:podcast) { build_stubbed(:podcast, prx_account_uri: "/api/v1/accounts/#{account_id}")}
   let(:episode) { build_stubbed(:episode, podcast: podcast)}
+
+  def token(scopes, set_account_id=account_id)
+    StubToken.new(set_account_id, scopes)
+  end
 
   describe '#update?' do
     it 'returns false if token is not present' do
@@ -13,11 +15,11 @@ describe EpisodePolicy do
     end
 
     it 'returns false if token is not a member of the account' do
-      EpisodePolicy.new(non_member_token, episode).wont_allow :update?
+      EpisodePolicy.new(token('feeder:episode', account_id + 1), episode).wont_allow :update?
     end
 
     it 'returns true if token is a member of the account' do
-      EpisodePolicy.new(member_token, episode).must_allow :update?
+      EpisodePolicy.new(token('feeder:episode'), episode).must_allow :update?
     end
   end
 end
