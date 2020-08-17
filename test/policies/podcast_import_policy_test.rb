015627745524e64d5bd90bc863d2210d922fcd6c
Chris Quamme Rhoden <carhoden@gmail.com>
2020-08-17 15:49:32 -0400

    PodcastImport requires both series and story scopes

---
+++ test/policies/podcast_import_policy_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+describe PodcastImportPolicy do
+  let(:account) { build_stubbed(:account, id: 1234) }
+  let(:import) { build(:podcast_import, account: account) }
+
+  def policy(scope)
+    PodcastImportPolicy.new(StubToken.new(account.id, scope), import)
+  end
+
+  it 'requires both story and series scopes' do
+    policy('cms:series cms:story').must_allow :create?
+    policy('cms:series cms:story').must_allow :update?
+
+    policy('cms:story').wont_allow :create?
+    policy('cms:series').wont_allow :create?
+  end
+end
