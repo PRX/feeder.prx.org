ffb4da8b3e7fff1b906e816cf10925863d78d0cd
cavis <ryancavis@gmail.com>
2021-02-04 12:05:17 -0700

    Cleanup with some consts

--- db/migrate/20210204184430_change_explicit_allowed_values.rb
+++ db/migrate/20210204184430_change_explicit_allowed_values.rb
@@ -1,24 +1,31 @@
 class ChangeExplicitAllowedValues < ActiveRecord::Migration
 
+  NOT_EXPLICITS = [nil, '', 'no', 'clean', 'false'].freeze
+  EXPLICITS = %w(yes explicit true).freeze
+
   # as of 2021-02-04, only true/false are allowed
   # https://help.apple.com/itc/podcasts_connect/#/itcb54353390
   def up
-    Podcast.where(explicit: [nil, '', 'no', 'clean']).update_all(explicit: 'false')
-    Podcast.where(explicit: %w(yes explicit)).update_all(explicit: 'true')
+    Podcast.where(explicit: NOT_EXPLICITS).update_all(explicit: 'false')
+    Podcast.where(explicit: EXPLICITS).update_all(explicit: 'true')
 
     # channel level explicit now required - error on anything else
     bad = Podcast.where.not(explicit: %w(true false))
-    raise "Bad podcast statuses: #{bad.inspect}" if bad.any?
+    raise "Bad podcast explicits: #{bad.inspect}" if bad.any?
 
     # set episodes where different than podcast
     Podcast.all.each do |p|
       if p.explicit == 'false'
-        p.episodes.where(explicit: [nil, '', 'no', 'clean', 'false']).update_all(explicit: nil)
-        p.episodes.where.not(explicit: [nil, '', 'no', 'clean', 'false']).update_all(explicit: 'true')
+        p.episodes.where(explicit: NOT_EXPLICITS).update_all(explicit: nil)
+        p.episodes.where(explicit: EXPLICITS).update_all(explicit: 'true')
       else
-        p.episodes.where(explicit: [nil, '', 'no', 'clean', 'false']).update_all(explicit: 'false')
-        p.episodes.where.not(explicit: [nil, '', 'no', 'clean', 'false']).update_all(explicit: nil)
+        p.episodes.where(explicit: NOT_EXPLICITS).update_all(explicit: 'false')
+        p.episodes.where(explicit: EXPLICITS).update_all(explicit: nil)
       end
     end
+
+    # allow nils this time, but nothing else
+    bad = Episode.where.not(explicit: [nil, 'true', 'false'])
+    raise "Bad episode explicits: #{bad.inspect}" if bad.any?
   end
 end
