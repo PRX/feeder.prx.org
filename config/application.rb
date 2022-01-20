e8b855f34a4d5d1967ff9573098c4b56e7915809
cavis <ryancavis@gmail.com>
2022-01-20 10:21:08 -0700

    Relocate validators

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,11 @@ module Feeder
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W( #{config.root}/app/representers/concerns #{config.root}/app/workers )
+    config.autoload_paths += %W(
+      #{config.root}/app/models/validators
+      #{config.root}/app/representers/concerns
+      #{config.root}/app/workers
+    )
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
