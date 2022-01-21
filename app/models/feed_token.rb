ec4849d316982fc88304ab9a0c286fde432f5917
cavis <ryancavis@gmail.com>
2022-01-21 10:57:14 -0700

    Generate tokens

--- app/models/feed_token.rb
+++ app/models/feed_token.rb
@@ -1,3 +1,12 @@
 class FeedToken < BaseModel
   belongs_to :feed
+
+  validates :token, presence: true, uniqueness: { scope: :feed_id }
+  validates_format_of :token, with: /\A[0-9a-zA-Z_.-]+\z/
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.token ||= SecureRandom.urlsafe_base64(20)
+  end
 end
