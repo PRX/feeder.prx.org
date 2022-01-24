16dd1526d548001806e86f69a11a988159d5fb69
cavis <ryancavis@gmail.com>
2022-01-24 11:42:31 -0700

    Basic authentication

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,6 @@
 class FeedsController < ApplicationController
+  before_filter :authenticate_feeds_token!
+
   def index
     max_updated_at = Feed.maximum(:updated_at)
 
@@ -11,6 +13,14 @@ class FeedsController < ApplicationController
   end
 
   private
+
+  def authenticate_feeds_token!
+    token = (request.headers['HTTP_AUTHORIZATION'] || '').split('Token ').last
+    feeds_token = ENV['FEEDS_TOKEN']
+    if token.blank? || feeds_token.blank? || token != feeds_token
+      render plain: 'No auth token provided', status: :unauthorized
+    end
+  end
 
   def podcasts_json(podcasts)
     podcasts.map { |p| [p.id, podcast_json(p)] }.to_h
