85a82ebfe73e91372b026407828c1c581feb3057
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-02-07 17:31:47 -0500

    Have the job make all the feeds using the feed builder

--- test/jobs/publish_feed_job_test.rb
+++ test/jobs/publish_feed_job_test.rb
@@ -4,6 +4,7 @@ describe PublishFeedJob do
 
   let(:episode) { create(:episode, prx_uri: "/api/v1/stories/87683") }
   let(:podcast) { episode.podcast }
+  let(:feed) { create(:feed, podcast: podcast, slug: 'adfree') }
 
   let(:job) { PublishFeedJob.new }
 
@@ -20,54 +21,26 @@ describe PublishFeedJob do
   end
 
   it 'knows the right key to write to' do
-    assert_equal job.key(podcast), 'jjgo/feed-rss.xml'
-  end
-
-  it 'can load the rss template' do
-    refute_nil job.rss_template
-    assert_equal job.rss_template[0,12], 'xml.instruct'
-  end
-
-  it 'can setup the data based on the podcast' do
-    job.setup_data(podcast)
-    assert_equal job.podcast, podcast
-    assert_equal job.episodes.count, 1
-  end
-
-  it 'can setup the data based on the podcast' do
-    job.podcast = podcast
-    job.episodes = podcast.feed_episodes
-    rss = job.generate_rss_xml
-    refute_nil rss
-    assert_equal rss[0, 38], '<?xml version="1.0" encoding="UTF-8"?>'
+    assert_equal job.key(podcast, podcast.default_feed), 'jjgo/feed-rss.xml'
+    assert_equal job.key(podcast, feed), 'jjgo/adfree/feed-rss.xml'
   end
 
   describe 'saving the rss file' do
     let (:stub_client) { Aws::S3::Client.new(stub_responses: true) }
 
     it 'can save a podcast file' do
-      job.podcast = podcast
       job.stub(:client, stub_client) do
-        rss = "<xml></xml>"
-        job.save_podcast_file(rss)
+        refute_nil job.save_file(podcast, podcast.default_feed)
+        refute_nil job.save_file(podcast, feed)
       end
     end
 
     it 'can process publishing a podcast' do
       job.stub(:client, stub_client) do
-        job.perform(podcast)
-        refute_nil job.rss
+        rss = job.perform(podcast)
+        refute_nil rss
         refute_nil job.put_object
         assert_nil job.copy_object
-      end
-    end
-
-    it 'makes an alias copy of the podcast file' do
-      podcast.default_feed.file_name = 'some-alias'
-      job.stub(:client, stub_client) do
-        job.perform(podcast)
-        refute_nil job.put_object
-        refute_nil job.copy_object
       end
     end
   end
