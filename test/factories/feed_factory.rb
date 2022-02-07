7e4c0aca18bcad6c5d40c1705673bfa2aced5163
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-02-07 17:39:20 -0500

    Add an audio_format and private attribute

--- test/factories/feed_factory.rb
+++ test/factories/feed_factory.rb
@@ -5,10 +5,13 @@ FactoryGirl.define do
 
     url 'http://feeds.feedburner.com/thornmorris'
     new_feed_url 'http://feeds.feedburner.com/newthornmorris'
+    audio_format { Hash(f: 'mp3', b: 128, c: 2, s: 44100) }
 
     factory :default_feed do
       slug nil
       file_name 'feed-rss.xml'
+      private false
+      audio_format { Hash(f: 'flac', b: 16, c: 2, s: 44100) }
     end
   end
 end
