6caa47a383e20db05ba8baea23150389cfbec11a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-03-18 10:34:46 -0400

    Trigger podcast publish when feed is updated

--- app/controllers/api/auth/feeds_controller.rb
+++ app/controllers/api/auth/feeds_controller.rb
@@ -2,4 +2,10 @@ class Api::Auth::FeedsController < Api::BaseController
   api_versions :v1
   represent_with Api::Auth::FeedRepresenter
   filter_resources_by :podcast_id
+
+  after_action :publish, only: [:create, :update, :destroy]
+
+  def publish
+    resource.podcast.publish! if resource && resource.podcast
+  end
 end
