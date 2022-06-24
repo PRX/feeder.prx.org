90154bdf76ca444cb8a417e8e52eda6bafdda8d6
Sam Vevang <sam.vevang@gmail.com>
2022-06-24 14:57:45 -0500

    fixup! Create an initial log structure

---
+++ app/models/sync_log.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class SyncLog < BaseModel
+  self.inheritance_column = nil
+
+  scope :complete, -> { where('sync_completed_at IS NOT NULL AND external_id IS NOT NULL') }
+
+  enum feeder_type: {
+    feeds: 'f'
+  }
+
+
+end
