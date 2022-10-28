644b0d28373c6109ca826880e101f914c25d268a
Sam Vevang <sam.vevang@gmail.com>
2022-10-28 13:49:55 -0500

    Remove opentelemetry gems

--- config/initializers/opentelemetry.rb
+++
@@ -1,7 +0,0 @@
-require 'opentelemetry/sdk'
-require 'opentelemetry/exporter/otlp'
-require 'opentelemetry/instrumentation/all'
-
-OpenTelemetry::SDK.configure do |c|
-    c.use_all() # enables all instrumentation!
-end
