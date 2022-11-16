de3cfedd54636543567434cadfe58851376c9f8b
Sam Vevang <sam.vevang@gmail.com>
2022-11-16 16:29:12 -0600

    Remove reference to local/other extension

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20220928174716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "episode_images", force: :cascade do |t|
     t.integer  "episode_id"
