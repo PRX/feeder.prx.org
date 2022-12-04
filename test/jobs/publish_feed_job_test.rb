a736fd9741ee2a86ab3050c066c5312fe5c21f97
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-12-03 23:40:25 -0500

    Get non-announce jobs working

--- test/jobs/publish_feed_job_test.rb
+++ test/jobs/publish_feed_job_test.rb
@@ -2,47 +2,41 @@ require 'test_helper'
 
 describe PublishFeedJob do
 
-  # let(:episode) { create(:episode, prx_uri: "/api/v1/stories/87683") }
-  # let(:podcast) { episode.podcast }
-  # let(:feed) { create(:feed, podcast: podcast, slug: 'adfree') }
+  let(:episode) { create(:episode, prx_uri: '/api/v1/stories/87683') }
+  let(:podcast) { episode.podcast }
+  let(:feed) { create(:feed, podcast: podcast, slug: 'adfree') }
 
-  # let(:job) { PublishFeedJob.new }
+  let(:job) { PublishFeedJob.new }
 
-  # # Delete - uses the test adapter now
-  # # it 'gets an aws client' do
-  # #   refute_nil job.client
-  # #   assert_instance_of Aws::S3::Client, job.client
-  # # end
+  it 'knows the right bucket to write to' do
+    assert_equal job.feeder_storage_bucket, 'test-prx-feed'
+    ENV['FEEDER_STORAGE_BUCKET'] = 'foo'
+    assert_equal job.feeder_storage_bucket, 'foo'
+    ENV['FEEDER_STORAGE_BUCKET'] = 'test-prx-feed'
+  end
 
-  # it 'knows the right bucket to write to' do
-  #   assert_equal job.feeder_storage_bucket, 'test-prx-feed'
-  #   ENV['FEEDER_STORAGE_BUCKET'] = 'foo'
-  #   assert_equal job.feeder_storage_bucket, 'foo'
-  #   ENV['FEEDER_STORAGE_BUCKET'] = 'test-prx-feed'
-  # end
+  it 'knows the right key to write to' do
+    assert_equal job.key(podcast, podcast.default_feed), 'jjgo/feed-rss.xml'
+    assert_equal job.key(podcast, feed), 'jjgo/adfree/feed-rss.xml'
+  end
 
-  # it 'knows the right key to write to' do
-  #   assert_equal job.key(podcast, podcast.default_feed), 'jjgo/feed-rss.xml'
-  #   assert_equal job.key(podcast, feed), 'jjgo/adfree/feed-rss.xml'
-  # end
+  describe 'saving the rss file' do
+    let (:stub_client) { Aws::S3::Client.new(stub_responses: true) }
 
-  # describe 'saving the rss file' do
-  #   let (:stub_client) { Aws::S3::Client.new(stub_responses: true) }
+    it 'can save a podcast file' do
+      job.stub(:client, stub_client) do
+        refute_nil job.save_file(podcast, podcast.default_feed)
+        refute_nil job.save_file(podcast, feed)
+      end
+    end
 
-  #   it 'can save a podcast file' do
-  #     job.stub(:client, stub_client) do
-  #       refute_nil job.save_file(podcast, podcast.default_feed)
-  #       refute_nil job.save_file(podcast, feed)
-  #     end
-  #   end
-
-  #   it 'can process publishing a podcast' do
-  #     job.stub(:client, stub_client) do
-  #       rss = job.perform(podcast)
-  #       refute_nil rss
-  #       refute_nil job.put_object
-  #       assert_nil job.copy_object
-  #     end
-  #   end
-  # end
+    it 'can process publishing a podcast' do
+      job.stub(:client, stub_client) do
+        rss = job.perform(podcast)
+        refute_nil rss
+        refute_nil job.put_object
+        assert_nil job.copy_object
+      end
+    end
+  end
 end
