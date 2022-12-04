5f4a682f5ed5b6e8ca1717a08769cee1113cf37f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-12-04 17:42:40 -0500

    Get the tests running as well

--- test/workers/application_worker_test.rb
+++ test/workers/application_worker_test.rb
@@ -3,10 +3,18 @@ require 'test_helper'
 describe ApplicationWorker do
 
   let(:worker) { ApplicationWorker.new }
+  let(:msg) { { subject: :foo, action: :bar } }
 
   it 'adds queue name prefix' do
+    assert_equal ApplicationWorker.prefix_name('foo'), 'test_feeder_foo'
   end
 
   it 'lists announce queues' do
+    assert_equal ApplicationWorker.announce_queues('foo', ['bar', 'wat']),
+                 ['test_announce_feeder_foo_bar', 'test_announce_feeder_foo_wat']
+  end
+
+  it 'determines the delegate method' do
+    assert_equal worker.delegate_method(msg), 'receive_foo_bar'
   end
 end
--- test/workers/story_update_worker_test.rb
+++ test/workers/story_update_worker_test.rb
@@ -56,7 +56,7 @@ describe StoryUpdateWorker do
               lbd = episode.podcast.last_build_date
               uat = episode.updated_at
               bod = JSON.parse(real_story_update)
-              worker.perform(nil, {subject: 'story', action: 'update', body: bod})
+              worker.perform(nil, { subject: 'story', action: 'update', body: bod })
               assert_equal worker.episode.prx_uri, "/api/v1/stories/#{real_story_id}"
               assert_operator worker.episode.podcast.last_build_date, :>, lbd
               assert_operator worker.episode.updated_at, :>, uat
@@ -67,96 +67,94 @@ describe StoryUpdateWorker do
     end
   end
 
-  # it 'will not update a deleted episode' do
-  #   episode = create(:episode, prx_uri: "/api/v1/stories/#{prx_story_id}", podcast: podcast, deleted_at: Time.now)
-  #   assert episode.deleted?
-  #   podcast.stub(:copy_media, true) do
-  #     episode.stub(:copy_media, true) do
-  #       episode.stub(:podcast, podcast) do
-  #         Episode.stub(:by_prx_story, episode) do
-  #           worker.stub(:get_account_token, 'token') do
-  #             worker.perform(subject: 'story', action: 'update', body: JSON.parse(prx_story_update))
-  #             assert worker.episode.deleted?
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
+  it 'will not update a deleted episode' do
+    episode = create(:episode, prx_uri: "/api/v1/stories/#{prx_story_id}", podcast: podcast, deleted_at: Time.now)
+    assert episode.deleted?
+    podcast.stub(:copy_media, true) do
+      episode.stub(:copy_media, true) do
+        episode.stub(:podcast, podcast) do
+          Episode.stub(:by_prx_story, episode) do
+            worker.stub(:get_account_token, 'token') do
+              worker.perform(nil, { subject: 'story', action: 'update', body: JSON.parse(prx_story_update) })
+              assert worker.episode.deleted?
+            end
+          end
+        end
+      end
+    end
+  end
 
-  # it 'can delete an episode' do
-  #   episode = create(:episode, prx_uri: "/api/v1/stories/99999", podcast: podcast)
-  #   Episode.stub(:by_prx_story, episode) do
-  #     episode.stub(:podcast, podcast) do
-  #       worker.stub(:get_account_token, 'token') do
-  #         worker.perform(subject: 'story', action: 'delete', body: JSON.parse(prx_story_deleted))
-  #         refute_nil worker.episode.deleted_at
-  #       end
-  #     end
-  #   end
-  # end
+  it 'can delete an episode' do
+    episode = create(:episode, prx_uri: '/api/v1/stories/99999', podcast: podcast)
+    Episode.stub(:by_prx_story, episode) do
+      episode.stub(:podcast, podcast) do
+        worker.stub(:get_account_token, 'token') do
+          worker.perform(nil, { subject: 'story', action: 'delete', body: JSON.parse(prx_story_deleted) })
+          refute_nil worker.episode.deleted_at
+        end
+      end
+    end
+  end
 
-  # describe 'with an invalid story' do
-  #   let(:invalid_story_update) { json_file(:prx_invalid_story_updates) }
-  #   let(:invalid_story_id) { 149727 }
+  describe 'with an invalid story' do
+    let(:invalid_story_update) { json_file(:prx_invalid_story_updates) }
+    let(:invalid_story_id) { 149727 }
 
-  #   let(:invalid_update_msg) do
-  #     {
-  #       subject: 'story',
-  #       action: 'update',
-  #       body: invalid_story_update,
-  #       sent_at: 1.second.ago
-  #     }
-  #   end
-  #   let(:invalid_update_job) do
-  #     StoryUpdateWorker.new.tap do |j|
-  #       j.message = invalid_update_msg
-  #       j.subject = invalid_update_msg[:subject]
-  #       j.action = invalid_update_msg[:action]
-  #     end
-  #   end
+    let(:invalid_update_msg) do
+      {
+        subject: 'story',
+        action: 'update',
+        body: invalid_story_update,
+        sent_at: 1.second.ago
+      }
+    end
+    let(:invalid_update_job) do
+      StoryUpdateWorker.new.tap do |j|
+        j.message = invalid_update_msg
+      end
+    end
 
-  #   before do
-  #     stub_request(:get, "https://cms.prx.org/api/v1/authorization/stories/#{invalid_story_id}").
-  #       with(headers: { 'Accept' => 'application/json' } ).
-  #       to_return(status: 200, body: invalid_story_update, headers: {})
-  #   end
+    before do
+      stub_request(:get, "https://cms.prx.org/api/v1/authorization/stories/#{invalid_story_id}").
+        with(headers: { 'Accept' => 'application/json' }).
+        to_return(status: 200, body: invalid_story_update, headers: {})
+    end
 
-  #   it 'wont accept an invalid story on update' do
-  #     episode = create(:episode, prx_uri: "/api/v1/stories/#{invalid_story_id}", podcast: podcast)
-  #     episode.stub(:copy_media, true) do
-  #       episode.stub(:podcast, podcast) do
-  #         Episode.stub(:by_prx_story, episode) do
-  #           worker.stub(:get_account_token, 'token') do
-  #             lbd = episode.podcast.last_build_date
-  #             uat = episode.updated_at
-  #             worker.perform(subject: 'story', action: 'update', body: invalid_story_update)
-  #             refute_operator episode.podcast.last_build_date, :>, lbd
-  #             refute_operator episode.updated_at, :>, uat
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
+    it 'wont accept an invalid story on update' do
+      episode = create(:episode, prx_uri: "/api/v1/stories/#{invalid_story_id}", podcast: podcast)
+      episode.stub(:copy_media, true) do
+        episode.stub(:podcast, podcast) do
+          Episode.stub(:by_prx_story, episode) do
+            worker.stub(:get_account_token, 'token') do
+              lbd = episode.podcast.last_build_date
+              uat = episode.updated_at
+              worker.perform(nil, { subject: 'story', action: 'update', body: invalid_story_update })
+              refute_operator episode.podcast.last_build_date, :>, lbd
+              refute_operator episode.updated_at, :>, uat
+            end
+          end
+        end
+      end
+    end
 
-  #   it 'will accept an invalid story on unpublish' do
-  #     episode = create(:episode, prx_uri: "/api/v1/stories/#{invalid_story_id}", podcast: podcast)
-  #     episode.stub(:copy_media, true) do
-  #       episode.stub(:podcast, podcast) do
-  #         episode.podcast.stub(:copy_media, true) do
-  #           Episode.stub(:by_prx_story, episode) do
-  #             worker.stub(:get_account_token, 'token') do
-  #               lbd = episode.podcast.last_build_date
-  #               uat = episode.updated_at
-  #               worker.perform(subject: 'story', action: 'unpublish', body: invalid_story_update)
-  #               refute worker.episode.published?
-  #               assert_operator worker.episode.podcast.last_build_date, :>, lbd
-  #               assert_operator worker.episode.updated_at, :>, uat
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
+    it 'will accept an invalid story on unpublish' do
+      episode = create(:episode, prx_uri: "/api/v1/stories/#{invalid_story_id}", podcast: podcast)
+      episode.stub(:copy_media, true) do
+        episode.stub(:podcast, podcast) do
+          episode.podcast.stub(:copy_media, true) do
+            Episode.stub(:by_prx_story, episode) do
+              worker.stub(:get_account_token, 'token') do
+                lbd = episode.podcast.last_build_date
+                uat = episode.updated_at
+                worker.perform(nil, { subject: 'story', action: 'unpublish', body: invalid_story_update })
+                refute worker.episode.published?
+                assert_operator worker.episode.podcast.last_build_date, :>, lbd
+                assert_operator worker.episode.updated_at, :>, uat
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
