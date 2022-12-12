99868bf956cf430186ac6f024d4618ea0f9ce4c8
Alexa Macias <alexamacias192@gmail.com>
2022-12-12 16:00:30 -0500

    Redo previous migration to include apple_provider_id in apple_credentials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221010175311) do
+ActiveRecord::Schema.define(version: 20220928174716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,9 +51,8 @@ ActiveRecord::Schema.define(version: 20221010175311) do
     t.integer  "podcast_delivery_id"
     t.string   "external_id"
     t.string   "api_response"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "uploaded",            default: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   add_index "apple_podcast_delivery_files", ["external_id"], name: "index_apple_podcast_delivery_files_on_external_id", unique: true, using: :btree
