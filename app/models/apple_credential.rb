b9e2f77fcd778a94b100e4be54ff4f8c96264fb3
Alexa M <88990394+a-maci29@users.noreply.github.com>
2022-12-14 14:36:36 -0500

    Feat/validate and test apple credentials (#486)
    * Validation and test progress
    
    * Refactor and tests

--- app/models/apple_credential.rb
+++ app/models/apple_credential.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AppleCredential < ActiveRecord::Base
+class AppleCredential < BaseModel
   belongs_to :public_feed, class_name: "Feed"
   belongs_to :private_feed, class_name: "Feed"
 
@@ -8,9 +8,14 @@ class AppleCredential < ActiveRecord::Base
   validates_presence_of :private_feed
   validates_associated :public_feed
   validates_associated :private_feed
-  validates_presence_of :apple_key_id
-  validates_presence_of :apple_key_pem_b64
+  validates_presence_of :apple_provider_id, if: :any_apple_credentials_exist?
+  validates_presence_of :apple_key_id, if: :any_apple_credentials_exist?
+  validates_presence_of :apple_key_pem_b64, if: :any_apple_credentials_exist?
   validates :public_feed, uniqueness: { scope: :private_feed,
                                         message: "can only have one credential per public and private feed" }
   validates :public_feed, exclusion: { in: ->(apple_credential) { [apple_credential.private_feed] } }
+
+  def any_apple_credentials_exist?
+    apple_provider_id.present? || apple_key_id.present? || apple_key_pem_b64.present?
+  end
 end
