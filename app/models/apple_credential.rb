cb9dfe04930cd0e0dd50755f5134ade160f2277f
Sam Vevang <sam.vevang@gmail.com>
2022-12-20 13:36:57 -0600

    Linting

--- app/models/apple_credential.rb
+++ app/models/apple_credential.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class AppleCredential < ApplicationRecord
-  belongs_to :public_feed, class_name: "Feed"
-  belongs_to :private_feed, class_name: "Feed"
+  belongs_to :public_feed, class_name: 'Feed'
+  belongs_to :private_feed, class_name: 'Feed'
 
   validates_presence_of :public_feed
   validates_presence_of :private_feed
@@ -13,7 +13,7 @@ class AppleCredential < ApplicationRecord
   validates_presence_of :apple_key_id, if: :any_apple_credentials_exist?
   validates_presence_of :apple_key_pem_b64, if: :any_apple_credentials_exist?
   validates :public_feed, uniqueness: { scope: :private_feed,
-                                        message: "can only have one credential per public and private feed" }
+                                        message: 'can only have one credential per public and private feed' }
   validates :public_feed, exclusion: { in: ->(apple_credential) { [apple_credential.private_feed] } }
 
   def any_apple_credentials_exist?
