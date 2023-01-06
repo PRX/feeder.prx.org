382385921e753f62f73134aa8cf8ae900f99db9b
cavis <ryancavis@gmail.com>
2023-01-06 09:39:18 -0700

    Add top level links, because, why not

--- app/representers/api/authorization_representer.rb
+++ app/representers/api/authorization_representer.rb
@@ -22,6 +22,17 @@ class Api::AuthorizationRepresenter < Api::BaseRepresenter
     }
   end
 
+  links :guid do
+    [
+      {
+        title:     "Get a single episode by item guid",
+        profile:   profile_url(:episode),
+        href:      api_authorization_podcast_guid_path_template(api_version: represented.version, podcast_id: '{id}', id: '{guid}'),
+        templated: true
+      }
+    ]
+  end
+
   link :podcasts do
     {
       title: 'Get a paged collection of authorized podcasts',
