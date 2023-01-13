1262b8665a1566e3067d8552bca3e2274e1b65ce
cavis <ryancavis@gmail.com>
2023-01-13 13:41:26 -0700

    Add production_notes to episodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_203436) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_203943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,6 +123,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_203436) do
     t.string "prx_audio_version_uri"
     t.string "audio_version"
     t.integer "segment_count"
+    t.text "production_notes"
     t.index ["guid"], name: "index_episodes_on_guid", unique: true
     t.index ["keyword_xid"], name: "index_episodes_on_keyword_xid", unique: true
     t.index ["original_guid", "podcast_id"], name: "index_episodes_on_original_guid_and_podcast_id", unique: true, where: "((deleted_at IS NULL) AND (original_guid IS NOT NULL))"
