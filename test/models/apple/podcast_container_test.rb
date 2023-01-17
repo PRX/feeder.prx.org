PRX/feeder.prx.org

commit b783d2977211686ad167cfbe91b2c2c558e679bd

Feeder episode has one apple podcast container

app/models/apple/podcast_container.rb
@@ -102,7 +102,6 @@ module Apple
 
     def self.get_urls_for_episode_podcast_containers(api, episode_podcast_containers_json)
       containers_json = episode_podcast_containers_json['api_response']['val']['data']
-      # TODO: support > 1 podcast container per feeder episode
       raise "Unsupported number of podcast containers for episode: #{ep.feeder_id}" if containers_json.length > 1
 
       containers_json.map do |podcast_container_json|

app/models/apple/episode.rb
@@ -251,8 +251,7 @@ module Apple
     end
 
     def podcast_container
-      # TODO: differentiate these by container type: audio versus images
-      Apple::PodcastContainer.find_by(episode_id: feeder_episode.id)
+      feeder_episode.podcast_container
     end
 
     def podcast_deliveries

app/models/apple/podcast_delivery.rb
@@ -36,7 +36,7 @@ module Apple
     end
 
     def self.create_podcast_deliveries(api, episodes)
-      # TODO: Support multiple containers per episode
+      # TODO: Support multiple deliveries per episode
       podcast_containers = episodes.map(&:podcast_container)
 
       podcast_containers = podcast_containers.reject do |container|

app/models/apple/podcast_delivery_file.rb
@@ -73,7 +73,6 @@ module Apple
     def self.create_podcast_delivery_files(api, episodes)
       return [] if episodes.empty?
 
-      # TODO: handle multiple containers
       podcast_containers = episodes.map(&:podcast_container)
       podcast_deliveries = podcast_containers.map do |pc|
         raise('Missing podcast deliveries') if pc.podcast_deliveries.empty?

app/models/apple/publisher.rb
@@ -88,9 +88,9 @@ module Apple
     end
 
     def sync_podcast_containers!
-      # TODO: right now we only create one container,
-      # Apple RSS scaping means we don't need containers for freemium episode images
-      # But we do need asset containers for apple-only (non-rss) images
+      # TODO: right now we only create one delivery per container,
+      # Apple RSS scaping means we don't need deliveries for freemium episode images
+      # But we do need asset deliveries for apple-only (non-rss) images
 
       Rails.logger.info('Starting podcast container sync')
 

app/models/episode.rb
@@ -35,11 +35,11 @@ class Episode < ApplicationRecord
            -> { order('created_at DESC') },
            autosave: true, dependent: :destroy
 
-  has_many :apple_podcast_containers, class_name: "Apple::PodcastContainer"
-  has_many :apple_podcast_deliveries, through: :apple_podcast_containers, source: :podcast_deliveries,
-                                      class_name: "Apple::PodcastDelivery"
+  has_one :apple_podcast_container, class_name: 'Apple::PodcastContainer'
+  has_many :apple_podcast_deliveries, through: :apple_podcast_container, source: :podcast_deliveries,
+                                      class_name: 'Apple::PodcastDelivery'
   has_many :apple_podcast_delivery_files, through: :apple_podcast_deliveries, source: :podcast_delivery_files,
-                                          class_name: "Apple::PodcastDeliveryFile"
+                                          class_name: 'Apple::PodcastDeliveryFile'
 
   validates :podcast_id, :guid, presence: true
   validates :original_guid, uniqueness: { scope: :podcast_id, allow_nil: true }

commit 446f2f950d5615d948ae11e2fb2e2d2fd9698bbe

Make sure to reload the episode attribute upon upsert
Fixes caching problem introduced with b783d2977211686ad167cfbe91b2c2c558e679bd

app/models/apple/podcast_container.rb
@@ -75,6 +75,9 @@ module Apple
                   episode_id: episode.feeder_id,)
         end
 
+      # reset the episode's podcast container cached value
+      episode.feeder_episode.reload_apple_podcast_container
+
       SyncLog.create!(feeder_id: pc.id, feeder_type: :podcast_containers, external_id: external_id)
 
       pc

test/models/apple/podcast_container_test.rb
@@ -46,6 +46,26 @@ class Apple::PodcastContainerTest < ActiveSupport::TestCase
       end
     end
 
+    it 'should reload the feeder episodes apple_podcast_container attribute' do
+      apple_episode.stub(:apple_id, apple_episode_id) do
+        apple_episode.stub(:audio_asset_vendor_id, apple_audio_asset_vendor_id) do
+
+          # show that the call to upsert_podcast_container will reload the
+          # feeder episode's apple_podcast_container attribute
+
+          # Set the apple_podcast_container to a value that is not the pc1 result
+          apple_episode.feeder_episode.build_apple_podcast_container(vendor_id: 'not this one!')
+          assert_equal 'not this one!', apple_episode.podcast_container.vendor_id
+
+          pc1 = Apple::PodcastContainer.upsert_podcast_container(apple_episode,
+                                                                 podcast_container_json_row)
+          # The apple_podcast_container should be reloaded
+          # it reflects the stubbed apple_audio_assed_vendor_id, now upserted as a col in pc1
+          assert_equal 'apple-vendor-id', apple_episode.podcast_container.vendor_id
+        end
+      end
+    end
+
     it 'should update timestamps when upserting' do
       apple_episode.stub(:apple_id, apple_episode_id) do
         apple_episode.stub(:audio_asset_vendor_id, apple_audio_asset_vendor_id) do