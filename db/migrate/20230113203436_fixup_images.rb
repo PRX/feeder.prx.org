d564c0df881f45aed64bda03510ef3db4a4c64ef
cavis <ryancavis@gmail.com>
2023-01-17 13:08:51 -0700

    Also remove link

--- db/migrate/20230113203436_fixup_images.rb
+++ db/migrate/20230113203436_fixup_images.rb
@@ -4,8 +4,11 @@ class FixupImages < ActiveRecord::Migration[7.0]
       add_column tbl, :alt_text, :string
       add_column tbl, :caption, :string
       add_column tbl, :credit, :string
+
+      # these are all blank, and not useful in the RSS
       remove_column tbl, :title, :string
       remove_column tbl, :description, :string
+      remove_column tbl, :link, :string
     end
   end
 end
