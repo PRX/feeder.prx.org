a368ba1d6a8adc4291cb9b6050e130d1c0d32437
cavis <ryancavis@gmail.com>
2023-01-20 12:32:49 -0700

    Debugging turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+
+// debugging turbo
+document.addEventListener('turbo:frame-missing', function(event) {
+  alert('FRAME MISSING!!!!!!!!!!!!!')
+  // event.detail.response.text().then(
+  //   text => console.log('GOT TEXT:', text),
+  //   err => console.log('GOT ERR:', err)
+  // )
+})
