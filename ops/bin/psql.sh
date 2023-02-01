94c920b363ea6ca4515c880f01b824c340f767de
Sam Vevang <sam.vevang@gmail.com>
2023-02-01 12:18:31 -0600

    Make sure that pg_restore has creds

--- ops/bin/psql.sh
+++ ops/bin/psql.sh
@@ -4,8 +4,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/_env.sh
 
-export PGPASSWORD=$POSTGRES_PASSWORD
-export PGUSER=$POSTGRES_USER
-export PGHOST=$POSTGRES_HOST
-
 psql feeder_development
