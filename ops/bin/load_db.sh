50c4ec3355ef65cef957c82ea04de82d301f8e38
Sam Vevang <sam.vevang@gmail.com>
2023-02-02 09:36:36 -0600

    Lint shell code

--- ops/bin/load_db.sh
+++ ops/bin/load_db.sh
@@ -4,14 +4,14 @@ set -u
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $DIR/_env.sh
+. "$DIR/_env.sh"
 
 load_db () {
   # drop all the open connections
   echo "REVOKE CONNECT ON DATABASE $POSTGRES_DATABASE FROM public;" | psql
   echo "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$POSTGRES_DATABASE' AND pid <> pg_backend_pid();" | psql
 
-  dropdb $POSTGRES_DATABASE || true
+  dropdb "$POSTGRES_DATABASE" || true
 
   echo "CREATE DATABASE $POSTGRES_DATABASE WITH TEMPLATE $CLONE_DB_NAME OWNER $POSTGRES_USER;" | time psql
 }
--- ops/bin/psql.sh
+++ ops/bin/psql.sh
@@ -2,6 +2,6 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $DIR/_env.sh
+. "$DIR/_env.sh"
 
 psql feeder_development
