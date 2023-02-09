d1a7718152cbcb2f707f8a118055ef52b1258dbc
cavis <ryancavis@gmail.com>
2023-02-09 13:47:50 -0700

    Optional debounce

--- app/javascript/controllers/dynamic_form_controller.js
+++ app/javascript/controllers/dynamic_form_controller.js
@@ -2,11 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["link"]
+  static values = { debounce: Number }
 
   change(event) {
+    if (this.debounceValue) {
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => this.clickLinks(event.target), this.debounceValue)
+    } else {
+      this.clickLinks(event.target)
+    }
+  }
+
+  clickLinks(element) {
     for (const link of this.linkTargets) {
       const oldHref = link.href
-      link.search = new URLSearchParams({ [event.target.name]: event.target.value })
+      link.search = new URLSearchParams({ [element.name]: element.value })
       if (link.href !== oldHref) {
         link.click()
       }
