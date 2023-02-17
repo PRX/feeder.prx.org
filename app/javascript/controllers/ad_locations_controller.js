62aa9a631a907dbf24c667b6b61b150d3a92d4c1
Rick Peterman <rick.peterman@prx.org>
2023-02-17 14:16:41 -0600

    feat(ad-locations): add route and controllers
    - add round icon button style
    - add app stylesheets dir to organize component style modules

---
+++ app/javascript/controllers/ad_locations_controller.js
@@ -0,0 +1,117 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["waveformInspector", "markersInput"]
+
+  static values = {
+    labelPrefix: { type: String, default: "Ad Position" },
+    segmentCount: { type: Number, default: 1 },
+    markers: Array,
+  }
+
+  connect() {
+    console.log("connect >> markersValue", this.markersValue)
+  }
+
+  /**
+   * Handle external changes to incoming marker values.
+   * Should only run when controller initializes, or we need to edit ad locations for a different episode.
+   * DO NOT UPDATE THIS VALUE AS A RESULT OF EDITS IN THE AD LOCATIONS UI.
+   */
+  markersValueChanged() {
+    this.initMarkers()
+  }
+
+  /**
+   * Initial render of ad markers.
+   */
+  initMarkers() {
+    console.log("ad-locations#updateAdMarkers >> markersValue", this.markersValue)
+
+    this.adMarkers = [...Array(this.segmentCountValue - 1).keys()]
+      .map((key) => this.markersValue?.[key] || [])
+      .map(([startTime, endTime], index) => ({
+        id: Math.random().toString(16).split(".")[1],
+        labelText: `${this.labelPrefixValue} ${index + 1}`,
+        startTime,
+        endTime,
+      }))
+
+    console.log("ad-locations#updateAdMarkers >> adMarkers", this.adMarkers)
+
+    this.sortAdMarkers()
+
+    this.renderMarkers()
+  }
+
+  /**
+   * Handle changes to an ad marker.
+   * @param CustomEvent Event containing changed marker data.
+   */
+  updateAdMarker({ detail: { id, startTime, endTime } }) {
+    const adMarkerIndex = this.adMarkers.findIndex((marker) => marker.id === id)
+    const adMarker = this.adMarkers[adMarkerIndex]
+
+    this.adMarkers[adMarkerIndex] = {
+      ...adMarker,
+      startTime,
+      endTime,
+    }
+
+    this.updateAdMarkers()
+  }
+
+  /**
+   * Update ad markers after a change and rerender.
+   */
+  updateAdMarkers() {
+    console.log("ad-locations#updateAdMarkers >> adMakers", this.adMarkers)
+
+    this.sortAdMarkers()
+
+    this.renderMarkers()
+  }
+
+  /**
+   * Render ad markers (form output, waveform update, location control fields).
+   */
+  renderMarkers() {
+    // Convert ad markers to markers array.
+    const markers = this.getMarkers()
+
+    console.log("ad-locations#renderMarkers", markers, this.adMarkers)
+
+    // Updated markers form input value.
+    this.markersInputTarget.value = JSON.stringify(markers)
+
+    // Update waveform inspector.
+    if (this.hasWaveformInspectorTarget) {
+      this.waveformInspectorTarget.dataset.waveformInspectorMarkersValue = JSON.stringify(this.adMarkers)
+    }
+  }
+
+  /**
+   * Sort ad markers by start time, in ascending order, and update label text.
+   */
+  sortAdMarkers() {
+    this.adMarkers = this.adMarkers
+      .sort((a, b) => a.startTime - b.startTime)
+      .map((marker, index) => ({
+        ...marker,
+        labelText: `${this.labelPrefixValue} ${index + 1}`,
+      }))
+
+    console.log("ad-locations#sortAdMarkers >> adMakers", this.adMarkers)
+  }
+
+  /**
+   * Get ad markers as times only in array format. (e.g. [[start], [start, end])
+   * @returns Array of arrays containing start and end times for markers.
+   */
+  getMarkers() {
+    return this.adMarkers.reduce(
+      (a, { startTime, endTime }) => (startTime ? [...a, [startTime, ...(endTime ? [endTime] : [])]] : a),
+      []
+    )
+  }
+}
---
+++ app/javascript/controllers/waveform_inspector_controller.js
@@ -0,0 +1,333 @@
+import { Controller } from "@hotwired/stimulus"
+import Peaks from "peaks.js"
+import _ from "lodash"
+
+function convertToSeconds(value) {
+  if (!value) return NaN
+
+  if (typeof value === "number") return value
+
+  if (value.indexOf(":") != -1) {
+    // Convert duration string to seconds.
+    const parts = value
+      .replaceAll(/[^0-9.:]/gi) // Keep only characters used in duration string.
+      .split(":") // Split into duration segments.
+      .map((v) => parseFloat(v)) // Parse segments into numbers.
+      .reduce((a, c) => [c, ...a], []) // Flip the order so less common segments (e.g. hours) will be undefined.
+    const [seconds, minutes, hours] = parts
+    return seconds + (minutes || 0) * 60 + (hours || 0) * 360
+  } else {
+    // Convert string to number.
+    const numericValue = value.replaceAll(/[^0-9.]/gi)
+    return parseFloat(numericValue)
+  }
+}
+
+function convertSecondsToDuration(inputSeconds) {
+  let duration = "00:00"
+
+  if (["number", "string"].indexOf(typeof inputSeconds) === -1) return duration
+
+  if (typeof inputSeconds === "string" && inputSeconds.indexOf(":") > -1) {
+    return inputSeconds
+  }
+
+  const totalSeconds = typeof inputSeconds === "string" ? parseFloat(inputSeconds) : inputSeconds
+
+  if (typeof totalSeconds === "number" && totalSeconds > 0) {
+    const hours = Math.floor(totalSeconds / 3600)
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+    const seconds = (totalSeconds % 60).toFixed(2)
+    duration = [
+      ...(hours ? [hours] : []),
+      String(minutes).padStart(2, "0"),
+      seconds > 9 ? seconds : `0${seconds}`,
+    ].join(":")
+  }
+
+  return duration
+}
+
+export default class extends Controller {
+  static targets = ["overview", "zoom", "scrollbar", "seekInput"]
+
+  static values = {
+    audioUrl: String,
+    waveformUrl: String,
+    markers: Array,
+  }
+
+  static classes = ["playing"]
+
+  updateLayout = _.debounce(() => {
+    this.peaks?.views.getView("zoomview")?.fitToContainer()
+    this.peaks?.views.getView("overview")?.fitToContainer()
+  }, 500)
+
+  connect() {
+    console.log("waveform-inspector#connect >> markers", this.markersValue)
+
+    this.audioElement = new Audio()
+    this.audioElement.preload = "metadata"
+    this.audioElement.src = this.audioUrlValue
+
+    this.peaksOptions = {
+      ...(this.hasZoomTarget && {
+        zoomview: {
+          container: this.zoomTarget,
+          fontSize: 12,
+        },
+      }),
+      ...(this.hasOverviewTarget && {
+        overview: {
+          container: this.overviewTarget,
+          highlightOffset: 0,
+          highlightBorderRadius: 0,
+          highlightColor: "#0072a3",
+          highlightOpacity: 0.1,
+          segmentOptions: {
+            overlayOffset: 0,
+          },
+        },
+      }),
+      ...(this.hasScrollbarTarget && {
+        scrollbar: {
+          container: this.scrollbarTarget,
+          color: "#aaa",
+          minWidth: 20,
+        },
+      }),
+      mediaElement: this.audioElement,
+      dataUri: {
+        json: this.waveformUrlValue,
+      },
+      keyboard: true,
+      emitCueEvents: true,
+      showPlayheadTime: true,
+
+      waveformColor: "#ddd",
+      playedWaveformColor: "#aaa",
+
+      pointMarkerColor: "#ff9600",
+
+      segmentOptions: {
+        waveformColor: "#ff9600",
+        startMarkerColor: "#ff9600",
+        endMarkerColor: "#ff9600",
+        overlay: true,
+        overlayOffset: 0,
+        overlayColor: "#ff9600",
+        overlayOpacity: 0.3,
+        overlayBorderWidth: 0,
+        overlayCornerRadius: 0,
+      },
+    }
+
+    this.initPeaks()
+  }
+
+  initPeaks() {
+    if (this.peaks) {
+      this.peaks.destroy()
+    }
+
+    const self = this
+    Peaks.init(this.peaksOptions, (err, peaksInstance) => {
+      if (err) {
+        console.error(err.message)
+        return
+      }
+
+      const zoomView = peaksInstance.views.getView("zoomview")
+      const overviewView = peaksInstance.views.getView("overview")
+
+      // Prevent segments from overlapping other segments.
+      // We will also add some placeholder segments to prevent overlapping points.
+      zoomView.setSegmentDragMode("no-overlap")
+
+      // Allow segments to be move all at once, instead of just by the markers.
+      zoomView.enableSegmentDragging(true)
+
+      // Player Events
+      peaksInstance.on("player.pause", () => {
+        console.log("player.pause")
+
+        // Remove playing classes.
+        self.element.classList.remove(...this.playingClasses)
+      })
+
+      peaksInstance.on("player.playing", () => {
+        console.log("player.playing")
+
+        // Apply playing classes.
+        self.element.classList.add(...this.playingClasses)
+      })
+
+      peaksInstance.on("player.timeupdate", (time) => {
+        console.log("player.timeupdate", time)
+
+        // Update currentTime input placeholder.
+        self.seekInputTarget.placeholder = convertSecondsToDuration(time)
+
+        // Update data attribute so external controls have easy access to the playhead time.
+        self.element.dataset.playheadTime = time
+      })
+
+      // Points Events
+      peaksInstance.on("points.dragstart", ({ point }) => {
+        console.log("points.dragstart", point)
+      })
+
+      peaksInstance.on("points.dragend", ({ point }) => {
+        console.log("points.dragend", point)
+
+        const { id, time } = point
+        let newTime = time
+
+        // Prevent point from being dropped into any segment ranges.
+        const segments = peaksInstance.segments.getSegments()
+        const intersectingSegment = segments.find(({ startTime, endTime }) => newTime > startTime && newTime < endTime)
+
+        if (intersectingSegment) {
+          const { startTime, endTime } = intersectingSegment
+          const midTime = (startTime + endTime) / 2
+          newTime = time > midTime ? endTime + 0.1 : startTime - 0.1
+
+          point.update({
+            time: newTime,
+          })
+        }
+
+        // Update placeholder segment
+        const placeholder = peaksInstance.segments.getSegment(`placeholder.segments.${id}`)
+
+        placeholder.update({
+          startTime: newTime,
+          endTime: newTime,
+        })
+
+        // Dispatch marker update event.
+        self.dispatch("marker.update", { detail: { id, startTime: newTime } })
+      })
+
+      // Segment Events
+      peaksInstance.on("segments.dragstart", ({ segment }) => {
+        console.log("segments.dragstart", segment)
+      })
+
+      peaksInstance.on("segments.dragend", ({ segment }) => {
+        console.log("segments.dragend", segment)
+
+        const { id, startTime, endTime } = segment
+
+        // Dispatch marker update event.
+        self.dispatch("marker.update", { detail: { id, startTime, endTime } })
+      })
+
+      // Store peaks instance for later use.
+      self.peaks = peaksInstance
+
+      // Initialize markers.
+      if (self.markersValue) {
+        self.initMarkers()
+      }
+    })
+  }
+
+  clearMarkers() {
+    this.peaks?.points.removeAll()
+    this.peaks?.segments.removeAll()
+  }
+
+  initMarkers() {
+    console.log("waveform-inspector#initMarkers >> peaks", this.peaks)
+
+    if (!this.peaks) return
+
+    const segments = []
+    const points = []
+
+    this.markersValue.forEach(({ id, labelText, startTime, endTime }) => {
+      const optionsDefault = {
+        editable: true,
+      }
+
+      if (!startTime) return
+
+      if (endTime) {
+        segments.push({
+          ...optionsDefault,
+          id,
+          labelText,
+          startTime,
+          endTime,
+        })
+      } else {
+        points.push({
+          ...optionsDefault,
+          id,
+          labelText,
+          time: startTime,
+        })
+
+        // Add a placeholder segment for this point.
+        segments.push({
+          id: `placeholder.segments.${id}`,
+          startTime,
+          endTime: startTime,
+        })
+      }
+    })
+
+    console.log("waveform-inspector#initMarkers >> points", points)
+    console.log("waveform-inspector#initMarkers >> segments", segments)
+
+    this.peaks?.points.add(points)
+    this.peaks?.segments.add(segments)
+  }
+
+  markersValueChanged() {
+    console.log("waveform-inspector#markersValueChanged >> markers", this.markersValue, this.peaks)
+
+    this.clearMarkers()
+    this.initMarkers()
+  }
+
+  togglePlaying() {
+    if (this.audioElement.paused) {
+      this.peaks.player.play()
+    } else {
+      this.peaks.player.pause()
+    }
+  }
+
+  seekTo(time) {
+    const seconds = convertToSeconds(time)
+
+    if (seconds) {
+      this.peaks.player.seek(seconds)
+    }
+  }
+
+  seekSubmit(event) {
+    const { target } = event
+    const { value } = target
+
+    this.seekTo(value)
+
+    target.value = ""
+  }
+
+  seekToInputValue() {
+    this.seekTo(this.seekInputTarget.value)
+    this.seekInputTarget.value = ""
+  }
+
+  zoomIn() {
+    this.peaks.zoom.zoomIn()
+  }
+
+  zoomOut() {
+    this.peaks.zoom.zoomOut()
+  }
+}
