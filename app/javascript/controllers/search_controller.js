0466a3b4da1caab9b84620b4cae40f1370466c81
Ube <57785974+radical-ube@users.noreply.github.com>
2023-02-24 16:31:23 -0500

    Revert search controller to original state

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -10,31 +10,13 @@ export default class extends Controller {
   submit() {
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
-      const next = this.encodeForm()
-      if (next !== window.location.href) {
-        this.submitForm()
-        this.setHistory(next)
-      }
+      this.submitForm()
     }, 200)
-  }
-
-  encodeForm() {
-    const data = new FormData(this.element)
-    const query = new URLSearchParams(data).toString()
-    const clean = query
-      .split("&")
-      .filter((s) => s.split("=")[1])
-      .join("&")
-    return window.location.href.split("?")[0] + (clean ? `?${clean}` : "")
   }
 
   // NOTE: form.submit() doesn't seem to play well with Turbo at the moment -
   // so just click the hidden submit button
   submitForm() {
     this.submitTarget.click()
-  }
-
-  setHistory(next) {
-    window.history.replaceState(window.history.state, null, next)
   }
 }
