e91c6f2004e164df5e7d80b2198b4d0f78c4997b
Rick Peterman <rick.peterman@prx.org>
2023-02-28 13:39:25 -0600

    fix(popover_controller): add disconnect cleanup

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-    const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl))
+    this.popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl))
+  }
+
+  disconnect() {
+    this.popoverList.forEach((popover) => {
+      popover.dispose()
+    })
+    delete this.popoverList
   }
 }
