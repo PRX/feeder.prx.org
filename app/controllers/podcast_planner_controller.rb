755e9ce8490df5dcefe59f0d94b8a1685a87a5a8
Ube <57785974+radical-ube@users.noreply.github.com>
2023-03-13 17:22:25 -0400

    Add date calculator given planner params
    This commit focuses on the calculate_dates method and subsequent helper
    methods. Because of the multiple branching conditionals, periodic vs. monthly
    week calculating are separated into different branches, and end conditions
    are separated into their own branches. This commit is in the middle of DRYing
    up the helper methods in an effort to be more readable and clarify the intent
    of the conditional logic. This commit does not render any of the
    calculations as it wants to focus on the controller logic first.

--- app/controllers/podcast_planner_controller.rb
+++ app/controllers/podcast_planner_controller.rb
@@ -7,6 +7,8 @@ class PodcastPlannerController < ApplicationController
 
   # PATCH/PUT /podcasts/1/planner
   def update
+    planned_dates = calculate_dates(planner_params)
+    binding.pry
     # respond_to do |format|
     #   if @podcast.update(podcast_planner_params)
     #     format.html { redirect_to podcast_planner_url(@podcast), notice: "Podcast planner was successfully updated." }
@@ -28,6 +30,7 @@ class PodcastPlannerController < ApplicationController
     params.permit(
       :podcast_id,
       :week_condition,
+      :periodic_weeks,
       :start_date,
       :end_condition,
       :number_of_episodes,
@@ -35,8 +38,87 @@ class PodcastPlannerController < ApplicationController
       :publish_time,
       :segment_count,
       day: [],
-      periodic_weeks: [],
       monthly_weeks: []
     )
+  end
+
+  def calculate_dates(params)
+    start = params[:start_date].to_date
+    selected_days = params[:day].reject!(&:blank?).map { |day| day.to_i }
+    period = params[:periodic_weeks].to_i
+    selected_weeks = params[:monthly_weeks].reject!(&:blank?).map { |week| week.to_i }
+
+    if params[:end_condition] == "episodes"
+      if params[:week_condition] == "periodic"
+        dates = calculate_periodic_dates(start, selected_days, period)
+      elsif params[:week_condition] == "monthly"
+        dates = calculate_monthly_dates(start, selected_days, selected_weeks)
+      end
+
+      return dates.slice(0, params[:number_of_episodes].to_i)
+    elsif params[:end_condition] == "date"
+      dates = []
+
+      if params[:week_condition] == "periodic"
+        current_day = start
+
+        while current_day <= params[:end_date].to_date
+          next_days = calculate_days_of_week(current_day, selected_days)
+          dates.concat(next_days)
+          current_day += period.weeks
+        end
+      elsif params[:week_condition] == "monthly"
+        current_day = start
+
+        while current_day <= params[:end_date].to_date
+          next_days = calculate_days_of_week(current_day, selected_days)
+          valid_next_days = next_days.select { |day| selected_weeks.include?(week_of_the_month(day)) }
+          dates.concat(next_days)
+          current_day += 1.week
+        end
+      end
+
+      return dates.select { |date| date <= params[:end_date].to_date }
+    end
+  end
+
+  def calculate_days_of_week(current_day, selected_days)
+    dates = []
+    7.times do |day|
+      dates.push(current_day) if selected_days.include?(current_day.wday)
+      current_day += 1.day
+    end
+    dates
+  end
+
+  def calculate_periodic_dates(start_date, selected_days, period)
+    dates = []
+    current_day = start_date
+
+    while dates.length < params[:number_of_episodes].to_i
+      next_days = calculate_days_of_week(current_day, selected_days)
+      dates.concat(next_days)
+      current_day += period.weeks
+    end
+
+    dates
+  end
+
+  def calculate_monthly_dates(start_date, selected_days, selected_weeks)
+    dates = []
+    current_day = start_date
+
+    while dates.length < params[:number_of_episodes].to_i
+      next_days = calculate_days_of_week(current_day, selected_days)
+      valid_next_days = next_days.select { |day| selected_weeks.include?(week_of_the_month(day)) }
+      dates.concat(valid_next_days)
+      current_day += 1.week
+    end
+
+    dates
+  end
+
+  def week_of_the_month(date)
+    (date.day.to_f / 7).ceil
   end
 end
