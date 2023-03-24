455b8426c0160c4d6f0ff6e2891e6d1f2891129f
Ube <57785974+radical-ube@users.noreply.github.com>
2023-03-24 13:14:44 -0400

    Rename podcast planner stimulus to click for generic use

--- app/javascript/controllers/click_controller.js
+++ app/javascript/controllers/click_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["submit"]
 
-  preview() {
+  submit() {
     this.submitTarget.click()
   }
 }
