bab958a03711ba8cad123733f17310d51629aa12
cavis <ryancavis@gmail.com>
2023-03-28 13:29:55 -0600

    Only use sessions if session_id is already set

--- lib/feeder_active_record_store.rb
+++ lib/feeder_active_record_store.rb
@@ -1,11 +1,7 @@
 # customized AR-store, which doesn't run SQL on API requests
 class FeederActiveRecordStore < ActionDispatch::Session::ActiveRecordStore
   def find_session(request, id)
-    # TODO: how to determine if request.session_options[:skip], before that before_action has even run?
-    # or just check request path?
-    # or a controller class method/var?
-    # binding.pry
-    # then return nil instead of super
-    super
+    # id gets set from non request.session_options[:skip] controllers
+    super unless id.nil?
   end
 end
