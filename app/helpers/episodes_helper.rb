175fe159f7a97ef2e988c22bebcb8def95a199bb
cavis <ryancavis@gmail.com>
2023-04-10 11:06:17 -0600

    Reorganize audio status partials

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -38,4 +38,10 @@ module EpisodesHelper
       episodes_path
     end
   end
+
+  def episode_replacing_content?(episode, content)
+    episode.contents.reject(&:new_record?).select(&:marked_for_destruction?).any? do |c|
+      c.position == content.position
+    end
+  end
 end
