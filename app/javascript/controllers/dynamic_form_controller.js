f8305acda87ac0fc33e6a380b0bbb38171ea6aeb
cavis <ryancavis@gmail.com>
2023-04-12 09:45:37 -0600

    Revert extra params

--- app/javascript/controllers/dynamic_form_controller.js
+++ app/javascript/controllers/dynamic_form_controller.js
@@ -1,30 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["link", "extra"]
-  static values = { debounce: Number }
+  static targets = ["link"]
 
   change(event) {
-    if (this.debounceValue) {
-      clearTimeout(this.timeout)
-      this.timeout = setTimeout(() => this.clickLinks(event.target), this.debounceValue)
-    } else {
-      this.clickLinks(event.target)
-    }
-  }
-
-  clickLinks(element) {
     for (const link of this.linkTargets) {
       const oldHref = link.href
-      const params = new URLSearchParams(link.search)
-
-      // combine element with (optional) extra form fields
-      params.set(element.name, element.value)
-      for (const f of this.extraTargets) {
-        params.set(f.name, f.value)
-      }
-
-      link.search = params
+      link.search = new URLSearchParams({ [event.target.name]: event.target.value })
       if (link.href !== oldHref) {
         link.click()
       }
