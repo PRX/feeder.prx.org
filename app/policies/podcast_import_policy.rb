e3443a1e053f7ef62ec21d0b12d17c83a7e9854c
Sam Vevang <sam.vevang@gmail.com>
2023-04-18 14:40:15 -0500

    Podcast import policy

--- app/policies/podcast_import_policy.rb
+++ app/policies/podcast_import_policy.rb
@@ -1,9 +1,18 @@
-class PodcastImportPolicy < AccountablePolicy
-  def initialize(token, import)
-    super(token, import, [:story, :series])
+class PodcastImportPolicy < PodcastPolicy
+  private
+
+  def account_id
+    resource.podcast.account_id
   end
 
-  def verify_rss?
-    true
+  def account_id_was
+    resource.podcast.account_id_was
+  end
+
+  class Scope < Scope
+    def resolve
+      podcast_policy_scope = PodcastPolicy::Scope.new(token, Podcast.all).resolve
+      PodcastImport.from(PodcastImport.joins(:podcast).where(podcast: podcast_policy_scope), :podcast_imports)
+    end
   end
 end
