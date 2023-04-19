aff25d075a2cdfad939cd9ee66f4f05402763bef
cavis <ryancavis@gmail.com>
2023-04-19 08:19:44 -0600

    Chain policies to feed

---
+++ app/policies/feed_image_policy.rb
@@ -0,0 +1,17 @@
+class FeedImagePolicy < ApplicationPolicy
+  def create?
+    update?
+  end
+
+  def update?
+    if resource.podcast.present?
+      FeedPolicy.new(token, resource.feed).update?
+    else
+      false
+    end
+  end
+
+  def destroy?
+    update?
+  end
+end
