fb5b9f47590c64a95b74d3b2549e0987e7d3e7bc
Ube <57785974+radical-ube@users.noreply.github.com>
2023-04-21 12:42:49 -0400

    Add pager controller
    The multi-calendar view needed a way to page through multiple months
    while keeping selection in-tact when scrolling. The method used
    for this controller is to add or remove the d-none class to any
    elements that are within the range of the pager position.

---
+++ app/javascript/controllers/pager_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["page", "prev", "next"]
+  static values = {
+    currentPosition: {type: Number, default: 0},
+    range: {type: Number, default: 6},
+    step: {type: Number, default: 1}
+  }
+
+  currentPositionValueChanged() {
+    this.pageTargets.forEach(el => {
+      if (this.inRange(parseInt(el.getAttribute("index")))) {
+        el.classList.remove("d-none")
+      } else {
+        el.classList.add("d-none")
+      }
+    })
+  }
+
+  inRange(index) {
+    return index >= this.currentPositionValue && index < this.currentPositionValue + this.rangeValue
+  }
+
+  pageForward() {
+    if (this.currentPositionValue < this.pageTargets.length - this.rangeValue) {
+      this.currentPositionValue += this.stepValue
+      this.prevTarget.disabled = false
+    }
+    if (this.currentPositionValue === this.pageTargets.length - this.rangeValue) {
+      this.nextTarget.disabled = true
+    }
+  }
+
+  pageBackward() {
+    if (this.currentPositionValue > 0) {
+      this.currentPositionValue -= this.stepValue
+      this.nextTarget.disabled = false
+    }
+    if (this.currentPositionValue === 0) {
+      this.prevTarget.disabled = true
+    }
+  }
+}
