9a6e816f121aa2f5bbc3a94548cf06a668dcdc75
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-02 11:37:46 -0400

    Add count controller

---
+++ app/javascript/controllers/count_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["counter", "display"]
+  static values = {
+    selected: { type: Number, default: 0 }
+  }
+
+  connect() {
+    this.selectedValue = this.countSelectedTargets()
+    this.displayTarget.innerHTML = this.selectedValue
+  }
+
+  recount() {
+    this.selectedValue = this.countSelectedTargets()
+    this.displayTarget.innerHTML = this.selectedValue
+  }
+
+  countSelectedTargets() {
+    const selectedTargets = this.counterTargets.filter(el => {
+      return el.firstElementChild.disabled
+    })
+    return this.counterTargets.length - selectedTargets.length
+  }
+}
