ba0da677b83d05828ee251d2aa52d4e3e2f2e9ea
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-05 12:28:14 -0400

    Fix feed controller tests to account for the currently working actions

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -1,18 +1,15 @@
 require "test_helper"
 
 class FeedsControllerTest < ActionDispatch::IntegrationTest
+  let(:podcast) { create(:podcast, prx_account_uri: "/api/v1/accounts/123") }
+  let(:feed) { create(:feed, podcast: podcast) }
+
   setup_current_user { build(:user) }
-  setup { @feed = create(:feed) }
 
-  test "should get index" do
-    get feeds_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_feed_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_feed_url
+  #   assert_response :success
+  # end
 
   # test "should create feed" do
   #   assert_difference("Feed.count") do
@@ -23,25 +20,20 @@ class FeedsControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test "should show feed" do
-    get feed_url(@feed)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_feed_url(@feed)
+    get podcast_feed_url(podcast, feed)
     assert_response :success
   end
 
   test "should update feed" do
-    patch feed_url(@feed), params: {feed: {}}
-    assert_redirected_to feed_url(@feed)
+    patch podcast_feed_url(podcast, feed), params: {feed: {}}
+    assert_redirected_to podcast_feed_url(podcast, feed)
   end
 
-  test "should destroy feed" do
-    assert_difference("Feed.count", -1) do
-      delete feed_url(@feed)
-    end
+  # test "should destroy feed" do
+  #   assert_difference("Feed.count", -1) do
+  #     delete feed_url(@feed)
+  #   end
 
-    assert_redirected_to feeds_url
-  end
+  #   assert_redirected_to feeds_url
+  # end
 end
