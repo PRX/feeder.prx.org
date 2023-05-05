11eeba11c38f14a79d0e0fa9a8190f6ad8d0482e
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-05 12:02:51 -0400

    Add help text and move header/help text to en.yml

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,12 +1,12 @@
 module FeedsHelper
   def episode_offset_options
     [
-      ['5 minutes', 300],
-      ['15 minutes', 900],
-      ['1 hour', 3600],
-      ['6 hours', 21600],
-      ['12 hours', 43200],
-      ['24 hours', 86400]
+      ["5 minutes", 300],
+      ["15 minutes", 900],
+      ["1 hour", 3600],
+      ["6 hours", 21600],
+      ["12 hours", 43200],
+      ["24 hours", 86400]
     ]
   end
 
@@ -20,14 +20,14 @@ module FeedsHelper
 
   def audio_bitrate_options
     [
-      ['96 kbps', 96],
-      ['112 kbps', 112],
-      ['128 kbps', 128],
-      ['160 kbps', 160],
-      ['192 kbps', 192],
-      ['224 kbps', 224],
-      ['256 kbps', 256],
-      ['320 kbps', 320]
+      ["96 kbps", 96],
+      ["112 kbps", 112],
+      ["128 kbps", 128],
+      ["160 kbps", 160],
+      ["192 kbps", 192],
+      ["224 kbps", 224],
+      ["256 kbps", 256],
+      ["320 kbps", 320]
     ]
   end
 
@@ -48,14 +48,14 @@ module FeedsHelper
 
   def audio_sample_options
     [
-      ['8 kHz', 8000],
-      ['11.025 kHz', 11025],
-      ['12 kHz', 12000],
-      ['16 kHz', 16000],
-      ['22.05 kHz', 22050],
-      ['24 kHz', 24000],
-      ['44.1 kHz', 44100],
-      ['48 kHz', 48000]
+      ["8 kHz", 8000],
+      ["11.025 kHz", 11025],
+      ["12 kHz", 12000],
+      ["16 kHz", 16000],
+      ["22.05 kHz", 22050],
+      ["24 kHz", 24000],
+      ["44.1 kHz", 44100],
+      ["48 kHz", 48000]
     ]
   end
 end
