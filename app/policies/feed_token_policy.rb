8ec12319697bdffe3107096c24fc464df947ea61
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-09 14:48:21 -0400

    Extract custom feed fields into their own partial

--- app/policies/feed_token_policy.rb
+++ app/policies/feed_token_policy.rb
@@ -1,9 +1,21 @@
 class FeedTokenPolicy < ApplicationPolicy
+  def show?
+    update?
+  end
+
+  def create?
+    update?
+  end
+
   def update?
     if resource.feed.present?
       FeedPolicy.new(token, resource.feed).update?
     else
       false
     end
+  end
+
+  def destroy?
+    update?
   end
 end
