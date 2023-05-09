b42d85802c53f96666493e4a8de1cc9df2f7e821
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-09 11:50:19 -0400

    Move options labels into en.yml

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,61 +1,37 @@
 module FeedsHelper
   def episode_offset_options
-    [
-      ["5 minutes", 300],
-      ["15 minutes", 900],
-      ["1 hour", 3600],
-      ["6 hours", 21600],
-      ["12 hours", 43200],
-      ["24 hours", 86400]
-    ]
+    values = [300, 900, 3600, 21600, 43200, 86400]
+
+    I18n.t([:min_5, :min_15, :hr_1, :hr_6, :hr_12, :hr_24], scope: [:feeds, :helper, :episode_offset_options]).map.with_index { |label, i| [label, values[i]] }
   end
 
   def audio_format_options
-    [
-      ["MP3", "mp3"],
-      ["FLAC", "flac"],
-      ["WAV", "wav"]
-    ]
+    values = ["mp3", "flac", "wav"]
+
+    I18n.t([:mp3, :flac, :wav], scope: [:feeds, :helper, :audio_format_options]).map.with_index { |label, i| [label, values[i]] }
   end
 
   def audio_bitrate_options
-    [
-      ["96 kbps", 96],
-      ["112 kbps", 112],
-      ["128 kbps", 128],
-      ["160 kbps", 160],
-      ["192 kbps", 192],
-      ["224 kbps", 224],
-      ["256 kbps", 256],
-      ["320 kbps", 320]
-    ]
+    values = [96, 112, 128, 160, 192, 224, 256, 320]
+
+    I18n.t([:kb_96, :kb_112, :kb_128, :kb_160, :kb_192, :kb_224, :kb_256, :kb_320], scope: [:feeds, :helper, :audio_bitrate_options]).map.with_index { |label, i| [label, values[i]] }
   end
 
   def audio_bitdepth_options
-    [
-      ["16 bit", 16],
-      ["24 bit", 24],
-      ["32 bit", 32]
-    ]
+    values = [16, 24, 32]
+
+    I18n.t([:b_16, :b_24, :b_32], scope: [:feeds, :helper, :audio_bitdepth_options]).map.with_index { |label, i| [label, values[i]] }
   end
 
   def audio_channel_options
-    [
-      ["Mono", 1],
-      ["Stereo", 2]
-    ]
+    values = [1, 2]
+
+    I18n.t([:mono, :stereo], scope: [:feeds, :helper, :audio_channel_options]).map.with_index { |label, i| [label, values[i]] }
   end
 
   def audio_sample_options
-    [
-      ["8 kHz", 8000],
-      ["11.025 kHz", 11025],
-      ["12 kHz", 12000],
-      ["16 kHz", 16000],
-      ["22.05 kHz", 22050],
-      ["24 kHz", 24000],
-      ["44.1 kHz", 44100],
-      ["48 kHz", 48000]
-    ]
+    values = [8000, 11025, 12000, 16000, 22050, 24000, 44100, 48000]
+
+    I18n.t([:khz_8, :khz_11, :khz_12, :khz_16, :khz_22, :khz_24, :khz_44, :khz_48], scope: [:feeds, :helper, :audio_sample_options]).map.with_index { |label, i| [label, values[i]] }
   end
 end
