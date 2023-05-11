7728efb27d26f95f73baa92e4535fe26da052fe2
cavis <ryancavis@gmail.com>
2023-05-11 07:53:45 -0600

    Actually test something

--- test/models/content_test.rb
+++ test/models/content_test.rb
@@ -1,21 +1,20 @@
 require "test_helper"
 
 describe Content do
-  let(:episode) { create(:episode) }
-  let(:content) { Content.create(original_url: "u", file_size: 10, mime_type: "mt", episode: episode) }
-  let(:crier_content) {
-    {
-      "position" => 1,
-      "url" => "https://s3.amazonaws.com/prx-dovetail/testserial/serial_audio.mp3",
-      "type" => "audio/mpeg",
-      "file_size" => 26017749,
-      "medium" => "audio",
-      "expression" => "sample",
-      "bitrate" => 64,
-      "samplingrate" => "44.1",
-      "channels" => 1,
-      "duration" => "3252.19",
-      "lang" => "en"
-    }
-  }
+  let(:episode) { create(:episode, contents: [c1, c2, c3]) }
+  let(:c1) { build(:content, position: 1) }
+  let(:c2) { build(:content, position: 2) }
+  let(:c3) { build(:content, position: 3) }
+
+  describe "#replace_resources!" do
+    it "replaces contents with the same position" do
+      assert_equal [c1, c2, c3], episode.contents
+
+      c4 = create(:content, episode: episode, position: 2)
+      assert_equal [c1, c4, c3], episode.reload.contents
+
+      refute_nil c2.reload.deleted_at
+      refute_nil c2.replaced_at
+    end
+  end
 end
