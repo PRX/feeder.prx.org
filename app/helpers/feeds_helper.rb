4ebe30a99dc08480c5ebd0f6b978cdc16805bb12
Ube <57785974+radical-ube@users.noreply.github.com>
2023-05-12 14:28:38 -0400

    Simplify options helper more

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,37 +1,37 @@
 module FeedsHelper
   def episode_offset_options
-    values = [300, 900, 3600, 21600, 43200, 86400]
-
-    I18n.t(values, scope: [:feeds, :helper, :episode_offset_options]).map.with_index { |label, i| [label, values[i]] }
+    [300, 900, 3600, 21600, 43200, 86400].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :episode_offset_options]), value]
+    end
   end
 
   def audio_format_options
-    values = ["mp3", "flac", "wav"]
-
-    I18n.t(values, scope: [:feeds, :helper, :audio_format_options]).map.with_index { |label, i| [label, values[i]] }
+    ["mp3", "flac", "wav"].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :audio_format_options]), value]
+    end
   end
 
   def audio_bitrate_options
-    values = [96, 112, 128, 160, 192, 224, 256, 320]
-
-    I18n.t(values, scope: [:feeds, :helper, :audio_bitrate_options]).map.with_index { |label, i| [label, values[i]] }
+    [96, 112, 128, 160, 192, 224, 256, 320].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :audio_bitrate_options]), value]
+    end
   end
 
   def audio_bitdepth_options
-    values = [16, 24, 32]
-
-    I18n.t(values, scope: [:feeds, :helper, :audio_bitdepth_options]).map.with_index { |label, i| [label, values[i]] }
+    [16, 24, 32].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :audio_bitdepth_options]), value]
+    end
   end
 
   def audio_channel_options
-    values = [1, 2]
-
-    I18n.t(values, scope: [:feeds, :helper, :audio_channel_options]).map.with_index { |label, i| [label, values[i]] }
+    [1, 2].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :audio_channel_options]), value]
+    end
   end
 
   def audio_sample_options
-    values = [8000, 11025, 12000, 16000, 22050, 24000, 44100, 48000]
-
-    I18n.t(values, scope: [:feeds, :helper, :audio_sample_options]).map.with_index { |label, i| [label, values[i]] }
+    [8000, 11025, 12000, 16000, 22050, 24000, 44100, 48000].map do |value|
+      [I18n.t(value, scope: [:feeds, :helper, :audio_sample_options]), value]
+    end
   end
 end
