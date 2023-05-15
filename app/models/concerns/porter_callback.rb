0a8404ad1171d6afa4c27ffaf3144e14fae3bf83
cavis <ryancavis@gmail.com>
2023-05-15 15:11:42 -0600

    Rename to callback

--- app/models/concerns/porter_callback.rb
+++ app/models/concerns/porter_callback.rb
@@ -1,6 +1,6 @@
 require "active_support/concern"
 
-module PorterParser
+module PorterCallback
   extend ActiveSupport::Concern
 
   class_methods do
--- test/models/concerns/porter_callback_test.rb
+++ test/models/concerns/porter_callback_test.rb
@@ -1,59 +1,59 @@
 require "test_helper"
 
-class TestParser
-  include PorterParser
+class TestCallback
+  include PorterCallback
   attr_accessor :result
 end
 
-describe PorterParser do
+describe PorterCallback do
   it "handles non-porter messages" do
-    assert_nil TestParser.porter_callback_job_id({})
-    assert_nil TestParser.porter_callback_status({})
-    assert_nil TestParser.porter_callback_time({})
+    assert_nil TestCallback.porter_callback_job_id({})
+    assert_nil TestCallback.porter_callback_status({})
+    assert_nil TestCallback.porter_callback_time({})
   end
 
   it "parses porter job received callback messages" do
     porter_received_callback = build(:porter_job_received)
-    assert_equal TestParser.porter_callback_job_id(porter_received_callback), "the-job-id"
-    assert_equal TestParser.porter_callback_status(porter_received_callback), "processing"
-    assert_equal TestParser.porter_callback_time(porter_received_callback), Time.parse("2012-12-21T12:34:56Z")
+    assert_equal TestCallback.porter_callback_job_id(porter_received_callback), "the-job-id"
+    assert_equal TestCallback.porter_callback_status(porter_received_callback), "processing"
+    assert_equal TestCallback.porter_callback_time(porter_received_callback), Time.parse("2012-12-21T12:34:56Z")
   end
 
   it "ignores porter task result callback messages" do
     task_result_callback = build(:porter_task_result)
-    assert_equal TestParser.porter_callback_job_id(task_result_callback), "the-job-id"
-    assert_nil TestParser.porter_callback_status(task_result_callback)
+    assert_equal TestCallback.porter_callback_job_id(task_result_callback), "the-job-id"
+    assert_nil TestCallback.porter_callback_status(task_result_callback)
   end
 
   it "ignores porter task error callback messages" do
     task_error_callback = build(:porter_task_error)
-    assert_equal TestParser.porter_callback_job_id(task_error_callback), "the-job-id"
-    assert_nil TestParser.porter_callback_status(task_error_callback)
+    assert_equal TestCallback.porter_callback_job_id(task_error_callback), "the-job-id"
+    assert_nil TestCallback.porter_callback_status(task_error_callback)
   end
 
   it "parses porter ingest failed callback messages" do
     porter_404_callback = build(:porter_job_ingest_failed)
-    assert_equal TestParser.porter_callback_job_id(porter_404_callback), "the-job-id"
-    assert_equal TestParser.porter_callback_status(porter_404_callback), "error"
-    assert_equal TestParser.porter_callback_time(porter_404_callback), Time.parse("2012-12-21T12:34:56Z")
+    assert_equal TestCallback.porter_callback_job_id(porter_404_callback), "the-job-id"
+    assert_equal TestCallback.porter_callback_status(porter_404_callback), "error"
+    assert_equal TestCallback.porter_callback_time(porter_404_callback), Time.parse("2012-12-21T12:34:56Z")
   end
 
   it "parses porter job failed callback messages" do
     porter_error_callback = build(:porter_job_failed)
-    assert_equal TestParser.porter_callback_job_id(porter_error_callback), "the-job-id"
-    assert_equal TestParser.porter_callback_status(porter_error_callback), "error"
-    assert_equal TestParser.porter_callback_time(porter_error_callback), Time.parse("2012-12-21T12:34:56Z")
+    assert_equal TestCallback.porter_callback_job_id(porter_error_callback), "the-job-id"
+    assert_equal TestCallback.porter_callback_status(porter_error_callback), "error"
+    assert_equal TestCallback.porter_callback_time(porter_error_callback), Time.parse("2012-12-21T12:34:56Z")
   end
 
   it "parses porter successful callback messages" do
     porter_success_callback = build(:porter_job_results)
-    assert_equal TestParser.porter_callback_job_id(porter_success_callback), "the-job-id"
-    assert_equal TestParser.porter_callback_status(porter_success_callback), "complete"
-    assert_equal TestParser.porter_callback_time(porter_success_callback), Time.parse("2012-12-21T12:34:56Z")
+    assert_equal TestCallback.porter_callback_job_id(porter_success_callback), "the-job-id"
+    assert_equal TestCallback.porter_callback_status(porter_success_callback), "complete"
+    assert_equal TestCallback.porter_callback_time(porter_success_callback), Time.parse("2012-12-21T12:34:56Z")
   end
 
   it "parses porter task results" do
-    model = TestParser.new
+    model = TestCallback.new
     model.result = build(:porter_job_results)
     copy_result = build(:porter_copy_result)
     inspect_result = build(:porter_inspect_audio_result)
@@ -66,7 +66,7 @@ describe PorterParser do
   end
 
   it "parses porter inspect info" do
-    model = TestParser.new
+    model = TestCallback.new
     assert_empty model.porter_callback_inspect
 
     model.result = build(:porter_job_results)
@@ -75,7 +75,7 @@ describe PorterParser do
   end
 
   it "parses porter mime type" do
-    model = TestParser.new
+    model = TestCallback.new
     assert_nil model.porter_callback_mime
 
     model.result = build(:porter_job_results)
@@ -94,7 +94,7 @@ describe PorterParser do
   end
 
   it "parses porter file sizes" do
-    model = TestParser.new
+    model = TestCallback.new
     assert_nil model.porter_callback_size
 
     model.result = build(:porter_job_results)
