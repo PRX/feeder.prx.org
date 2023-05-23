ccfb97f078ff2cf0ac545b695ddd26d75f15c30f
cavis <ryancavis@gmail.com>
2023-05-23 11:04:47 -0600

    Fixup tests

--- test/models/tasks/copy_media_task_test.rb
+++ test/models/tasks/copy_media_task_test.rb
@@ -55,7 +55,7 @@ describe Tasks::CopyMediaTask do
     it "replaces resources and publishes on complete" do
       publish = MiniTest::Mock.new
 
-      task.podcast.stub(:publish!, publish) do
+      task.episode.stub(:publish!, publish) do
         task.update(status: "created")
         publish.verify
 
