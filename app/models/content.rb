951ea6e25bab397df9e3109d6171195b998cc311
cavis <ryancavis@gmail.com>
2023-05-24 09:07:14 -0600

    Make sure episode and content medium matches

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,16 @@
 class Content < MediaResource
   after_save :publish_episode!
 
+  validate :validate_episode_medium, if: :status_complete?
+
+  def validate_episode_medium
+    if episode&.medium_video?
+      errors.add(:medium, :not_video, message: "must be a video file") if medium != "video"
+    elsif episode&.medium_audio? || episode&.medium_uncut?
+      errors.add(:medium, :not_audio, message: "must be an audio file") if medium != "audio"
+    end
+  end
+
   def publish_episode!
     episode&.publish! if status_complete? && status_previously_changed?
   end
