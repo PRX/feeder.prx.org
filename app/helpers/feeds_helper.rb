9c1b895e9293e09d78dfd82528a78710c46f7d3b
Ube <57785974+radical-ube@users.noreply.github.com>
2023-06-01 15:09:18 -0400

    Add image card to custom feeds following episode edit pattern

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -34,4 +34,20 @@ module FeedsHelper
       [I18n.t(value, scope: [:feeds, :helper, :audio_sample_options]), value]
     end
   end
+
+  def feed_destroy_image_path(feed, form)
+    if feed.new_record?
+      new_podcast_feed_path feed.podcast_id, uploads_destroy_params(form)
+    else
+      podcast_feed_path feed.podcast_id, uploads_destroy_params(form)
+    end
+  end
+
+  def feed_retry_image_path(feed, form)
+    if feed.new_record?
+      new_podcast_feed_path feed.podcast_id, uploads_destroy_params(form)
+    else
+      podcast_feed_path feed.podcast_id, uploads_destroy_params(form)
+    end
+  end
 end
