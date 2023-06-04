1d7603ed77a58d4d1bbeee7cd8e1fa7721db4448
Sam Vevang <sam.vevang@gmail.com>
2023-06-04 13:22:24 -0500

    Non-threadsafe methods for looking at the publishing queue

--- app/models/publishing_attempt.rb
+++ app/models/publishing_attempt.rb
@@ -2,18 +2,21 @@ class PublishingAttempt < ApplicationRecord
   # add a scope that returns the most recent publishing attempt for a given podcast
   scope :latest, -> { order(created_at: :desc) }
 
-  belongs_to :publishing_log
+  belongs_to :publishing_queue_entry
   belongs_to :podcast
 
+  # None of the methods in here are threadsafe if we assume that creating
+  # published artifacts is non-idempotent (e.g. creatig remote Apple resources)
+
+  def self.complete?(podcast)
+    latest_attempt(podcast)&.complete?
+  end
+
   def self.latest_attempt(podcast)
-    PublishingAttempt.where(podcast_id: podcast.id).latest.first
+    where(podcast_id: podcast.id).latest.first
   end
 
-  def mark_complete
-    update!(complete: true)
-  end
-
-  def schedule!
-    PublishingAttempt.create!(podcast: podcast, publishing_log: PublishingLog.create!(podcast: podcast))
+  def complete_publishing!
+    create!(podcast: podcast, publishing_log: publishing_log, complete: true)
   end
 end
