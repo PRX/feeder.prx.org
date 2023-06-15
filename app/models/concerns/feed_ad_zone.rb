56a2f304fb111c37ada212751bc540a211bbcfc8
Ube <57785974+radical-ube@users.noreply.github.com>
2023-06-15 12:52:33 -0400

    Fix ad zones nil check

--- app/models/concerns/feed_ad_zone.rb
+++ app/models/concerns/feed_ad_zone.rb
@@ -3,24 +3,14 @@ require "active_support/concern"
 module FeedAdZone
   extend ActiveSupport::Concern
 
-  def add_zone(zone)
-    include_zones << zone unless include_zones.include?(zone)
-  end
-
-  def remove_zone(zone)
-    include_zones.reject! { |z| z == zone }
-  end
+  ALL_ZONES = ["billboard", "house", "ad", "sonic_id"]
 
   def billboard
-    return unless include_zones.present?
-
-    include_zones.include?("billboard") ? 1 : 0
+    check("billboard")
   end
 
   def billboard=(val)
-    self.include_zones ||= []
-
-    if val == "1"
+    if checked?(val)
       add_zone("billboard")
     else
       remove_zone("billboard")
@@ -28,15 +18,11 @@ module FeedAdZone
   end
 
   def house
-    return unless include_zones.present?
-
-    include_zones.include?("house") ? 1 : 0
+    check("house")
   end
 
   def house=(val)
-    self.include_zones ||= []
-
-    if val == "1"
+    if checked?(val)
       add_zone("house")
     else
       remove_zone("house")
@@ -44,15 +30,11 @@ module FeedAdZone
   end
 
   def paid
-    return unless include_zones.present?
-
-    include_zones.include?("ad") ? 1 : 0
+    check("ad")
   end
 
   def paid=(val)
-    self.include_zones ||= []
-
-    if val == "1"
+    if checked?(val)
       add_zone("ad")
     else
       remove_zone("ad")
@@ -60,18 +42,44 @@ module FeedAdZone
   end
 
   def sonic_id
-    return unless include_zones.present?
-
-    include_zones.include?("sonic_id") ? 1 : 0
+    check("sonic_id")
   end
 
   def sonic_id=(val)
-    self.include_zones ||= []
-
-    if val == "1"
+    if checked?(val)
       add_zone("sonic_id")
     else
       remove_zone("sonic_id")
+    end
+  end
+
+  private
+
+  def add_zone(zone)
+    return if self.include_zones.nil?
+
+    self.include_zones << zone unless self.include_zones.include?(zone)
+    if self.include_zones.count == 4
+      self.include_zones = nil
+    end
+  end
+
+  def remove_zone(zone)
+    if self.include_zones.nil?
+      self.include_zones = ALL_ZONES
+    end
+    self.include_zones.reject! { |z| z == zone }
+  end
+
+  def checked?(val)
+    val == "1"
+  end
+
+  def check(zone)
+    if self.include_zones.nil?
+      "1"
+    else
+      self.include_zones.include?(zone) ? "1" : "0"
     end
   end
 end
