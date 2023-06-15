44c38781aaa93bb8a37e8257a85c5dc69394e471
Ube <57785974+radical-ube@users.noreply.github.com>
2023-06-15 15:46:51 -0400

    Add tests for audio format setter methods

--- test/models/concerns/feed_audio_format_test.rb
+++ test/models/concerns/feed_audio_format_test.rb
@@ -19,6 +19,20 @@ class FeedAudioFormatTest < ActiveSupport::TestCase
     end
   end
 
+  describe "#audio_type=" do
+    it "sets the :f value in audio_format" do
+      feed.audio_format = nil
+      feed.audio_type = "mp3"
+      assert_equal feed.audio_format[:f], "mp3"
+    end
+
+    it "nils audio_format if no type is selected" do
+      feed.audio_format = default_mp3_format
+      feed.audio_type = nil
+      assert_nil feed.audio_format
+    end
+  end
+
   describe "#audio_bitrate" do
     it "returns nil if audio_format is blank" do
       feed.audio_format = {}
@@ -33,6 +47,20 @@ class FeedAudioFormatTest < ActiveSupport::TestCase
     it "returns nil if audio_format type is not type mp3" do
       feed.audio_format = default_wav_format
       assert_nil feed.audio_bitrate
+    end
+  end
+
+  describe "#audio_bitrate=" do
+    it "sets the :b value in audio_format if type is mp3" do
+      feed.audio_format = default_mp3_format
+      feed.audio_bitrate = 96
+      assert_equal feed.audio_format[:b], 96
+    end
+
+    it "does not set any value if there is no audio_format" do
+      feed.audio_format = nil
+      feed.audio_bitrate = 96
+      assert_nil feed.audio_format.try(:[], :b)
     end
   end
 
@@ -55,6 +83,23 @@ class FeedAudioFormatTest < ActiveSupport::TestCase
     end
   end
 
+  describe "#audio_bitdepth=" do
+    it "sets the :b value in audio_format if type is not mp3" do
+      feed.audio_format = default_wav_format
+      feed.audio_bitdepth = 32
+      assert_equal feed.audio_format[:b], 32
+      feed.audio_format = default_flac_format
+      feed.audio_bitdepth = 16
+      assert_equal feed.audio_format[:b], 16
+    end
+
+    it "does not set any value if there is no audio_format" do
+      feed.audio_format = nil
+      feed.audio_bitdepth = 16
+      assert_nil feed.audio_format.try(:[], :b)
+    end
+  end
+
   describe "#audio_channel" do
     it "returns nil if audio_format is blank" do
       feed.audio_format = {}
@@ -67,6 +112,20 @@ class FeedAudioFormatTest < ActiveSupport::TestCase
     end
   end
 
+  describe "#audio_channel=" do
+    it "sets the :c value in audio_format" do
+      feed.audio_format = default_mp3_format
+      feed.audio_channel = 1
+      assert_equal feed.audio_format[:c], 1
+    end
+
+    it "does not set any value if there is no audio_format" do
+      feed.audio_format = nil
+      feed.audio_channel = 1
+      assert_nil feed.audio_format.try(:[], :c)
+    end
+  end
+
   describe "#audio_sample" do
     it "returns nil if audio_format is blank" do
       feed.audio_format = {}
@@ -76,6 +135,20 @@ class FeedAudioFormatTest < ActiveSupport::TestCase
     it "gets the :s value in audio_format" do
       feed.audio_format = default_mp3_format
       assert_equal feed.audio_sample, 44100
+    end
+  end
+
+  describe "#audio_sample=" do
+    it "sets the :s value in audio_format" do
+      feed.audio_format = default_mp3_format
+      feed.audio_sample = 16000
+      assert_equal feed.audio_format[:s], 16000
+    end
+
+    it "does not set any value if there is no audio_format" do
+      feed.audio_format = nil
+      feed.audio_sample = 16000
+      assert_nil feed.audio_format.try(:[], :s)
     end
   end
 end
