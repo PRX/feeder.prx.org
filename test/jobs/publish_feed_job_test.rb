855c2a19a7afcdb037a4bfd5f77a4d174a094dd9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2023-06-17 08:20:25 -0400

    Use correct s3 mixin bucket method

--- test/jobs/publish_feed_job_test.rb
+++ test/jobs/publish_feed_job_test.rb
@@ -1,38 +1,38 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 describe PublishFeedJob do
-  let(:episode) { create(:episode, prx_uri: '/api/v1/stories/87683') }
+  let(:episode) { create(:episode, prx_uri: "/api/v1/stories/87683") }
   let(:podcast) { episode.podcast }
-  let(:feed) { create(:feed, podcast: podcast, slug: 'adfree') }
+  let(:feed) { create(:feed, podcast: podcast, slug: "adfree") }
 
   let(:job) { PublishFeedJob.new }
 
-  it 'knows the right bucket to write to' do
-    assert_equal job.feeder_storage_bucket, 'test-prx-feed'
-    ENV['FEEDER_STORAGE_BUCKET'] = 'foo'
-    assert_equal job.feeder_storage_bucket, 'foo'
-    ENV['FEEDER_STORAGE_BUCKET'] = 'test-prx-feed'
+  it "knows the right bucket to write to" do
+    assert_equal job.s3_bucket, "test-prx-feed"
+    ENV["FEEDER_STORAGE_BUCKET"] = "foo"
+    assert_equal job.s3_bucket, "foo"
+    ENV["FEEDER_STORAGE_BUCKET"] = "test-prx-feed"
   end
 
-  it 'knows the right key to write to' do
+  it "knows the right key to write to" do
     assert_equal job.key(podcast, podcast.default_feed), "#{podcast.path}/feed-rss.xml"
     assert_equal job.key(podcast, feed), "#{podcast.path}/adfree/feed-rss.xml"
   end
 
-  describe 'saving the rss file' do
+  describe "saving the rss file" do
     let(:stub_client) { Aws::S3::Client.new(stub_responses: true) }
 
-    it 'can save a podcast file' do
-      job.stub(:client, stub_client) do
+    it "can save a podcast file" do
+      job.stub(:s3_client, stub_client) do
         refute_nil job.save_file(podcast, podcast.default_feed)
         refute_nil job.save_file(podcast, feed)
       end
     end
 
-    it 'can process publishing a podcast' do
-      job.stub(:client, stub_client) do
+    it "can process publishing a podcast" do
+      job.stub(:s3_client, stub_client) do
         rss = job.perform(podcast)
         refute_nil rss
         refute_nil job.put_object
