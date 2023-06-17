32aab8fb8d748b9ee0ca4a782c81250e8e8d7d12
Andrew Kuklewicz <andrew@beginsinwonder.com>
2023-06-16 21:17:30 -0400

    Get basic episode test working

--- test/lib/cms_syncher_test.rb
+++ test/lib/cms_syncher_test.rb
@@ -3,11 +3,11 @@ require "cms_syncher"
 
 describe CmsSyncher do
   let(:user_id) { 123 }
-  let(:episode) { create(:episode) }
   let(:podcast) { create(:podcast) }
   let(:feed) { create(:feed, podcast: podcast) }
   let(:image) { create(:feed_image, feed: feed) }
   let(:image) { create(:itunes_image, feed: feed) }
+  let(:episode) { create(:episode_with_media, podcast: podcast) }
 
   let(:syncher) { CmsSyncher.new }
 
@@ -19,5 +19,7 @@ describe CmsSyncher do
 
   it "synchs a cms story to an episode" do
     refute_nil episode
+    story = syncher.sync_story(episode, user_id)
+    assert_equal episode.title, story.title
   end
 end
