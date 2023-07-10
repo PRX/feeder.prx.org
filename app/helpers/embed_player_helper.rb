2dba4b85dab68d074fad47b8b03dade56465e557
cavis <ryancavis@gmail.com>
2023-07-10 16:31:46 -0600

    Init episode player links/preview

--- app/helpers/embed_player_helper.rb
+++ app/helpers/embed_player_helper.rb
@@ -4,13 +4,41 @@ module EmbedPlayerHelper
   EMBED_PLAYER_PATH = "/e"
   EMBED_PLAYER_FEED = "uf"
   EMBED_PLAYER_GUID = "ge"
+  EMBED_PLAYER_CARD = "ca"
 
-  def embed_player_episode_url(ep, preview = false)
+  def embed_player_episode_url(ep, type = nil, preview = false)
+    params = {}
+
     if preview && !episode.published?
       # TODO: private token auth url
+      {}
     else
-      embed_params(EMBED_PLAYER_FEED => ep.podcast_feed_url, EMBED_PLAYER_GUID => ep.item_guid)
+      params[EMBED_PLAYER_FEED] = ep.podcast_feed_url
+      params[EMBED_PLAYER_GUID] = ep.item_guid
     end
+
+    if type == "card" || type == "fixed_card"
+      params[EMBED_PLAYER_CARD] = "1"
+    end
+
+    embed_params(params)
+  end
+
+  def embed_player_episode_iframe(ep, type = nil)
+    if type == "card"
+      # TODO: this is not working
+      tag.div style: "width: 100%; height: calc(100% + 200px); position: relative;" do
+        tag.iframe src: embed_player_episode_url(ep, type), style: "position: absolute; inset: 0;"
+      end
+    elsif type == "fixed_card"
+      tag.iframe src: embed_player_episode_url(ep, type), width: "500", height: "700"
+    else
+      tag.iframe src: embed_player_episode_url(ep, type), width: "100%", height: "200"
+    end
+  end
+
+  def embed_player_type_options(selected)
+    options_for_select(%w[standard card fixed_card], selected)
   end
 
   private
