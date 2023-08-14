2b06c8dbfb38715a5bef73b8bb56b443aeabf2aa
Ube <57785974+radical-ube@users.noreply.github.com>
2023-08-14 13:48:56 -0400

    Add RSS Language codes into helper and localization

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -3,6 +3,8 @@ require "text_sanitizer"
 module PodcastsHelper
   include TextSanitizer
 
+  RSS_LANGUAGE_CODES = %w[af sq eu be bg ca zh-cn zh-tw hr cs da nl nl-be nl-nl en en-au en-bz en-ca en-ie en-jm en-nz en-ph en-za en-tt en-gb en-us en-zw et fo fi fr fr-be fr-ca fr-fr fr-lu fr-mc fr-ch gl gd de de-at de-de de-li de-lu de-ch el haw hu is in ga it it-it it-ch ja ko mk no pl pt pt-br pt-pt ro ro-mo ro-ro ru ru-mo ru-ru sr sk sl es es-ar es-bo es-cl es-co es-cr es-do es-ec es-sv es-gt es-hn es-mx es-ni es-pa es-py es-pe es-pr es-es es-uy es-ve sv sv-fi sv-se tr uk]
+
   def full_contact(type, item)
     name = item.try("#{type}_name")
     email = item.try("#{type}_email")
@@ -74,5 +76,9 @@ module PodcastsHelper
     else
       edit_podcast_path(podcast, uploads_retry_params(feed_form, image_form))
     end
+  end
+
+  def rss_language_options
+    RSS_LANGUAGE_CODES.map { |val| [I18n.t("rss_lang_code.#{val}"), val] }
   end
 end
