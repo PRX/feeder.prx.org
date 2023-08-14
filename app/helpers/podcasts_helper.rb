5568502ed79b85d163d0b08a0e31521711079010
Ube <57785974+radical-ube@users.noreply.github.com>
2023-08-14 15:41:01 -0400

    Update formatting and remove comment

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -7,9 +7,7 @@ module PodcastsHelper
 
   PUBLISH_LANGUAGE_CODES = %w[ach ady af af-na af-za ak ar ar-ar ar-ma ar-sa ay-bo az az-az be-by bg bg-bg bn bn-in bn-bd bs-ba ca ca-es cak ck-us cs cs-cz cy cy-gb da da-dk de de-at de-de de-ch dsb el el-gr en en-gb en-au en-ca en-ie en-in en-pi en-ud en-us en-za en@pirate eo eo-eo es es-ar es-419 es-cl es-co es-ec es-es es-la es-ni es-mx es-us es-ve et et-ee eu eu-es fa fa-ir fb-lt ff fi fi-fi fo-fo fr fr-ca fr-fr fr-be fr-ch fy-nl ga ga-ie gl gl-es gn-py gu-in gx-gr he he-il hi hi-in hr hr-hr hsb ht hu hu-hu hy-am id id-id is is-is it it-it ja ja-jp jv-id ka-ge kk-kz km km-kh kab kn kn-in ko ko-kr ku-tr la la-va lb li-nl lt lt-lt lv lv-lv mai mg-mg mk mk-mk ml ml-in mn-mn mr mr-in ms ms-my mt mt-mt my no nb nb-no ne ne-np nl nl-be nl-nl nn-no oc or-in pa pa-in pl pl-pl ps-af pt pt-br pt-pt qu-pe rm-ch ro ro-ro ru ru-ru sa-in se-no si-lk sk sk-sk sl sl-si so-so sq sq-al sr sr-rs su sv sv-se sw sw-ke ta ta-in te te-in tg tg-tj th th-th tl tl-ph tlh tr tr-tr tt-ru uk uk-ua ur ur-pk uz uz-uz vi vi-vn xh-za yi yi-de zh zh-hans zh-hant zh-cn zh-hk zh-sg zh-tw zu-za]
 
-  NON_COMPLIANT_CODES = PUBLISH_LANGUAGE_CODES.filter do |code|
-    !RSS_LANGUAGE_CODES.include?(code)
-  end
+  NON_COMPLIANT_CODES = PUBLISH_LANGUAGE_CODES.filter { |code| !RSS_LANGUAGE_CODES.include?(code) }
 
   def full_contact(type, item)
     name = item.try("#{type}_name")
