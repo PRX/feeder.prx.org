dd09c3a316fc35ad5326402f7df76738615fc8c4
cavis <ryancavis@gmail.com>
2023-08-17 10:05:26 -0600

    Enable production routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 Rails.application.routes.draw do
-  # feeder frontend dev
-  unless Rails.env.production?
-    resources :podcasts do
-      resource :engagement, only: [:show, :update], controller: :podcast_engagement
-      resource :player, only: :show, controller: :podcast_player
-      resources :imports, only: [:index, :show, :create]
-      resource :planner, only: [:show, :create], controller: :podcast_planner
-      resources :feeds, except: [:edit]
-      resources :episodes, only: [:index, :create, :new]
-    end
-
-    resources :episodes, except: [:create, :new] do
-      resource :media, only: [:show, :update], controller: :episode_media
-      get "media_status", to: "episode_media#status"
-      resource :player, only: :show, controller: :episode_player
-    end
-
-    resource :podcast_switcher, only: [:show, :create], controller: :podcast_switcher
-    get "/uploads/signature", to: "uploads#signature", as: :uploads_signature
-
-    mount PrxAuth::Rails::Engine => "/auth", :as => "prx_auth_engine"
-    get "sessions/logout", to: "application#logout", as: :logout
-    get "sessions/refresh", to: "application#refresh", as: :refresh
+  resources :podcasts do
+    resource :engagement, only: [:show, :update], controller: :podcast_engagement
+    resource :player, only: :show, controller: :podcast_player
+    resources :imports, only: [:index, :show, :create]
+    resource :planner, only: [:show, :create], controller: :podcast_planner
+    resources :feeds, except: [:edit]
+    resources :episodes, only: [:index, :create, :new]
   end
+
+  resources :episodes, except: [:create, :new] do
+    resource :media, only: [:show, :update], controller: :episode_media
+    get "media_status", to: "episode_media#status"
+    resource :player, only: :show, controller: :episode_player
+  end
+
+  resource :podcast_switcher, only: [:show, :create], controller: :podcast_switcher
+  get "/uploads/signature", to: "uploads#signature", as: :uploads_signature
+
+  mount PrxAuth::Rails::Engine => "/auth", :as => "prx_auth_engine"
+  get "sessions/logout", to: "application#logout", as: :logout
+  get "sessions/refresh", to: "application#refresh", as: :refresh
 
   namespace :api do
     scope ":api_version", api_version: "v1", defaults: {format: "hal"} do
