9fde19750024d93f75bb2cc9600279d378f03a91
Rick Peterman <rick.peterman@prx.org>
2023-09-09 05:03:04 -0500

    feat(podcast): refactor embed player form
    - use preview url for embed preview iframe src
    - get embed url and html from post message from preview iframe
    - post message to preview iframe to make config changes
    - improve dynamic form controllers handling of select options help text
    - various updates to labels and help text

--- app/helpers/embed_player_helper.rb
+++ app/helpers/embed_player_helper.rb
@@ -3,6 +3,7 @@ module EmbedPlayerHelper
 
   EMBED_PLAYER_LANDING_PATH = "/listen"
   EMBED_PLAYER_PATH = "/e"
+  EMBED_PLAYER_PREVIEW_PATH = "/preview"
   EMBED_PLAYER_FEED = "uf"
   EMBED_PLAYER_GUID = "ge"
   EMBED_PLAYER_CARD = "ca"
@@ -48,48 +49,73 @@ module EmbedPlayerHelper
     params = {}
     params[EMBED_PLAYER_FEED] = podcast.published_url
 
-    params[EMBED_PLAYER_PLAYLIST] = (options[:all_episodes] == "all") ? options[:all_episodes] : options[:episode_number]
-    params[EMBED_PLAYER_SEASON] = options[:season]
-    params[EMBED_PLAYER_CATEGORY] = options[:category]
+    if options[:all_episodes] === "all"
+      params[EMBED_PLAYER_PLAYLIST] = "all"
+    end
 
-    # TODO: the height styling doesn't really work here in the way Play specifies how it needs to be for a playlist, so leaving this as a TODO for now.
-    # if options[:embed_player_type] == "card" || options[:embed_player_type] == "fixed_card"
-    #   params[EMBED_PLAYER_CARD] = "1"
-    # end
+    if options[:all_episodes] == "number" && options[:episode_number].to_i > 1
+      params[EMBED_PLAYER_PLAYLIST] = options[:episode_number]
+    end
+
+    if !options[:season].to_s.strip.empty? && options[:season].to_i > 0
+      params[EMBED_PLAYER_SEASON] = options[:season]
+    end
+
+    if !options[:category].to_s.strip.empty?
+      params[EMBED_PLAYER_CATEGORY] = options[:category]
+    end
+
+    if options[:embed_player_type] == "card" || options[:embed_player_type] == "fixed_card"
+      params[EMBED_PLAYER_CARD] = "1"
+    end
 
     embed_params(params)
   end
 
   def embed_player_episode_iframe(ep, type = nil, preview = false)
     src = embed_player_episode_url(ep, type, preview)
-    allow = "monetization"
+    allow = "monetization origin"
+    data = { :embed_preview_target => 'embedIframe' }
 
     if type == "card"
-      # TODO: this is NOW working, but I'm not sure how helpful this is to a producer that wishes to embed it.
-      tag.iframe src: src, allow: allow, width: "100%", height: "700", style: "--aspect-ratio: 2/3; width: 100%;"
+      # Responsive cards require a wrapper div around iframe. The wrapper uses padding to determine the size of the area the iframe should fill.
+      tag.div style: "position: relative; height: 0; width: 100%; padding-top: calc(100% + 200px);" do
+        tag.iframe src: src, allow: allow, width: "100%", height: "100%", style: "position: absolute; inset: 0;", data: data
+      end
     elsif type == "fixed_card"
-      tag.iframe src: src, allow: allow, width: "500", height: "700"
+      # Fixxed card heights should always be 200px larger than the width.
+      tag.iframe src: src, allow: allow, width: "500", height: "700", data: data
     else
-      tag.iframe src: src, allow: allow, width: "100%", height: "200"
+      tag.iframe src: src, allow: allow, width: "100%", height: "200", data: data
     end
   end
 
-  def embed_player_podcast_iframe(podcast, options, preview = false)
-    src = embed_player_podcast_url(podcast, options, preview)
+  def embed_player_iframe(options)
+    src = ''
     allow = "monetization"
+    iframeHeight = '200'
+    iframeStyle = ''
+    wrapperStyle = 'line-height: 0;'
 
     if options[:embed_player_type] == "card"
-      # TODO: this is NOW working, but I'm not sure how helpful this is to a producer that wishes to embed it.
-      tag.iframe src: src, allow: allow, width: "100%", height: "700", style: "--aspect-ratio: 2/3; width: 100%;"
-    elsif options[:embed_player_type] == "fixed_card"
-      tag.iframe src: src, allow: allow, width: "500", height: "700"
-    else
-      tag.iframe src: src, allow: allow, width: "100%", height: "200"
+      wrapperStyle = "position: relative; height: 0; width: 100%; padding-top: calc(100% + #{iframeHeight}px); line-height: 0;"
+      iframeHeight = '100%'
+      iframeStyle = "position: absolute; inset: 0;"
+    end
+
+
+    tag.div style: wrapperStyle, data: { :embed_preview_target => 'embedIframeWrapper' } do
+      tag.iframe class: 'bg-light', src: src, allow: allow, width: "100%", height: iframeHeight, style: iframeStyle, data: { :embed_preview_target => 'embedIframe' }
     end
   end
 
   def embed_player_type_options(selected)
-    opts = %w[standard card fixed_card].map { |v| [t("helpers.label.episode.embed_player_types.#{v}"), v] }
+    opts = %w[standard card].map { |v| [t("helpers.label.episode.embed_player_types.#{v}"), v] }
+    options_for_select(opts, selected)
+  end
+
+  def embed_player_theme_options(selected)
+    opts = %w[dark light auto].map { |v| [t("helpers.label.episode.embed_player_themes.#{v}"), v] }
     options_for_select(opts, selected)
   end
 
@@ -106,7 +132,7 @@ module EmbedPlayerHelper
   private
 
   def embed_params(params)
-    "#{play_root}#{EMBED_PLAYER_PATH}?#{params.to_query}"
+    "https://#{ENV["PLAY_HOST"]}#{EMBED_PLAYER_PREVIEW_PATH}?#{params.to_query}"
   end
 
   def enclosure_with_token(ep)
--- app/javascript/controllers/dynamic_form_controller.js
+++ app/javascript/controllers/dynamic_form_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["link"]
+  static targets = ["link", "help"]
+
+  static values = {
+    help: Object
+  }
+
+  updateHelp(event) {
+    if (this.hasHelpValue && this.hasHelpTarget) {
+      this.helpTarget.innerText = this.helpValue[event.target.value]
+    }
+  }
 
   change(event) {
     for (const link of this.linkTargets) {
