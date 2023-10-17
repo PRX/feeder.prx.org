5cf4b69b35a6084edcf5a20731ad0c2d1a08f2ca
cavis <ryancavis@gmail.com>
2023-10-17 13:40:55 -0600

    Fix app access detection

--- app/helpers/current_user_helper.rb
+++ app/helpers/current_user_helper.rb
@@ -4,11 +4,16 @@ module CurrentUserHelper
   end
 
   def current_user_app(name)
-    current_user_apps.values.find do |url|
-      url.include?("#{name}.prx.org") ||
-        url.include?("#{name}.staging.prx.tech") ||
-        url.include?("#{name}.prx.test")
-    end
+    current_user_apps.filter_map do |key, url|
+      # TODO: temporary, as we shuffle/solidify these keys
+      if name == "augury"
+        url if key.downcase.include?(name) || key.downcase.include?("inventory")
+      elsif name == "feeder"
+        url if key.downcase.include?(name) || key.downcase.include?("podcasts")
+      elsif key.downcase.include?(name)
+        url
+      end
+    end.first
   end
 
   def current_user_id_profile
