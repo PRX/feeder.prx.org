2c9f2db3cd4b76e6b67d816c69d2315029653c9e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2023-10-23 17:59:38 -0400

    Update migration to handle changed apple_config.podcast

--- db/migrate/20231011213406_remove_apple_excluded_category.rb
+++ db/migrate/20231011213406_remove_apple_excluded_category.rb
@@ -1,19 +1,23 @@
 class RemoveAppleExcludedCategory < ActiveRecord::Migration[7.0]
   def change
     Apple::Config.all.each do |ac|
-      pub = ac.build_publisher
+      begin
+        pub = ac.build_publisher
 
-      eps =
-        pub.show.podcast_episodes.filter do |ep|
-          next false unless ep.feeder_episode.categories.include?("apple-excluded")
-          ep.feeder_episode.update(categories: ep.feeder_episode.categories - ["apple-excluded"])
+        eps =
+          pub.show.podcast_episodes.filter do |ep|
+            next false unless ep.feeder_episode.categories.include?("apple-excluded")
+            ep.feeder_episode.update(categories: ep.feeder_episode.categories - ["apple-excluded"])
 
-          true
-        end
+            true
+          end
 
-      pub.poll!(eps)
+        pub.poll!(eps)
 
-      Apple::Config.mark_as_delivered!(pub)
+        Apple::Config.mark_as_delivered!(pub)
+      rescue => e
+        Rails.logger.error("Error on RemoveAppleExcludedCategory", {error: e.message, backtrace: e.backtrace[0,2].join("\n")})
+      end
     end
   end
 end
