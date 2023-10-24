37e74b40be42f64437c7a3b6ca7f04773a117067
Sam Vevang <sam.vevang@gmail.com>
2023-10-24 09:29:51 -0500

    Turn down log level for api retry

--- app/models/apple/api.rb
+++ app/models/apple/api.rb
@@ -305,7 +305,7 @@ module Apple
     def retry_bridge_api_operation(bridge_resource, row_operations_ok, row_operation_errs, attempts = 1, batch_size: DEFAULT_BATCH_SIZE)
       return [row_operations_ok, row_operation_errs] if attempts >= ERROR_RETRIES || row_operation_errs.empty?
 
-      row_operation_errs.map { |err| Rails.logger.error("Retrying: #{err.to_json}") }
+      row_operation_errs.map { |err| Rails.logger.info("Retrying: #{err.to_json}") }
 
       # Slice off the api response and retry the row operation
       formatted_error_operations_for_retry = row_operation_errs.map do |r|
