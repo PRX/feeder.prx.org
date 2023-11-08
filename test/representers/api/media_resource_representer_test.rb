82fa20e69d53765de242b02434b378120f814254
cavis <ryancavis@gmail.com>
2023-11-08 09:05:07 -0700

    Fixup test

--- test/representers/api/media_resource_representer_test.rb
+++ test/representers/api/media_resource_representer_test.rb
@@ -4,9 +4,9 @@ describe Api::MediaResourceRepresenter do
   let(:media_resource) { MediaResource.new }
   let(:representer) { Api::MediaResourceRepresenter.new(media_resource) }
 
-  it "includes the href" do
+  it "does not include the href" do
     media_resource.stub(:href, "url") do
-      assert_equal representer.as_json["href"], "url"
+      assert_nil representer.as_json["href"]
     end
   end
 
