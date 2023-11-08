366d0e1cdcb326c4b8c0f2438b734defc0d349d8
cavis <ryancavis@gmail.com>
2023-11-08 09:05:25 -0700

    Make href write-only

--- app/representers/api/media_resource_representer.rb
+++ app/representers/api/media_resource_representer.rb
@@ -2,6 +2,7 @@ class Api::MediaResourceRepresenter < Roar::Decorator
   include Roar::JSON
   include HalApi::Representer::FormatKeys
 
+  property :href, readable: false
   property :file_name
   property :mime_type, as: :type
   property :file_size, as: :size
