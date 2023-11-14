494bfcee428e0b02fcb02a26ad30106f6c61f2ae
cavis <ryancavis@gmail.com>
2023-11-14 12:57:17 -0700

    Just overwrite instead of deleting

--- app/jobs/unlink_job.rb
+++ app/jobs/unlink_job.rb
@@ -2,6 +2,6 @@ class UnlinkJob < ApplicationJob
   queue_as :feeder_default
 
   def perform(key)
-    s3_client.delete_object(bucket: s3_bucket, key: key)
+    s3_client.put_object(bucket: s3_bucket, key: key, body: "")
   end
 end
