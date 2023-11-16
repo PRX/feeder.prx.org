6be98bbff15f2cc89b30fda5bd2956be0e011fc2
cavis <ryancavis@gmail.com>
2023-11-16 15:48:21 -0700

    Fix retrying; allow error retrying (for source ingest errors esp)

--- app/models/media_resource.rb
+++ app/models/media_resource.rb
@@ -165,6 +165,8 @@ class MediaResource < ApplicationRecord
     if %w[started created processing retrying].include?(status)
       last_event = task&.updated_at || updated_at || Time.now
       Time.now - last_event > 100
+    elsif status_error?
+      true
     else
       false
     end
