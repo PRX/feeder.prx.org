3cead644b6c915f0e053369182ef298a8ab0360b
cavis <ryancavis@gmail.com>
2023-12-01 09:06:00 -0700

    Clarify how deprecation will work

--- app/representers/api/media_resource_representer.rb
+++ app/representers/api/media_resource_representer.rb
@@ -2,7 +2,8 @@ class Api::MediaResourceRepresenter < Roar::Decorator
   include Roar::JSON
   include HalApi::Representer::FormatKeys
 
-  property :href, readable: false
+  # TODO: uncomment once the readable href is deprecated
+  # property :href, readable: false
   property :file_name
   property :mime_type, as: :type
   property :file_size, as: :size
