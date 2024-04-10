65b1b4115b4c91bc32b620fdd9723c8a8a3b16eb
cavis <ryancavis@gmail.com>
2024-04-10 17:22:50 -0600

    Alias old associations

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -20,6 +20,7 @@ class Podcast < ApplicationRecord
   serialize :restrictions, JSON
 
   has_one :default_feed, -> { default }, class_name: "Feed", validate: true, autosave: true, inverse_of: :podcast
+  alias_method :public_feed, :default_feed
   has_one :apple_config, class_name: "::Apple::Config", validate: true, autosave: true, inverse_of: :podcast
 
   has_many :episodes, -> { order("published_at desc") }, dependent: :destroy
--- test/factories/apple_config_factory.rb
+++ test/factories/apple_config_factory.rb
@@ -3,12 +3,6 @@ FactoryBot.define do
     publish_enabled { true }
     sync_blocks_rss { true }
     key { build(:apple_key) }
-    podcast
-
-    # set up the private and public feeds
-    before(:create) do |apple_config, evaluator|
-      apple_config.public_feed ||= create(:feed, podcast: evaluator.podcast)
-      apple_config.private_feed ||= create(:feed, podcast: evaluator.podcast)
-    end
+    feed
   end
 end
