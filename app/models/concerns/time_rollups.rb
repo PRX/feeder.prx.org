4223016825a4720dafb9a4183e8809c343deab72
cavis <ryancavis@gmail.com>
2024-04-22 16:38:22 -0600

    Move timezone handling to scope

--- app/models/concerns/time_rollups.rb
+++ app/models/concerns/time_rollups.rb
@@ -4,13 +4,26 @@ module TimeRollups
   extend ActiveSupport::Concern
 
   included do
+    scope :rollup_time_zone, ->(name) do
+      if defined?(@rollup_time_called) && @rollup_time_called
+        raise "rollup_time_zone must be called before rollup_time"
+      elsif !@@rollup_zone_supported
+        raise "time zone not supported for: #{@@rollup_time_col}"
+      end
+
+      @rollup_time_zone = name
+
+      all
+    end
+
     scope :rollup_time, ->(date_part, facets = []) do
       if date_part && %w[hour day week month year].exclude?(date_part.to_s)
         raise "invalid date part: #{date_part}"
       end
 
+      @rollup_time_called = true
       time = @@rollup_time_col
-      zone = @@rollup_zone_attr && try(@@rollup_zone_attr) || "UTC"
+      zone = defined?(@rollup_time_zone) && @rollup_time_zone || "UTC"
       select_time = "DATE_TRUNC('#{date_part}', #{time}, '#{zone}') AS #{time}" if date_part
       select_count = "SUM(#{@@rollup_count_col}) AS #{@@rollup_count_col}"
       selects = (facets + [select_time, select_count]).compact
@@ -26,10 +39,10 @@ module TimeRollups
     scope :rollup_year, ->(*facets) { rollup_time("year", facets) }
     scope :rollup_total, ->(*facets) { rollup_time(nil, facets) }
 
-    def self.time_rollups(time_col, count_col, zone_attr = nil)
+    def self.time_rollups(time_col, count_col, zone_supported = false)
       @@rollup_time_col = time_col
       @@rollup_count_col = count_col
-      @@rollup_zone_attr = zone_attr
+      @@rollup_zone_supported = zone_supported
     end
   end
 end
--- app/models/rollups/hourly_download.rb
+++ app/models/rollups/hourly_download.rb
@@ -2,18 +2,5 @@ class Rollups::HourlyDownload < ActiveRecord::Base
   include TimeRollups
 
   establish_connection :clickhouse
-  time_rollups :hour, :count, :rollup_zone
-
-  def self.rollup_zone(*args)
-    if args.empty?
-      @@rollup_zone if defined?(@@rollup_zone)
-    else
-      @@rollup_zone = args.first
-      self
-    end
-  end
-
-  def self.rollup_zone=(name)
-    @@rollup_zone = name
-  end
+  time_rollups :hour, :count, true
 end
