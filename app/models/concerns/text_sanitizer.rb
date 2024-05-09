982760dea45d05a130ea473d7aae63895220f659
cavis <ryancavis@gmail.com>
2024-05-09 11:45:01 -0600

    Combine fields when importing RSS

--- app/models/concerns/text_sanitizer.rb
+++ app/models/concerns/text_sanitizer.rb
@@ -22,4 +22,16 @@ module TextSanitizer
     return nil if text.blank?
     Loofah.fragment(text).scrub!(:prune).text(encode_special_chars: false)
   end
+
+  def sanitize_keywords(kws, strict)
+    Array(kws).map { |kw| sanitize_keyword(kw, kw.length, strict) }.uniq.reject(&:blank?)
+  end
+
+  def sanitize_keyword(kw, max_length, strict)
+    if strict
+      kw.to_s.downcase.gsub(/[^ a-z0-9_-]/, "").gsub(/\s+/, " ").strip.slice(0, max_length)
+    else
+      kw.strip.slice(0, max_length)
+    end
+  end
 end
