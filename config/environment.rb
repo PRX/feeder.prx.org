e3f1df80e47752c6fb93e99ba51b4f619741c8ea
cavis <ryancavis@gmail.com>
2024-06-06 08:30:10 -0600

    Also disable OTEL for all rails consoles

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require "opentelemetry/sdk"
 # Load the Rails application.
 require_relative "application"
 
-if ENV["OTEL_EXPORTER_OTLP_ENDPOINT"].present?
+if ENV["OTEL_EXPORTER_OTLP_ENDPOINT"].present? && !Rails.const_defined?(:Console)
   OpenTelemetry::SDK.configure do |c|
     c.use_all
   end
