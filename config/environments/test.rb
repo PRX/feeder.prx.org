83bf545f180b0b086ba5f01f3b72e4c11c22e57b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-06-12 16:23:34 -0400

    The cache config is equivalent

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Using spring:
-  config.cache_classes = false
   config.action_view.cache_template_loading = true
 
   # While tests run files are not watched, reloading is not necessary.
