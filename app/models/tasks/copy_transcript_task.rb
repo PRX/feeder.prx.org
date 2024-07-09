b0859232386b5233b75a3cac038a76eb9b9220da
Ube <57785974+radical-ube@users.noreply.github.com>
2024-07-09 12:52:02 -0400

    Port and modify relevant task methods from image_file

---
+++ app/models/tasks/copy_transcript_task.rb
@@ -0,0 +1,47 @@
+class Tasks::CopyTranscriptTask < ::Task
+  def transcript
+    owner
+  end
+
+  def source_url
+    transcript.href
+  end
+
+  def porter_tasks
+    [
+      {
+        Type: "Inspect"
+      },
+      {
+        Type: "Copy",
+        Mode: "AWS/S3",
+        BucketName: ENV["FEEDER_STORAGE_BUCKET"],
+        ObjectKey: porter_escape(transcript.path),
+        ContentType: "REPLACE",
+        Parameters: {
+          CacheControl: "max-age=86400",
+          ContentDisposition: "attachment; filename=\"#{porter_escape(transcript.file_name)}\""
+        }
+      }
+    ]
+  end
+
+  def update_transcript
+    transcript.status = status
+
+    if complete?
+      info = porter_callback_inspect
+      transcript.size = porter_callback_size
+
+      # only return for actual images - not detected images in id3 tags
+      if info[:Transcript] && porter_callback_mime&.starts_with?("transcript/")
+        transcript.format = info[:Transcript][:Format]
+      end
+
+      # change status, if metadata doesn't pass validations
+      transcript.status = "invalid" if transcript.invalid?
+    end
+
+    transcript.save!
+  end
+end
--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -2,4 +2,86 @@ class Transcript < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :episode
+
+  has_one :task, -> { order("id desc") }, as: :owner
+  has_many :tasks, as: :owner
+
+  def published_url
+  end
+
+  def initialize_attributes
+    self.status ||= :created
+    guid
+  end
+
+  def guid
+    self[:guid] ||= SecureRandom.uuid
+    self[:guid]
+  end
+
+  def file_name
+    if original_url.present?
+      File.basename(URI.parse(original_url).path)
+    end
+  end
+
+  def url
+    self[:url] ||= published_url
+  end
+
+  def path
+    URI.parse(url).path.sub(/\A\//, "") if url.present?
+  end
+
+  def href
+    (status_complete? || status_invalid?) ? url : original_url
+  end
+
+  def href=(h)
+    if original_url != h
+      self.original_url = h
+    end
+    original_url
+  end
+
+  def original_url=(url)
+    super
+    if original_url_changed?
+      reset_transcript_attributes
+    end
+    self[:original_url]
+  end
+
+  def reset_transcript_attributes
+    self.format = nil
+    self.status = :created
+  end
+
+  def copy_transcript(force = false)
+    if force || !(status_complete? || task)
+      Tasks::CopyTranscriptTask.create! do |task|
+        task.owner = self
+      end.start!
+    end
+  end
+
+  def retryable?
+    if %w[started created processing retrying].include?(status)
+      last_event = task&.updated_at || updated_at || Time.now
+      Time.now - last_event > 100
+    else
+      status_error?
+    end
+  end
+
+  def retry!
+    if retryable?
+      status_retrying!
+      copy_transcript(true)
+    end
+  end
+
+  def _retry=(_val)
+    retry!
+  end
 end
