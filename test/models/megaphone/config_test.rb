844fb2024690c06f3152336ffbcafb920853b175
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-10-01 18:17:09 -0400

    fix that test with an actual feed

--- test/models/megaphone/config_test.rb
+++ test/models/megaphone/config_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 describe Megaphone::Config do
   describe "#valid?" do
     it "must have required attributes" do
-      config = build(:megaphone_config, feed_id: 123)
+      feed = create(:feed)
+      config = build(:megaphone_config, feed: feed)
       assert_not_nil config.token
       assert_not_nil config.network_id
       assert_not_nil config.feed_id
