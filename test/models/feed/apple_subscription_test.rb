062b77aac574edd6eb3007be7a4ba8694a538bf7
cavis <ryancavis@gmail.com>
2024-10-10 09:20:22 -0600

    Use mp3 instead of flac for apple feeds

--- test/models/feed/apple_subscription_test.rb
+++ test/models/feed/apple_subscription_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 describe Feeds::AppleSubscription do
-  let(:podcast) { create(:podcast) }
-  let(:feed_1) { podcast.default_feed }
+  let(:podcast) { create(:podcast, default_feed: default_feed) }
+  let(:default_feed) { build(:default_feed, audio_format: nil) }
   let(:apple_feed) { build(:apple_feed, podcast: podcast) }
 
   describe "#set_defaults" do
-    let(:default_feed) { build_stubbed(:feed, display_episodes_count: 99) }
+    let(:default_feed) { build_stubbed(:feed, display_episodes_count: 99, audio_format: nil) }
     let(:podcast) { build_stubbed(:podcast, default_feed: default_feed) }
 
     it "sets default values for apple subscription feeds" do
@@ -29,7 +29,7 @@ describe Feeds::AppleSubscription do
         podcast: podcast,
         slug: "foo",
         title: "bar",
-        audio_format: {f: "baz"},
+        audio_format: {f: "flac"},
         display_episodes_count: 88,
         include_zones: ["something"],
         private: false,
@@ -38,7 +38,7 @@ describe Feeds::AppleSubscription do
 
       assert_equal "foo", f.slug
       assert_equal "bar", f.title
-      assert_equal ({"f" => "baz"}), f.audio_format
+      assert_equal "flac", f.audio_format[:f]
       assert_equal 88, f.display_episodes_count
       assert_equal ["something"], f.include_zones
       assert_equal true, f.private
@@ -49,12 +49,34 @@ describe Feeds::AppleSubscription do
     end
   end
 
+  describe "#guess_audio_format" do
+    it "uses the default feed format" do
+      default_feed.audio_format = {f: "mp3", b: 192, c: 1, s: 48000}
+      assert_equal default_feed.audio_format, apple_feed.guess_audio_format
+    end
+
+    it "uses the max episode format" do
+      c1 = build(:content, mime_type: "audio/mpeg", bit_rate: 192, channels: 1, sample_rate: 22050)
+      c2 = build(:content, mime_type: "audio/mpeg", bit_rate: 64, channels: 2, sample_rate: 44100)
+      c3 = build(:content, mime_type: "audio/wav", bit_rate: 16, channels: 1, sample_rate: 48000)
+      create(:episode, podcast: podcast, contents: [c1])
+      create(:episode, podcast: podcast, contents: [c2])
+      create(:episode, podcast: podcast, contents: [c3])
+
+      assert_nil default_feed.audio_format
+      assert_equal ({f: "mp3", b: 192, c: 2, s: 44100}).with_indifferent_access, apple_feed.guess_audio_format
+    end
+
+    it "falls back to the default format" do
+      assert_equal Feeds::AppleSubscription::DEFAULT_AUDIO_FORMAT, apple_feed.guess_audio_format
+    end
+  end
+
   describe "#valid?" do
     it "cannot change the default properties once saved" do
       apple_feed.title = "new apple feed"
       apple_feed.slug = "new-apple-slug"
       apple_feed.file_name = "new_file.xml"
-      apple_feed.audio_format = {f: "flac", b: 16, c: 2, s: 44100}
       assert apple_feed.valid?
       apple_feed.save!
 
@@ -71,11 +93,6 @@ describe Feeds::AppleSubscription do
       apple_feed.file_name = "changed_file_name.xml"
       refute apple_feed.valid?
       apple_feed.file_name = "new_file.xml"
-      assert apple_feed.valid?
-
-      apple_feed.audio_format = {f: "wav", b: 128, c: 2, s: 44100}
-      refute apple_feed.valid?
-      apple_feed.audio_format = {f: "flac", b: 16, c: 2, s: 44100}
       assert apple_feed.valid?
     end
 
@@ -105,7 +122,7 @@ describe Feeds::AppleSubscription do
       assert apple_feed.apple_config.valid?
 
       apple_feed.save!
-      assert_equal feed_1, apple_feed.apple_config.public_feed
+      assert_equal default_feed, apple_feed.apple_config.public_feed
     end
   end
 
@@ -113,7 +130,7 @@ describe Feeds::AppleSubscription do
     it "returns true if the feed has apple credentials" do
       apple_feed.save!
 
-      refute feed_1.publish_to_apple?
+      refute default_feed.publish_to_apple?
       assert apple_feed.publish_to_apple?
     end
 
@@ -124,8 +141,8 @@ describe Feeds::AppleSubscription do
     end
 
     it "returns false if the feed is not an Apple Subscription feed" do
-      refute_equal feed_1.type, "Feeds::AppleSubscription"
-      refute feed_1.publish_to_apple?
+      refute_equal default_feed.type, "Feeds::AppleSubscription"
+      refute default_feed.publish_to_apple?
     end
   end
 end
