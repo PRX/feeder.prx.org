6556f1984447265191f5d43c891d34dbbe147158
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-10-23 17:36:28 -0400

    Have some feeds locked for editing

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -24,7 +24,7 @@ class FeedsController < ApplicationController
   end
 
   def get_apple_show_options(feed)
-    if feed.apple? && feed.apple_show_id.blank? && feed.apple_config
+    if !feed.edit_locked? && feed.apple? && feed.apple_show_id.blank? && feed.apple_config
       used_ids = used_apple_show_ids(feed)
       api = Apple::Api.from_apple_config(feed.apple_config)
       shows_json = Apple::Show.apple_shows_json(api) || []
