cc24147ee281437c7d5e52a5e07cb8b7b9d357ff
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-10-29 18:33:07 -0400

    Set apple ids on feeds

--- db/migrate/20241023012600_add_apple_show_id_to_feeds.rb
+++ db/migrate/20241023012600_add_apple_show_id_to_feeds.rb
@@ -2,5 +2,14 @@ class AddAppleShowIdToFeeds < ActiveRecord::Migration[7.2]
   def change
     add_column :feeds, :apple_show_id, :string
     add_index :feeds, :apple_show_id
+
+    reversible do |dir|
+      dir.up do
+        Feeds::AppleSubscription.all.each do |feed|
+          apple_id = feed.apple_sync_log&.external_id
+          feed.update_attribute!(:apple_show_id, apple_id)
+        end
+      end
+    end
   end
 end
