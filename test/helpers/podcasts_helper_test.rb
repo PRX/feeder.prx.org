35544c503810115c5dfe12d55ab91cd153e0ddee
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-11-16 14:04:09 -0500

    More tests, linting

---
+++ test/helpers/podcasts_helper_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class TestHelper
+  include PodcastsHelper
+end
+
+describe PodcastsHelper do
+  let(:helper) { TestHelper.new }
+  let(:podcast) { build(:podcast, description: "description") }
+  let(:feed1) { podcast.default_feed }
+  let(:feed2) { build(:feed, private: true, podcast: podcast, slug: "adfree", footer: "footer", description: "different") }
+  let(:episode) { build(:episode, description: "description") }
+
+  describe "#feed_description" do
+    it "gets the feed or podcast description" do
+      assert_equal "description", feed_description(feed1, podcast)
+      assert_equal "different", feed_description(feed2, podcast)
+    end
+  end
+
+  describe "#episode_description" do
+    it "gets the episode description with a footer" do
+      assert_equal "description", episode_description(episode, feed1)
+      assert_equal "description\n\nfooter", episode_description(episode, feed2)
+    end
+  end
+end
