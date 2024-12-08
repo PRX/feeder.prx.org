74c371ab603205eceacf615e893184a625446a04
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-12-08 16:55:03 -0500

    Make migration reversible

--- db/migrate/20241127171040_add_integration_to_sync_logs.rb
+++ db/migrate/20241127171040_add_integration_to_sync_logs.rb
@@ -1,5 +1,5 @@
 class AddIntegrationToSyncLogs < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :sync_logs, :integration, :integer
 
     execute(<<~SQL
@@ -10,5 +10,12 @@ class AddIntegrationToSyncLogs < ActiveRecord::Migration[7.2]
 
     remove_index :sync_logs, [:feeder_type, :feeder_id], unique: true
     add_index :sync_logs, [:integration, :feeder_type, :feeder_id], unique: true
+  end
+
+  def down
+    remove_index :sync_logs, [:integration, :feeder_type, :feeder_id]
+    remove_column :sync_logs, :integration
+
+    add_index :sync_logs, [:feeder_type, :feeder_id], unique: true
   end
 end
