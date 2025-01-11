9a63fd22d0650272b1386998cabd5640f879ce8c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2025-01-11 16:59:16 -0500

    Set token for megaphone feed, better default values

--- app/models/feeds/megaphone_feed.rb
+++ app/models/feeds/megaphone_feed.rb
@@ -1,11 +1,22 @@
 class Feeds::MegaphoneFeed < Feed
+  DEFAULT_TITLE = "Megaphone Integration"
+  DEFAULT_SLUG = "megaphone"
+
   has_one :megaphone_config, class_name: "::Megaphone::Config", dependent: :destroy, autosave: true, validate: true, inverse_of: :feed
+
+  validate :must_have_token
 
   after_initialize :set_defaults
 
   alias_method :config, :megaphone_config
 
   accepts_nested_attributes_for :megaphone_config, allow_destroy: true, reject_if: :all_blank
+
+  def must_have_token
+    if tokens.blank?
+      errors.add(:tokens, "must have a token")
+    end
+  end
 
   def self.model_name
     Feed.model_name
@@ -16,8 +27,9 @@ class Feeds::MegaphoneFeed < Feed
   end
 
   def set_defaults
-    self.slug = "prx-#{id}"
-    self.title = podcast&.title
+    self.slug = DEFAULT_SLUG
+    self.title = DEFAULT_TITLE
+    self.tokens = [FeedToken.new(label: DEFAULT_TITLE)] if tokens.empty?
     self.private = true
 
     super
