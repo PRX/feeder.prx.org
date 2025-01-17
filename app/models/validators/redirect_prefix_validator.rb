816167c225107a441912e43edd0a634325db1bce
cavis <ryancavis@gmail.com>
2025-01-17 13:45:03 -0700

    Validate enclosure prefixes with HEAD request

---
+++ app/models/validators/redirect_prefix_validator.rb
@@ -0,0 +1,39 @@
+class RedirectPrefixValidator < ActiveModel::EachValidator
+  TEST_AGENT = "PRX-Feeder-Validator/1.0 (Rails-#{Rails.env})"
+  TEST_URL = "dovetail.prxu.org/zero.mp3"
+  DEFAULT_MAX_JUMPS = 10
+
+  def validate_each(record, attribute, value)
+    if value.present? && record.changes[attribute].present?
+      success, jumps = head_request(File.join(value, TEST_URL))
+      max_jumps = options[:max_jumps] || DEFAULT_MAX_JUMPS
+
+      if !success
+        record.errors.add(attribute, :unreachable, message: "prefix not reachable")
+      elsif jumps > max_jumps
+        record.errors.add(attribute, :too_many_redirects, message: "too many redirects")
+      end
+    end
+  end
+
+  private
+
+  def head_request(uri_str, jumps = 0)
+    uri = URI.parse(uri_str)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == "https"
+    req = Net::HTTP::Head.new(uri)
+    req["User-Agent"] = TEST_AGENT
+    res = http.request(req)
+
+    if res.is_a?(Net::HTTPRedirection)
+      head_request(res[:location], jumps + 1)
+    elsif res.is_a?(Net::HTTPSuccess)
+      [true, jumps]
+    else
+      [false, jumps]
+    end
+  rescue URI::InvalidURIError, Socket::ResolutionError
+    [false, jumps]
+  end
+end
---
+++ test/models/validators/redirect_prefix_validator_test.rb
@@ -0,0 +1,90 @@
+require "test_helper"
+
+describe RedirectPrefixValidator do
+  let(:feed) { build_stubbed(:feed, enclosure_prefix: nil) }
+
+  it "allows nil/blank" do
+    assert feed.valid?
+
+    feed.enclosure_prefix = ""
+    assert feed.valid?
+  end
+
+  it "follows prefix redirects" do
+    one = "https://one.com/track/two.com/redirect.mp3/three.com/123456/dovetail.prxu.org/zero.mp3"
+    two = "https://two.com/redirect.mp3/three.com/123456/dovetail.prxu.org/zero.mp3"
+    three = "https://three.com/123456/dovetail.prxu.org/zero.mp3"
+    four = "https://dovetail.prxu.org/zero.mp3"
+
+    r1 = stub_request(:head, one).to_return(status: 302, headers: {location: two})
+    r2 = stub_request(:head, two).to_return(status: 301, headers: {location: three})
+    r3 = stub_request(:head, three).to_return(status: 302, headers: {location: four})
+    r4 = stub_request(:head, four).to_return(status: 200)
+
+    feed.enclosure_prefix = one.sub("dovetail.prxu.org/zero.mp3", "")
+    assert feed.valid?
+    assert_requested(r1)
+    assert_requested(r2)
+    assert_requested(r3)
+    assert_requested(r4)
+  end
+
+  it "validates successful responses" do
+    one = "https://one.com/track/stuff/two.com/redirect/dovetail.prxu.org/zero.mp3"
+    two = "https://two.com/redirect/dovetail.prxu.org/zero.mp3"
+
+    stub_request(:head, one).to_return(status: 302, headers: {location: two})
+    stub_request(:head, two).to_return(status: 404)
+
+    feed.enclosure_prefix = one.sub("/dovetail.prxu.org/zero.mp3", "")
+    assert feed.invalid?
+    assert_includes feed.errors[:enclosure_prefix], "prefix not reachable"
+  end
+
+  it "validates duplicate https" do
+    one = "https://https://two.com/redirect/dovetail.prxu.org/zero.mp3"
+
+    # can't really produce this error via webmock
+    WebMock.allow_net_connect!
+
+    feed.enclosure_prefix = one.sub("/dovetail.prxu.org/zero.mp3", "")
+    assert feed.invalid?
+    assert_includes feed.errors[:enclosure_prefix], "prefix not reachable"
+
+    WebMock.disable_net_connect!
+  end
+
+  it "validates bad domains" do
+    one = "https://one.com/track/stuff/two.com/redirect/dovetail.prxu.org/zero.mp3"
+    two = "https://two.com/redirect/dovetail.prxu.org/zero.mp3"
+
+    stub_request(:head, one).to_return(status: 302, headers: {location: two})
+    stub_request(:head, two).to_raise(Socket::ResolutionError)
+
+    feed.enclosure_prefix = one.sub("/dovetail.prxu.org/zero.mp3", "")
+    assert feed.invalid?
+    assert_includes feed.errors[:enclosure_prefix], "prefix not reachable"
+  end
+
+  it "validates too many redirects" do
+    one = "https://one.com/track/two.com/redirect.mp3/three.com/123456/dovetail.prxu.org/zero.mp3"
+    two = "https://two.com/redirect.mp3/three.com/123456/dovetail.prxu.org/zero.mp3"
+    three = "https://three.com/123456/dovetail.prxu.org/zero.mp3"
+    four = "https://dovetail.prxu.org/zero.mp3"
+
+    r1 = stub_request(:head, one).to_return(status: 302, headers: {location: two})
+    r2 = stub_request(:head, two).to_return(status: 301, headers: {location: three})
+    r3 = stub_request(:head, three).to_return(status: 302, headers: {location: four})
+    r4 = stub_request(:head, four).to_return(status: 200)
+
+    # override validation options
+    feed.enclosure_prefix = one.sub("/dovetail.prxu.org/zero.mp3", "")
+    validator = RedirectPrefixValidator.new(attributes: [:enclosure_prefix], max_jumps: 2)
+    validator.validate(feed)
+    assert_includes feed.errors[:enclosure_prefix], "too many redirects"
+    assert_requested(r1)
+    assert_requested(r2)
+    assert_requested(r3)
+    assert_requested(r4)
+  end
+end
