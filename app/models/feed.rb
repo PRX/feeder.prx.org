e1dd97bd42aa03f3a633b6694c241f2cfa9c21ed
cavis <ryancavis@gmail.com>
2025-02-12 09:18:27 -0700

    Guarantee order of itunes categories

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -32,7 +32,7 @@ class Feed < ApplicationRecord
 
   has_many :feed_images, -> { order("created_at DESC") }, autosave: true, dependent: :destroy, inverse_of: :feed
   has_many :itunes_images, -> { order("created_at DESC") }, autosave: true, dependent: :destroy, inverse_of: :feed
-  has_many :itunes_categories, validate: true, autosave: true, dependent: :destroy
+  has_many :itunes_categories, -> { order("created_at ASC") }, validate: true, autosave: true, dependent: :destroy
 
   has_one :apple_sync_log, -> { feeds }, foreign_key: :feeder_id, class_name: "SyncLog"
 
