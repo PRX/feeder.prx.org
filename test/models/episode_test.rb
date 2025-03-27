81ef15a60e4a35ed433ab5dc8488088e47e5d2f5
Sam Vevang <sam.vevang@gmail.com>
2025-03-27 09:00:49 -0500

    Use multibyte character

--- test/models/episode_test.rb
+++ test/models/episode_test.rb
@@ -95,7 +95,7 @@ describe Episode do
     e.title = "a" * 121
     refute e.valid?
 
-    e.title = "a" * 119 + "'"
+    e.title = "a" * 119 + "’"
     refute e.valid?
 
     e.strict_validations = false
