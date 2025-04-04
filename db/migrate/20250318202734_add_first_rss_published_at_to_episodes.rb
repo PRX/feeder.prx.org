b0a9e63a2f03e377bdfff5cd10aa3aa783567b50
Ube <57785974+radical-ube@users.noreply.github.com>
2025-04-04 12:25:50 -0400

    Change to select method for query efficiency

--- db/migrate/20250318202734_add_first_rss_published_at_to_episodes.rb
+++ db/migrate/20250318202734_add_first_rss_published_at_to_episodes.rb
@@ -3,7 +3,7 @@ class AddFirstRssPublishedAtToEpisodes < ActiveRecord::Migration[7.2]
     add_column :episodes, :first_rss_published_at, :timestamp
 
     default_feeds = EpisodesFeed.where(feed_id: Feed.default)
-    episodes_in_default_feeds = Episode.published.where(id: default_feeds.pluck(:episode_id))
+    episodes_in_default_feeds = Episode.published.where(id: default_feeds.select(:episode_id))
     episodes_in_default_feeds.update_all("first_rss_published_at = published_at")
   end
 end
