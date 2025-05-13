a7d6473f98ba02d075472c28a353faa54482f894
Andrew Kuklewicz <andrew@beginsinwonder.com>
2025-05-13 09:17:25 -0400

    no more uncut on the public api

--- app/representers/api/episode_representer.rb
+++ app/representers/api/episode_representer.rb
@@ -48,8 +48,6 @@ class Api::EpisodeRepresenter < Api::BaseRepresenter
     decorator: Api::MediaResourceRepresenter,
     class: MediaResource
 
-  property :uncut, decorator: Api::UncutRepresenter, class: Uncut
-
   property :image, decorator: Api::ImageRepresenter, class: EpisodeImage
 
   def self_url(episode)
