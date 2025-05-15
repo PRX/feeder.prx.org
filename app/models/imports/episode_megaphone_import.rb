6ebf504d2f13b9216c6207749c926a4b3ca66498
Andrew Kuklewicz <andrew@beginsinwonder.com>
2025-05-15 10:36:24 -0400

    Megaphone podcast import, start on episodes

---
+++ app/models/imports/episode_megaphone_import.rb
@@ -0,0 +1,134 @@
+class EpisodeMegaphoneImport < EpisodeImport
+  store :config, accessors: [:entry, :audio], coder: JSON
+
+  validates :guid, presence: true
+
+  def set_defaults
+    super
+    self.audio ||= {files: []}
+  end
+
+  def import!
+    status_started!
+    # set_audio_metadata!
+
+    status_importing!
+    create_or_update_episode!
+    # set_file_resources!
+
+    # episode.save!
+    # episode.copy_media
+
+    status_complete!
+  rescue => err
+    status_error!
+    raise err
+  end
+
+  def audio_content_params
+    audio&.fetch("files")&.each_with_index&.map do |url, index|
+      {position: index, original_url: url}
+    end
+  end
+
+  def image_contents_params
+    if (image = entry[:itunes_image])
+      {original_url: image}
+    end
+  end
+
+  def set_audio_metadata!
+    audio_files = entry_audio_files(entry)
+    update!(audio: audio_files)
+  end
+
+  def set_file_resources!
+    content = audio_content_params
+    episode.media = content
+    episode.segment_count = content&.size
+    episode.image = image_contents_params
+    episode.save!
+    episode.images.reset
+    episode.contents.reset
+  end
+
+  def entry_audio_files(entry)
+    if config[:audio] && config[:audio][entry[:entry_id]]
+      {files: config[:audio][entry[:entry_id]] || []}
+    elsif (enclosure = enclosure_url(entry))
+      {files: [enclosure]}
+    end
+  end
+
+  def create_or_update_episode!
+    lookup = Episode.where(podcast_id: podcast.id).find_by_item_guid(guid)
+
+    if lookup.present?
+      self.episode = lookup
+    else
+      self.episode ||= Episode.new(podcast: podcast)
+    end
+
+    update_episode_with_entry!
+
+    episode.save!
+  end
+
+  def update_episode_with_entry!
+    episode.clean_title = entry[:itunes_title]
+    episode.description = entry_description(entry)
+    episode.episode_number = entry[:itunes_episode]
+    episode.published_at = entry[:published]
+    episode.season_number = entry[:itunes_season]
+    episode.subtitle = clean_string(episode_short_desc(entry))
+    episode.title = clean_title(entry[:title])
+
+    episode.author = person(entry[:itunes_author] || entry[:author] || entry[:creator])
+    episode.block = (clean_string(entry[:itunes_block]) == "yes")
+    episode.explicit = explicit(entry[:itunes_explicit])
+    episode.original_guid = clean_string(entry[:entry_id])
+    episode.is_closed_captioned = closed_captioned?(entry)
+    episode.is_perma_link = entry[:is_perma_link]
+    episode.position = entry[:itunes_order]
+    episode.url = episode_url(entry)
+    episode.itunes_type = entry[:itunes_episode_type] unless entry[:itunes_episode_type].blank?
+
+    # categories setter does the work of sanitizing these
+    cats = Array(entry[:categories])
+    keys = (entry[:itunes_keywords] || "").split(",")
+    episode.categories = cats + keys
+
+    episode
+  end
+
+  def entry_description(entry)
+    atr = entry_description_attribute(entry)
+    clean_text(entry[atr])
+  end
+
+  def entry_description_attribute(entry)
+    [:content, :description, :itunes_summary, :itunes_subtitle, :title].find { |d| !entry[d].blank? }
+  end
+
+  def title
+    clean_title(entry[:title])
+  end
+
+  def episode_url(entry)
+    url = clean_string(entry[:feedburner_orig_link] || entry[:url] || entry[:link])
+    if /libsyn\.com/.match?(url)
+      url = nil
+    end
+    url
+  end
+
+  def closed_captioned?(entry)
+    (clean_string(entry[:itunes_is_closed_captioned]) == "yes")
+  end
+
+  def episode_short_desc(item)
+    [item[:itunes_subtitle], item[:description], item[:title]].find do |field|
+      !field.blank? && field.split.length < 50
+    end
+  end
+end
--- app/models/imports/podcast_megaphone_import.rb
+++ app/models/imports/podcast_megaphone_import.rb
@@ -1,11 +1,16 @@
 require "feedjira"
 
 class PodcastMegaphoneImport < PodcastImport
-  store :config, accessors: [:megaphone_podcast_id], coder: JSON
+  store :config, accessors: [:megaphone_podcast_id, :megaphone_podcast, :override_enclosures, :new_episodes_only], coder: JSON
 
   has_many :episode_imports, dependent: :destroy, class_name: "EpisodeMegaphoneImport", foreign_key: :podcast_import_id
 
+  attr_accessor :megaphone_feed
+
   def set_defaults
+    @megaphone_episodes = []
+    self.new_episodes_only ||= false
+    self.override_enclosures ||= false
     super
   end
 
@@ -28,14 +33,98 @@ class PodcastMegaphoneImport < PodcastImport
   end
 
   def create_or_update_podcast!
-    self.podcast ||= Podcast.new
+    self.megaphone_podcast ||= find_megaphone_podcast
+
+    podcast.assign_attributes(**build_podcast_attributes)
+    if podcast.invalid?
+      podcast.restore_attributes(podcast.errors.attribute_names)
+    end
+    podcast.save!
+    update_images
+
+    megaphone_podcast.update_sync_log
+
+    podcast
+  end
+
+  def find_megaphone_podcast(mf = megaphone_feed, mpid = megaphone_podcast_id)
+    Megaphone::Podcast.find_megaphone_podcast(mf, mpid)
+  end
+
+  def update_images
+    default_feed = podcast.default_feed
+
+    if megaphone_podcast.background_image_file_url.present?
+      default_feed.itunes_image = megaphone_podcast.background_image_file_url
+      default_feed.save!
+    end
+
+    default_feed.itunes_images.reset
+    default_feed.feed_images.reset
+
+    default_feed.copy_media
+  end
+
+  def build_podcast_attributes
+    podcast_attributes = {}
+    podcast_attributes[:title] = megaphone_podcast.title
+    podcast_attributes[:subtitle] = megaphone_podcast.subtitle || megaphone_podcast.summary || megaphone_podcast.title
+    podcast_attributes[:description] = megaphone_podcast.summary
+    podcast_attributes[:itunes_categories] = parse_itunes_categories(megaphone_podcast.itunes_categories)
+    podcast_attributes[:language] = podcast_language(megaphone_podcast.language)
+    podcast_attributes[:link] = megaphone_podcast.link
+    podcast_attributes[:copyright] = megaphone_podcast.copyright
+    podcast_attributes[:author_name] = megaphone_podcast.author
+    podcast_attributes[:explicit] = megaphone_podcast.explicit
+    podcast_attributes[:owner_name] = megaphone_podcast.owner_name
+    podcast_attributes[:owner_email] = megaphone_podcast.owner_email
+    podcast_attributes[:display_episodes_count] = megaphone_podcast.episode_limit
+    podcast_attributes[:itunes_type] = megaphone_podcast.podcast_type
+
+    podcast_attributes
+  end
+
+  def podcast_language(language)
+    if language == "en"
+      "en-us"
+    elsif language == "es"
+      "es-mx"
+    else
+      language
+    end
   end
 
   def megaphone_feed
-    return nil if podcast.blank?
-    Feeds::MegaphoneFeed.where(podcast: podcast).first
+    @megaphone_feed ||= Feeds::MegaphoneFeed.where(podcast: podcast).first
+  end
+
+  # returns a paged collection of episodes
+  def megaphone_episodes
+    @megaphone_episodes ||= megaphone_podcast.episodes
   end
 
   def create_or_update_episode_imports!
+    update(feed_episode_count: megaphone_episodes.total)
+
+    # optionally skip existing
+    existing_guids = Episode.where(podcast_id: podcast_id).map(&:item_guid) if new_episodes_only
+
+    # cleanup existing dups - they may be recreated later
+    episode_imports.status_duplicate.destroy_all
+
+    # we'll update existing episode imports, instead of creating new
+    existing = episode_imports.map { |ei| [ei.guid, ei] }.to_h
+
+    # top-most guids win, others are marked dup
+    guids = []
+    to_import = []
+  end
+
+  def import_existing
+    !new_episodes_only
+  end
+
+  def import_existing=(val)
+    self.new_episodes_only = !ActiveModel::Type::Boolean.new.cast(val)
   end
 end
