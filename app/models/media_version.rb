3bbb88392d33ef6995e831a7bb4e77c93f041d1b
cavis <ryancavis@gmail.com>
2025-09-03 09:33:13 -0600

    Rely on AR aliases for both episodes index/show

--- app/models/media_version.rb
+++ app/models/media_version.rb
@@ -4,13 +4,7 @@ class MediaVersion < ApplicationRecord
   has_many :media_version_resources, dependent: :destroy
   has_many :media_resources, -> { with_deleted.order("position ASC, created_at DESC") }, through: :media_version_resources
 
-  scope :latest, ->(episode_ids = nil) do
-    if episode_ids.present?
-      where("id IN (SELECT MAX(id) FROM media_versions WHERE episode_id IN (?) GROUP BY episode_id)", episode_ids)
-    else
-      where("id IN (SELECT MAX(id) FROM media_versions GROUP BY episode_id)")
-    end
-  end
+  scope :latest, -> { where("id IN (SELECT max(id) FROM media_versions mv WHERE mv.episode_id = media_versions.episode_id)") }
 
   validates :media_version_resources, length: {minimum: 1}
 end
