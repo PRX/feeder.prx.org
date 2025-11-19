8eae629dbdc1d345e3894f44d17e72ff2150872d
Ube <57785974+radical-ube@users.noreply.github.com>
2025-11-19 11:37:39 -0500

    Remove podcast and episode metrics specific tabs in UI

--- app/javascript/controllers/apex_toggles_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import ApexCharts from "apexcharts"
-
-export default class extends Controller {
-  toggleSeries(event) {
-    ApexCharts.exec(event.params.id, "toggleSeries", event.params.series)
-  }
-}
