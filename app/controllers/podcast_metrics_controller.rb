5a687729e687c6f962abe5d5aa36f0d171ba93b5
Ube <57785974+radical-ube@users.noreply.github.com>
2025-12-05 15:47:03 -0500

    Add one more guard

--- app/controllers/podcast_metrics_controller.rb
+++ app/controllers/podcast_metrics_controller.rb
@@ -219,7 +219,7 @@ class PodcastMetricsController < ApplicationController
   end
 
   def calculate_episode_trend(episode, prev_episode)
-    return nil unless episode.in_default_feed? && prev_episode.present?
+    return nil unless episode.in_default_feed? && episode.first_rss_published_at.present? && prev_episode.present?
     return nil if (episode.first_rss_published_at + 1.day) > Time.now
 
     ep_dropday_sum = episode_dropday_query(episode)
