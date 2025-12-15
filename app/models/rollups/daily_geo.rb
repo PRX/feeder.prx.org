46abf24533f790c6578245bc76162cc8764e816c
Ube <57785974+radical-ube@users.noreply.github.com>
2025-12-15 13:16:34 -0500

    Fix country labels

--- app/models/rollups/daily_geo.rb
+++ app/models/rollups/daily_geo.rb
@@ -1,3 +1,15 @@
 class Rollups::DailyGeo < ActiveRecord::Base
   establish_connection :clickhouse
+
+  def country
+    ISO3166::Country[country_code] || "Other"
+  end
+
+  def country_label
+    if country_code == "Other"
+      "Other"
+    else
+      country.iso_short_name
+    end
+  end
 end
