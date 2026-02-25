9112091b2cae8ed812d46af6484064c7e8670f7f
cavis <ryancavis@gmail.com>
2026-02-25 12:13:25 -0700

    Include deleted; touch parent record

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
   enum :role, %w[host producer guest].to_enum_h, prefix: true, allow_nil: true
 
-  belongs_to :owner, polymorphic: true, optional: true
+  belongs_to :owner, -> { with_deleted }, polymorphic: true, optional: true, touch: true
 
   validates :name, presence: true
   validates :href, http_url: true
