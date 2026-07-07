9805619a3683ce0f053331fb1626251c43d40fc5
cavis <ryancavis@gmail.com>
2026-07-07 13:18:00 -0600

    Stub in tests

--- app/models/hls_video.rb
+++ app/models/hls_video.rb
@@ -1,5 +1,4 @@
 class HlsVideo < MediaResource
   validates :medium, inclusion: {in: %w[video]}, if: :status_complete?
   validates :duration, numericality: {greater_than: 0}, if: :status_complete?
-  validate :validate_segmentation
 end
