3d96272336e6beb68d3f7bab84b10a1b7da532a9
Sam Vevang <sam.vevang@gmail.com>
2026-07-08 15:30:45 -0500

    Make sync_logs index swap reversible

--- db/migrate/20260708000003_add_apple_show_id_to_sync_logs.rb
+++ db/migrate/20260708000003_add_apple_show_id_to_sync_logs.rb
@@ -4,7 +4,10 @@ class AddAppleShowIdToSyncLogs < ActiveRecord::Migration[7.2]
   def change
     add_column :sync_logs, :apple_show_id, :string
 
-    remove_index :sync_logs, name: "index_sync_logs_on_integration_and_feeder_type_and_feeder_id"
+    remove_index :sync_logs,
+      [:integration, :feeder_type, :feeder_id],
+      unique: true,
+      name: "index_sync_logs_on_integration_and_feeder_type_and_feeder_id"
 
     add_index :sync_logs,
       [:integration, :feeder_type, :feeder_id],
