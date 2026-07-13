8c5a78f7c02f3a658b801b444b5d9026b179f5c4
cavis <ryancavis@gmail.com>
2026-07-13 11:10:40 -0600

    Bit o tests

--- test/representers/api/auth/podcast_representer_test.rb
+++ test/representers/api/auth/podcast_representer_test.rb
@@ -5,6 +5,15 @@ describe Api::Auth::PodcastRepresenter do
   let(:representer) { Api::Auth::PodcastRepresenter.new(podcast) }
   let(:json) { JSON.parse(representer.to_json) }
 
+  it "has a feeds property" do
+    assert json.key?("feeds")
+    assert_equal 1, json["feeds"].count
+    assert_equal podcast.default_feed.id, json["feeds"][0]["id"]
+    assert_equal "Default RSS Feed", json["feeds"][0]["label"]
+    assert_equal false, json["feeds"][0]["private"]
+    assert_nil json["feeds"][0]["slug"]
+  end
+
   it "has authorized links" do
     assert_equal json["_links"]["self"]["href"], "/api/v1/authorization/podcasts/#{podcast.id}"
     assert_match("/api/v1/authorization/podcasts/#{podcast.id}/episodes", json["_links"]["prx:episodes"]["href"])
