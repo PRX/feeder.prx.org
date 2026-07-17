ff09d8f622910ddf004360e9635c7d333f1ac555
Sam Vevang <sam.vevang@gmail.com>
2026-07-17 11:25:36 -0500

    Expand show identity backfill to Apple delivery state

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_05_000001) do
     t.datetime "updated_at", null: false
     t.string "vendor_id", null: false
     t.string "apple_episode_id", null: false
+    t.string "apple_show_id"
+    t.index ["apple_show_id"], name: "index_apple_podcast_containers_on_apple_show_id"
     t.index ["episode_id"], name: "index_apple_podcast_containers_on_episode_id", unique: true
     t.index ["external_id"], name: "index_apple_podcast_containers_on_external_id", unique: true
   end
@@ -258,6 +260,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_05_000001) do
     t.integer "asset_processing_attempts", default: 0, null: false
     t.boolean "uploaded", default: false
     t.integer "integration"
+    t.string "apple_show_id"
+    t.index ["apple_show_id"], name: "index_integrations_episode_delivery_statuses_on_apple_show_id"
     t.index ["episode_id", "created_at"], name: "index_apple_episode_delivery_statuses_on_episode_id_created_at", include: ["delivered", "id"]
     t.index ["episode_id"], name: "index_integrations_episode_delivery_statuses_on_episode_id"
   end
