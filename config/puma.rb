9ec950afa4ee9ed97242f3a7c0d16efbe65f654a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2026-08-03 12:59:04 -0400

    Keep prior default of 3002

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ threads threads_count, threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 3002)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
