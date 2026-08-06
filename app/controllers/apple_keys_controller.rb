446a36b86be059bcdb303b5e6d56929f8e8d14b8
Sam Vevang <sam.vevang@gmail.com>
2026-08-06 09:37:39 -0500

    Add podcast-level Apple credential management
      - let podcasts select credentials owned by their PRX account
      - verify replacement credentials can access connected Apple shows
      - show account-wide credential usage
      - allow unused credentials to be removed
      - surface upload and selection errors

--- app/controllers/apple_keys_controller.rb
+++ app/controllers/apple_keys_controller.rb
@@ -1,5 +1,6 @@
 class AppleKeysController < ApplicationController
   before_action :set_podcast
+  before_action :set_apple_key, only: :destroy
 
   def create
     @apple_key = Apple::Key.new(apple_key_params.merge(account_id: @podcast.account_id))
@@ -8,9 +9,28 @@ class AppleKeysController < ApplicationController
     if @apple_key.save
       redirect_to podcast_integrations_path(@podcast), notice: t(".notice")
     else
-      @apple_keys = policy_scope(Apple::Key).for_account(@podcast.account_id).order(:created_at)
+      @apple_keys = policy_scope(Apple::Key)
+        .for_account(@podcast.account_id)
+        .includes(:podcasts)
+        .order(:created_at)
+      @connected_apple_show_ids = @podcast.feeds
+        .joins(:apple_show_feed_binding)
+        .distinct
+        .pluck("apple_show_feed_bindings.apple_show_id")
       flash.now[:error] = t(".error")
       render "podcast_integrations/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @apple_key
+
+    if @apple_key.podcasts.exists? || @apple_key.delegated_delivery_config
+      redirect_to podcast_integrations_path(@podcast), alert: t(".in_use")
+    elsif @apple_key.destroy
+      redirect_to podcast_integrations_path(@podcast), notice: t(".notice")
+    else
+      redirect_to podcast_integrations_path(@podcast), alert: t(".error")
     end
   end
 
@@ -25,5 +45,13 @@ class AppleKeysController < ApplicationController
 
   def apple_key_params
     params.require(:apple_key).permit(:provider_id, :key_id, :key_pem_b64)
+  end
+
+  def set_apple_key
+    @apple_key = policy_scope(Apple::Key)
+      .for_account(@podcast.account_id)
+      .find(params[:id])
+  rescue ActiveRecord::RecordNotFound => error
+    render_not_found(error)
   end
 end
--- test/controllers/apple_keys_controller_test.rb
+++ test/controllers/apple_keys_controller_test.rb
@@ -36,4 +36,46 @@ class AppleKeysControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :forbidden
   end
+
+  test "renders account credential errors" do
+    post podcast_apple_keys_url(podcast), params: {
+      apple_key: key_params.merge(provider_id: "short")
+    }
+
+    assert_response :unprocessable_entity
+    assert_select "body", text: /Provider is too short/
+    assert_select "body", text: /Stubbed Account \(123\)/
+  end
+
+  test "removes an unused account credential" do
+    key = create(:apple_key, account_id: 123)
+
+    assert_difference "Apple::Key.count", -1 do
+      delete podcast_apple_key_url(podcast, key)
+    end
+
+    assert_redirected_to podcast_integrations_url(podcast)
+  end
+
+  test "does not remove a credential selected by a podcast" do
+    key = create(:apple_key, account_id: 123)
+    podcast.update!(apple_key: key)
+
+    assert_no_difference "Apple::Key.count" do
+      delete podcast_apple_key_url(podcast, key)
+    end
+
+    assert_redirected_to podcast_integrations_url(podcast)
+    assert_equal "Apple credentials cannot be removed while podcasts use them", flash[:alert]
+  end
+
+  test "does not remove a credential from another account" do
+    key = create(:apple_key, account_id: 456)
+
+    assert_no_difference "Apple::Key.count" do
+      assert_raises ActiveRecord::RecordNotFound do
+        delete podcast_apple_key_url(podcast, key)
+      end
+    end
+  end
 end
