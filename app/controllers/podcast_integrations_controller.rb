81f4bef193bbc2c63c162ef4a821952d70f35535
Sam Vevang <sam.vevang@gmail.com>
2026-08-06 09:37:39 -0500

    Add podcast-level Apple credential management
      - let podcasts select credentials owned by their PRX account
      - verify replacement credentials can access connected Apple shows
      - show account-wide credential usage
      - allow unused credentials to be removed
      - surface upload and selection errors

--- app/controllers/podcast_integrations_controller.rb
+++ app/controllers/podcast_integrations_controller.rb
@@ -1,17 +1,74 @@
 class PodcastIntegrationsController < ApplicationController
   before_action :set_podcast
+  before_action :load_apple_credentials
 
   def show
-    @apple_keys = policy_scope(Apple::Key).for_account(@podcast.account_id).order(:created_at)
-    @apple_key = Apple::Key.new(account_id: @podcast.account_id)
+    authorize @podcast
+  end
+
+  def update
+    authorize @podcast
+
+    submitted_key_id = podcast_integration_params[:apple_key_id].presence
+    selected_key = @apple_keys.find_by(id: submitted_key_id) if submitted_key_id
+
+    if submitted_key_id && selected_key.nil?
+      @podcast.errors.add(:apple_key, t(".unavailable"))
+    elsif selected_key == @podcast.apple_key
+      return redirect_to podcast_integrations_path(@podcast), notice: t(".notice")
+    elsif selected_key_can_access_connected_shows?(selected_key) && @podcast.update(apple_key: selected_key)
+      return redirect_to podcast_integrations_path(@podcast), notice: t(".notice")
+    end
+
+    flash.now[:error] = t(".error")
+    render :show, status: :unprocessable_entity
   end
 
   private
 
   def set_podcast
     @podcast = Podcast.find(params[:podcast_id])
-    authorize @podcast, :show?
   rescue ActiveRecord::RecordNotFound => error
     render_not_found(error)
+  end
+
+  def load_apple_credentials
+    @apple_keys = policy_scope(Apple::Key)
+      .for_account(@podcast.account_id)
+      .includes(:podcasts)
+      .order(:created_at)
+    @apple_key = Apple::Key.new(account_id: @podcast.account_id)
+    @connected_apple_show_ids = @podcast.feeds
+      .joins(:apple_show_feed_binding)
+      .distinct
+      .pluck("apple_show_feed_bindings.apple_show_id")
+  end
+
+  def podcast_integration_params
+    params.require(:podcast).permit(:apple_key_id)
+  end
+
+  def selected_key_can_access_connected_shows?(selected_key)
+    return true if @connected_apple_show_ids.empty?
+
+    unless selected_key
+      @podcast.errors.add(:apple_key, t(".required"))
+      return false
+    end
+
+    missing_show_ids = selected_key.inaccessible_show_ids(@connected_apple_show_ids)
+    return true if missing_show_ids.empty?
+
+    @podcast.errors.add(:apple_key, t(".inaccessible", show_ids: missing_show_ids.to_sentence))
+    false
+  rescue => error
+    Rails.logger.error(
+      "Unable to verify Apple credential for podcast",
+      podcast_id: @podcast.id,
+      apple_key_id: selected_key&.id,
+      error: error
+    )
+    @podcast.errors.add(:apple_key, t(".verification_failed"))
+    false
   end
 end
