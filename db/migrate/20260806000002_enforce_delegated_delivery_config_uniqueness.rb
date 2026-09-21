4d593922beb83446d9ec80c180548882b27a40f7
Sam Vevang <sam.vevang@gmail.com>
2026-09-21 13:45:29 -0500

    Combine Apple uniqueness migrations

--- db/migrate/20260806000002_enforce_delegated_delivery_config_uniqueness.rb
+++ db/migrate/20260806000002_enforce_delegated_delivery_config_uniqueness.rb
@@ -7,17 +7,34 @@ class EnforceDelegatedDeliveryConfigUniqueness < ActiveRecord::Migration[7.2]
   def up
     verify_unique!(:feed_id)
     verify_unique!(:show_feed_binding_id)
+    verify_unique_show_connections!
 
     replace_index(:feed_id, FEED_INDEX, unique: true)
     replace_index(:show_feed_binding_id, BINDING_INDEX, unique: true)
+    add_index :apple_show_feed_bindings, :apple_show_id, unique: true
   end
 
   def down
+    remove_index :apple_show_feed_bindings, :apple_show_id
     replace_index(:feed_id, FEED_INDEX, unique: false)
     replace_index(:show_feed_binding_id, BINDING_INDEX, unique: false)
   end
 
   private
+
+  def verify_unique_show_connections!
+    duplicate_show_ids = select_values(<<~SQL.squish)
+      SELECT apple_show_id
+      FROM apple_show_feed_bindings
+      GROUP BY apple_show_id
+      HAVING COUNT(*) > 1
+      ORDER BY apple_show_id
+    SQL
+    return if duplicate_show_ids.empty?
+
+    raise ActiveRecord::MigrationError,
+      "Duplicate Apple show connections must be resolved: #{duplicate_show_ids.join(", ")}"
+  end
 
   def verify_unique!(column)
     duplicate_values = select_values(<<~SQL.squish)
